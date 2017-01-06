56be6e822bf0445505073d843b478a0b119aac07
Chris Wynn <chrisw@MacBook-Pro-2.local>
2017-01-06 15:48:39 -0700

    New Handling of the Menu

--- dmm-crm.php
+++ dmm-crm.php
@@ -4,7 +4,7 @@
 	Plugin URI: http://chasm.solutions
 	Description: This is the DMM CRM for digital marketing to disciple making movements.
 	Version: 0.1
-	Author: By Us
+	Author: Chasm.Solutions & Kingdom.Training
 	Author URI: http://chasm.solutions
 	License: GPL2
 	
@@ -18,21 +18,76 @@ add_action( 'admin_menu', 'dmm_menu' );
 
 /** DMM CRM menu creation */
 function dmm_menu() {
+	// Main Menus
 	//add_menu_page( string $page_title, string $menu_title, string $capability, string $menu_slug, callable $function = '', string $icon_url = '', int $position = null );
-	add_menu_page( 'Dashboard', 'DMM CRM', 'manage_options', 'dmm', 'dmm_dash_options', '' , '2' );
+	add_menu_page(  'Dashboard', 'Dashboard', 'dmm_team', 'dmm_dash', 'dmm_dash_options', 'dashicons-welcome-widgets-menus' , '1' );
+	add_menu_page(       'Prayer', 'Prayer', 'dmm_team', 'dmm_prayer', 'dmm_prayer_options', 'dashicons-smiley' , '2' );
+	add_menu_page(  'Outreach', 'Outreach', 'dmm_team', 'dmm_outreach', 'dmm_outreach_options', 'dashicons-admin-links' , '3' );
+	add_menu_page(  'Contacts', 'Contacts', 'dmm_team', 'dmm_contacts', 'dmm_contacts_options', 'dashicons-admin-users' , '4' );
+	add_menu_page(  'Coaching', 'Coaching', 'dmm_team', 'dmm_coaching', 'dmm_coaching_options', 'dashicons-admin-multisite' , '5' );
+	add_menu_page(  'Settings', 'Settings', 'dmm_team', 'dmm_settings', 'dmm_settings_options', 'dashicons-forms' , '6' );
 	
 	//add_submenu_page( string $parent_slug, string $page_title, string $menu_title, string $capability, string $menu_slug, callable $function = '' );
-	add_submenu_page( 'dmm', 'Prayer', 'Prayer', 'manage_options', 'dmm_prayer', 'dmm_prayer_options' );
-	add_submenu_page( 'dmm', 'Outreach', 'Outreach', 'manage_options', 'dmm_outreach', 'dmm_outreach_options' );
-	add_submenu_page( 'dmm', 'Contacts', 'Contacts', 'manage_options', 'dmm_contacts', 'dmm_contacts_options' );
-	add_submenu_page( 'dmm', 'Coaching', 'Coaching', 'manage_options', 'dmm_coaching', 'dmm_coaching_options' );
-	add_submenu_page( 'dmm', 'Settings', 'Settings', 'manage_options', 'dmm_settings', 'dmm_settings_options' );
+	// Dashboard Submenu
+	add_submenu_page( 'dmm_dash', 'Stats', 'Stats', 'dmm_team', 'dmm_dash_stats', 'dmm_options' );
+	add_submenu_page( 'dmm_dash', 'Maps', 'Maps', 'dmm_team', 'dmm_dash_maps', 'dmm_options' );
+	
+	// Prayer Submenu
+	add_submenu_page( 'dmm_prayer', 'Guide', 'Guide', 'dmm_team', 'dmm_prayer_guide', 'dmm_options' );
+	add_submenu_page( 'dmm_prayer', 'Map', 'Map', 'dmm_team', 'dmm_prayer_map', 'dmm_options' );
+	add_submenu_page( 'dmm_prayer', 'Updates', 'Updates', 'dmm_team', 'dmm_prayer_updates', 'dmm_options' );
+	add_submenu_page( 'dmm_prayer', 'Training', 'Training', 'dmm_team', 'dmm_prayer_training', 'dmm_options' );
+	
+	// Outreach Submenu
+	add_submenu_page( 'dmm_outreach', 'Campaigns', 'Campaigns', 'dmm_team', 'dmm_outreach_campaigns', 'dmm_options' );
+	add_submenu_page( 'dmm_outreach', 'Analytics', 'Analytics', 'dmm_team', 'dmm_outreach_analytics', 'dmm_options' );
+	add_submenu_page( 'dmm_outreach', 'Library', 'Library', 'dmm_team', 'dmm_outreach_library', 'dmm_options' );
+	add_submenu_page( 'dmm_outreach', 'Training', 'Training', 'dmm_team', 'dmm_outreach_training', 'dmm_options' );
+	
+	// Contacts Submenu
+	add_submenu_page( 'dmm_contacts', 'Contacts', 'Contacts', 'dmm_team', 'dmm_outreach_campaigns', 'dmm_options' );
+	add_submenu_page( 'dmm_contacts', 'Comments', 'Comments', 'dmm_team', 'dmm_outreach_campaigns', 'dmm_options' );
+	add_submenu_page( 'dmm_contacts', 'Needs Update', 'Needs Update', 'dmm_team', 'dmm_outreach_library', 'dmm_options' );
+	add_submenu_page( 'dmm_contacts', 'Needs Assigned', 'Needs Assigned', 'dmm_router', 'dmm_outreach_analytics', 'dmm_options' );
+	add_submenu_page( 'dmm_contacts', 'Training', 'Training', 'dmm_team', 'dmm_outreach_training', 'dmm_options' );
+	
+	// Coaching Submenu
+	add_submenu_page( 'dmm_coaching', 'Campaigns', 'Campaigns', 'dmm_team', 'dmm_outreach_campaigns', 'dmm_options' );
+	add_submenu_page( 'dmm_coaching', 'Analytics', 'Analytics', 'dmm_team', 'dmm_outreach_analytics', 'dmm_options' );
+	add_submenu_page( 'dmm_coaching', 'Library', 'Library', 'dmm_team', 'dmm_outreach_library', 'dmm_options' );
+	add_submenu_page( 'dmm_coaching', 'Training', 'Training', 'dmm_team', 'dmm_outreach_training', 'dmm_options' );
+	
+	
+	// Settings Submenu
+	add_submenu_page( 'dmm_settings', 'General', 'General', 'dmm_team', 'dmm_settings_general', 'dmm_options' );
+	add_submenu_page( 'dmm_settings', 'Maps', 'Maps', 'dmm_team', 'dmm_settings_maps', 'dmm_options' );
+	add_submenu_page( 'dmm_settings', 'Integrations', 'Integrations', 'dmm_team', 'dmm_settings_integrations', 'dmm_options' );
+	add_submenu_page( 'dmm_settings', 'Sites', 'Sites', 'dmm_team', 'dmm_settings_sites', 'dmm_options' );
+	add_submenu_page( 'dmm_settings', 'About', 'About', 'dmm_team', 'dmm_settings_about', 'dmm_options' );
+	
 	
 }
 
 /** Main Menu functions and page content */
+
+function dmm_options() {
+	if ( !current_user_can( 'dmm_team' ) )  {
+		wp_die( __( 'You do not have sufficient permissions to access this page.' ) );
+	}
+	$html = '<div class="wrap">
+			 	<h2>DMM OPTIONS PLACEHOLDER</h2>';
+	
+	
+	
+	$html .= '</div>'; // end div class wrap
+	
+	echo $html;
+	
+}
+
+
 function dmm_dash_options() {
-	if ( !current_user_can( 'manage_options' ) )  {
+	if ( !current_user_can( 'dmm_team' ) )  {
 		wp_die( __( 'You do not have sufficient permissions to access this page.' ) );
 	}
 	$html = '<div class="wrap">
@@ -47,7 +102,7 @@ function dmm_dash_options() {
 }
 
 function dmm_prayer_options() {
-	if ( !current_user_can( 'manage_options' ) )  {
+	if ( !current_user_can( 'dmm_team' ) )  {
 		wp_die( __( 'You do not have sufficient permissions to access this page.' ) );
 	}
 	
@@ -116,7 +171,7 @@ function dmm_prayer_options() {
 }
 
 function dmm_outreach_options() {
-	if ( !current_user_can( 'manage_options' ) )  {
+	if ( !current_user_can( 'dmm_team' ) )  {
 		wp_die( __( 'You do not have sufficient permissions to access this page.' ) );
 	}
 	
@@ -185,7 +240,7 @@ function dmm_outreach_options() {
 }
 
 function dmm_contacts_options() {
-	if ( !current_user_can( 'manage_options' ) )  {
+	if ( !current_user_can( 'dmm_team' ) )  {
 		wp_die( __( 'You do not have sufficient permissions to access this page.' ) );
 	}
 	
@@ -251,7 +306,7 @@ function dmm_contacts_options() {
 
 
 function dmm_coaching_options() {
-	if ( !current_user_can( 'manage_options' ) )  {
+	if ( !current_user_can( 'dmm_team' ) )  {
 		wp_die( __( 'You do not have sufficient permissions to access this page.' ) );
 	}
 	
@@ -327,7 +382,7 @@ function dmm_coaching_options() {
 }
 
 function dmm_settings_options() {
-	if ( !current_user_can( 'manage_options' ) )  {
+	if ( !current_user_can( 'dmm_team' ) )  {
 		wp_die( __( 'You do not have sufficient permissions to access this page.' ) );
 	}
 	
