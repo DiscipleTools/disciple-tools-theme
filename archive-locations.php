e8236c4c383e6f72464820f0f3595fec2bea4a4e
Chris Wynn <chris@chasm.solutions>
2017-04-21 14:06:53 -0600

    Archive Location edits.

---
+++ archive-locations.php
@@ -0,0 +1,37 @@
+<?php get_header(); ?>
+
+<div id="content">
+
+    <div id="inner-content" class="row">
+
+        <!-- Breadcrumb Navigation-->
+        <nav aria-label="You are here:" role="navigation">
+            <ul class="breadcrumbs">
+                <li><a href="/">Dashboard</a></li>
+                <li>
+                    <span class="show-for-sr">Current: </span> Locations
+                </li>
+            </ul>
+        </nav>
+
+        <main id="main" class="large-8 medium-8 columns" role="main">
+
+            <?php include ('parts/content-locations-tabs.php') ?>
+
+        </main> <!-- end #main -->
+
+        <aside class="large-4 medium-4 columns ">
+
+            <section class="block">
+
+                <p>Links</p>
+
+            </section>
+
+        </aside> <!-- end #aside -->
+
+    </div> <!-- end #inner-content -->
+
+</div> <!-- end #content -->
+
+<?php get_footer(); ?>
