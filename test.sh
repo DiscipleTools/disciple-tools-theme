cd51efa27d087d93b22b54a670261c1114852bd9
Burning Sun <burningsun@example.com>
2017-07-19 15:48:49 +0100

    Add .travis.yml for Travis CI integration

---
+++ test.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+cd "$(dirname "${BASH_SOURCE[0]}")"
+
+found_error=0
+
+while read -d '' filename ; do
+
+    # php -l checks the file for syntax errors
+    php -l "$filename" || found_error=1
+
+done < <(find . -name "*.php" -print0)
+
+exit $found_error
