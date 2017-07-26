15d946b4ec45ad2dc76fb14aeb1c5b5e3dc69d36
Burning Sun <burntsun@users.noreply.github.com>
2017-07-26 18:22:12 +0100

    Replace include with get_template_part in one instance

--- archive-contacts.php
+++ archive-contacts.php
@@ -24,7 +24,7 @@
 
             <main id="main" class="large-6 medium-6 columns padding-bottom" role="main">
 
-                <?php include('parts/content-contacts.php') ?>
+                <?php get_template_part('parts/content', 'contacts'); ?>
 
             </main> <!-- end #main -->
 
