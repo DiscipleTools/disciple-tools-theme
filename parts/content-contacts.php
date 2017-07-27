4d03170d66d07f20b9d3bf5ea66a3cb696d71d0a
Burning Sun <burntsun@users.noreply.github.com>
2017-07-27 20:23:36 +0100

    Use get_user_contacts from DT plugin
    The main advantage of using this method is that it checks user
    permissions correctly and in one place.

--- parts/content-contacts.php
+++ parts/content-contacts.php
@@ -4,21 +4,12 @@ declare(strict_types=1);
 /* Fetch and print the first 29 results only, list-contacts.js will fetch all
  * the results using AJAX and will replace the list. */
 
-$args = array(
-    'post_type' => 'contacts',
-    'nopaging' => true,
-    'posts_per_page' => 29,
-    'meta_query' => array (
-        'relation' => 'AND', // Optional, defaults to "AND"
-        array(
-            'key'     => 'assigned_to',
-            'value'   => 'user-'. get_current_user_id(),
-            'compare' => '='
-        )
-    ),
-    'orderby' => 'ID',
+$query1 = Disciple_Tools_Contacts::get_user_contacts(
+    get_current_user_id(), true, [ 'posts_per_page' => 29 ]
 );
-$query1 = new WP_Query( $args );
+if (is_wp_error( $query1 )) {
+    throw new Exception( "permission denied: " . implode( $query1->get_error_messages() ) );
+}
 
 ?>
 <div id="my-contacts" class="bordered-box">
