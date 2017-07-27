5d854aac13aa435ffd95ce8fb5ce85e73356b9a2
Burning Sun <burntsun@users.noreply.github.com>
2017-07-27 18:30:40 +0100

    Show notice when on old version of PHP

--- functions.php
+++ functions.php
@@ -1,5 +1,28 @@
 <?php
 
+function admin_notice_required_php_version_dt_theme() {
+    ?>
+    <div class="notice notice-error">
+        <p><?php _e( "The Disciple Tools theme requires PHP 7.0 or greater before it will have any effect. Please upgrade your PHP version or uninstall this theme." ); ?></p>
+    </div>
+    <?php
+}
+
+if (version_compare( phpversion(), '7.0', '<' ) || true) {
+
+    /* We only support PHP >= 7.0, however, we want to support allowing users
+     * to install this theme even on old versions of PHP, without showing a
+     * horrible message, but instead a friendly notice.
+     *
+     * For this to work, this file must be compatible with old PHP versions.
+     * Feel free to use PHP 7 features in other files, but not in this one.
+     */
+
+    add_action( 'admin_notices', 'admin_notice_required_php_version_dt_theme' );
+    error_log( 'Disciple Tools theme requires PHP version 7.0 or greater, please upgrade PHP or uninstall this theme' );
+    return;
+}
+
 /**
  * Globals
  */
