76fde332e363df39e08908f2f1ec32666ee1c58f
corsac <corsac@hushmail.com>
2017-08-04 22:42:30 +0100

    Contact Details: Fill assigned-to field, implement SHOW MORE section

--- parts/loop-single-contact.php
+++ parts/loop-single-contact.php
@@ -16,6 +16,7 @@
 
         <div class="medium-4 columns">
             <strong>Phone</strong>
+            <i class="fa fa-plus"></i>
             <ul>
                 <?php
                 foreach($contact->fields[ "phone_numbers" ] ?? [] as $field => $value){
@@ -32,6 +33,18 @@
             </ul>
         </div>
         <div class="medium-4 columns">
+            <strong>Assigned To</strong>
+            <ul>
+                <li>
+                <?php
+                if ( isset( $contact->fields["assigned_to"] ) ){
+                    echo esc_html( $contact->fields["assigned_to"]["display"] );
+                } else {
+                    echo "None Assigned";
+                }
+                ?>
+                </li>
+            </ul>
             <strong>Locations</strong>
             <ul>
                 <?php
@@ -39,18 +52,34 @@
                     echo '<li><a href="' . $value->permalink . '">'. $value->post_title .'</a></li>';
                 }?>
             </ul>
-            <strong>Address</strong>
-            <ul>
-                <?php
-                foreach($contact->fields[ "address" ]  ?? [] as $value){
-                    echo '<li>' . $value[0] . '</li>';
-                }?>
-            </ul>
+
         </div>
         <div class="medium-4 columns">
             <strong>Social Links</strong>
         </div>
-    </div> <!-- end article section -->
+    </div>
+
+    <div class="row">
+        <div id="show-more-content" data-toggler
+             data-animate="fade-in fade-out" aria-expanded="false" style="display:none;">
+            <div class="medium-4 columns">
+                <strong>Address</strong>
+                <ul>
+                    <?php
+                    foreach($contact->fields[ "address" ]  ?? [] as $value){
+                        echo '<li>' . $value[0] . '</li>';
+                    }?>
+                </ul>
+            </div>
+        </div>
+    </div>
+    <div class="row show-more-button" style="text-align: center" >
+        <button class="clear" data-toggle="show-more-button show-more-content show-content-button"  href="#">SHOW
+            <span id="show-more-button" data-toggler data-animate="fade-in fade-out">MORE <i class="fi-plus"></i></span>
+            <span id="show-content-button" data-toggler data-animate="fade-in fade-out" aria-expanded="false" style="display:none;">LESS <i class="fi-minus"></i></span>
+        </button>
+    </div>
+
 
 </section> <!-- end article -->
 
