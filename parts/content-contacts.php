64315524bd7e6723eeb36c749b387b1c2ac22fbb
Burning Sun <burntsun@users.noreply.github.com>
2017-09-01 17:38:51 +0100

    Add "sort by" button for contacts list

--- parts/content-contacts.php
+++ parts/content-contacts.php
@@ -3,6 +3,20 @@ declare(strict_types=1);
 ?>
 <div class="bordered-box">
     <h3><?php _e( "Contacts" ); ?></h3>
+    <div class="reveal js-list-contacts-sort-by-modal" data-reveal>
+        <h1><?php _e( "Sort by:" ); ?></h1>
+        <div>
+            <button class="js-sort-by button small hollow" data-column-index="1"><?php _e( "Name" ); ?></button>
+            <button class="js-sort-by button small hollow" data-column-index="2"><?php _e( "Status" ); ?></button>
+            <button class="js-sort-by button small hollow" data-column-index="3"><?php _e( "Faith milestones" ); ?></button>
+            <button class="js-sort-by button small hollow" data-column-index="4"><?php _e( "Assigned to" ); ?></button>
+            <button class="js-sort-by button small hollow" data-column-index="5"><?php _e( "Location" ); ?></button>
+            <button class="js-sort-by button small hollow" data-column-index="6"><?php _e( "Group" ); ?></button>
+        </div>
+        <button class="close-button" data-close aria-label="<?php _e( "Close" ); ?>" type="button"><!--
+            --><span aria-hidden="true">&times;</span><!--
+        --></button>
+    </div>
 
     <table class="js-list-contacts">
         <thead><tr>
