5f79c574e5dcd96e788f363c008d30b8af0bd0bc
Burning Sun <burntsun@users.noreply.github.com>
2017-09-11 18:46:38 +0100

    Add modal for filters in the group list page

--- parts/nav-offcanvas-topbar.php
+++ parts/nav-offcanvas-topbar.php
@@ -13,7 +13,7 @@
         <div class="title-bar-title"><?php _e( "Disciple Tools" ); ?></div>
     </div>
     <div class="title-bar-right">
-        <?php if (is_post_type_archive( "contacts" )): ?>
+        <?php if (is_post_type_archive( "contacts" ) || is_post_type_archive( "groups" )): ?>
         <button data-open="filters-modal">
             <img title="<?php _e( "Filters" ); ?>" src="<?php echo get_template_directory_uri() . "/assets/images/filter.svg" ?>">
         </button>
