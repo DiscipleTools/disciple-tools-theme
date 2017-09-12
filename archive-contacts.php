065c3b21c377d88907053d34663255bd05aa3a25
Burning Sun <burntsun@users.noreply.github.com>
2017-09-12 17:05:08 +0100

    Swap priorities and filters in contacts list

--- archive-contacts.php
+++ archive-contacts.php
@@ -22,51 +22,6 @@ $contacts_contact_unattempted = Disciple_Tools_Contacts::get_user_prioritized_co
 
         <div id="inner-content" class="grid-x grid-margin-x">
 
-            <aside class="large-2 cell padding-bottom hide-for-small-only">
-                <div class="bordered-box js-pane-filters">
-                    <?php /* Javascript may move .js-filters-modal-content to this location. */ ?>
-                </div>
-            </aside>
-
-            <div class="reveal js-filters-modal" id="filters-modal" data-reveal>
-                <div class="js-filters-modal-content">
-                    <h3><?php _e( "Filters" ); ?></h3>
-                    <div>
-                        <button class="button small js-my-contacts"><?php _e( "My contacts" ); ?></button>
-                        <button disabled class="button small js-clear-filters"><?php _e( "Clear filters" ); ?></button>
-                    </div>
-                    <div class="filter js-list-filter" data-filter="assigned_login">
-                        <div class="filter__title js-list-filter-title"><?php _e( "Assigned to" ); ?></div>
-                        <p><?php _e( "Loading..." ); ?></p>
-                    </div>
-                    <div class="filter filter--closed js-list-filter" data-filter="overall_status">
-                        <div class="filter__title js-list-filter-title"><?php _e( "Status" ); ?></div>
-                        <p><?php _e( "Loading..." ); ?></p>
-                    </div>
-                    <div class="filter filter--closed js-list-filter" data-filter="locations">
-                        <div class="filter__title js-list-filter-title"><?php _e( "Locations" ); ?></div>
-                        <p><?php _e( "Loading..." ); ?></p>
-                    </div>
-                    <div class="filter filter--closed js-list-filter" data-filter="seeker_path">
-                        <div class="filter__title js-list-filter-title"><?php _e( "Seeker path" ); ?></div>
-                        <p><?php _e( "Loading..." ); ?></p>
-                    </div>
-                    <div class="filter filter--closed js-list-filter" data-filter="requires_update">
-                        <div class="filter__title js-list-filter-title"><?php _e( "Update needed" ); ?></div>
-                        <p><?php _e( "Loading..." ); ?></p>
-                    </div>
-                </div>
-                <button class="close-button" data-close aria-label="<?php _e( "Close modal" ); ?>" type="button">
-                    <span aria-hidden="true">&times;</span>
-                </button>
-            </div>
-
-            <main id="main" class="large-8 cell padding-bottom" role="main">
-
-                <?php get_template_part( 'parts/content', 'contacts' ); ?>
-
-            </main> <!-- end #main -->
-
             <aside class="large-2 cell padding-bottom"  data-sticky-container>
                 <div class="bordered-box">
                     <h3><?php _e( "Priorities" ); ?></h3>
@@ -128,6 +83,51 @@ $contacts_contact_unattempted = Disciple_Tools_Contacts::get_user_prioritized_co
                     <?php endif; ?>
                 </div>
             </aside>
+
+            <main id="main" class="large-8 cell padding-bottom" role="main">
+
+                <?php get_template_part( 'parts/content', 'contacts' ); ?>
+
+            </main> <!-- end #main -->
+
+            <aside class="large-2 cell padding-bottom hide-for-small-only">
+                <div class="bordered-box js-pane-filters">
+                    <?php /* Javascript may move .js-filters-modal-content to this location. */ ?>
+                </div>
+            </aside>
+
+            <div class="reveal js-filters-modal" id="filters-modal" data-reveal>
+                <div class="js-filters-modal-content">
+                    <h3><?php _e( "Filters" ); ?></h3>
+                    <div>
+                        <button class="button small js-my-contacts"><?php _e( "My contacts" ); ?></button>
+                        <button disabled class="button small js-clear-filters"><?php _e( "Clear filters" ); ?></button>
+                    </div>
+                    <div class="filter js-list-filter" data-filter="assigned_login">
+                        <div class="filter__title js-list-filter-title"><?php _e( "Assigned to" ); ?></div>
+                        <p><?php _e( "Loading..." ); ?></p>
+                    </div>
+                    <div class="filter filter--closed js-list-filter" data-filter="overall_status">
+                        <div class="filter__title js-list-filter-title"><?php _e( "Status" ); ?></div>
+                        <p><?php _e( "Loading..." ); ?></p>
+                    </div>
+                    <div class="filter filter--closed js-list-filter" data-filter="locations">
+                        <div class="filter__title js-list-filter-title"><?php _e( "Locations" ); ?></div>
+                        <p><?php _e( "Loading..." ); ?></p>
+                    </div>
+                    <div class="filter filter--closed js-list-filter" data-filter="seeker_path">
+                        <div class="filter__title js-list-filter-title"><?php _e( "Seeker path" ); ?></div>
+                        <p><?php _e( "Loading..." ); ?></p>
+                    </div>
+                    <div class="filter filter--closed js-list-filter" data-filter="requires_update">
+                        <div class="filter__title js-list-filter-title"><?php _e( "Update needed" ); ?></div>
+                        <p><?php _e( "Loading..." ); ?></p>
+                    </div>
+                </div>
+                <button class="close-button" data-close aria-label="<?php _e( "Close modal" ); ?>" type="button">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
 
 
         </div> <!-- end #inner-content -->
