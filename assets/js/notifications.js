b289f1a06a5104e5246ec6411f4428766523fbc1
Chris Wynn <chris@chasm.solutions>
2017-10-02 16:51:05 -0600

    Notifications API call and template

--- assets/js/notifications.js
+++ assets/js/notifications.js
@@ -1,16 +1,12 @@
-jQuery.ajaxSetup({
-  beforeSend: function(xhr) {
-    xhr.setRequestHeader('X-WP-Nonce', wpApiNotifications.nonce);
-  },
-})
-
-
 function get_new_notification_count(){
   return jQuery.ajax({
     type: "POST",
     contentType: "application/json; charset=utf-8",
     dataType: "json",
     url: wpApiNotifications.root + 'dt/v1/notifications/get_new_notifications_count',
+    beforeSend: function(xhr) {
+      xhr.setRequestHeader('X-WP-Nonce', wpApiNotifications.nonce);
+    },
   })
     .done(function (data) {
       if(data > 0) {
@@ -33,6 +29,9 @@ function mark_viewed(notification_id){
     contentType: "application/json; charset=utf-8",
     dataType: "json",
     url: wpApiNotifications.root + 'dt/v1/notifications/mark_viewed/'+notification_id,
+    beforeSend: function(xhr) {
+      xhr.setRequestHeader('X-WP-Nonce', wpApiNotifications.nonce);
+    },
   })
     .done(function (data) {
       get_new_notification_count()
@@ -52,6 +51,9 @@ function mark_all_viewed(){
     contentType: "application/json; charset=utf-8",
     dataType: "json",
     url: wpApiNotifications.root + 'dt/v1/notifications/mark_all_viewed/'+id,
+    beforeSend: function(xhr) {
+      xhr.setRequestHeader('X-WP-Nonce', wpApiNotifications.nonce);
+    },
   })
     .done(function (data) {
       get_new_notification_count()
@@ -64,7 +66,59 @@ function mark_all_viewed(){
     })
 }
 
+/* TODO finish the template */
+let notificationTemplate = _.template(`
+    <div class="cell">
+        <div class="grid-x grid-margin-x grid-padding-y bottom-border">
+            <div class="cell medium-1 hide-for-small-only">
+                <img src="http://via.placeholder.com/50x50?text=icon" width="50px" height="50px"/>
+            </div>
+            <div class="auto cell">
+                 <%- note %>
+            </div>
+            
+            <div class="small-2 medium-1 cell padding-5">
+                 <a class="mark-viewed-<%- id %> button small" style="border-radius:100px; margin: .7em 0 0;">
+                    <i class="fi-check"></i>
+                 </a>
+            </div>
+            
+        </div>                                 
+    </div>`
+)
 
+/* TODO finish the notifications get call and template */
+function get_notifications( limit, offset ) {
+  "use strict";
+  let data = { "limit": limit, "offset": offset }
+  return jQuery.ajax({
+    type: "POST",
+    data: JSON.stringify(data),
+    contentType: "application/json; charset=utf-8",
+    dataType: "json",
+    url: wpApiNotifications.root + 'dt/v1/notifications/get_notifications',
+    beforeSend: function(xhr) {
+      xhr.setRequestHeader('X-WP-Nonce', wpApiNotifications.nonce);
+    }
+  })
+    .done(function ( data ) {
+      if( data ) {
+        jQuery.each( data, function (i, item) {
+          jQuery('#notification-list').append(notificationTemplate({
+            note: data[i].notification_note,
+            id: data[i].id,
+            is_new: data[i].is_new
+          }))
+        })
+      }
+    })
+    .fail(function (err) {
+      console.log("error")
+      console.log(err)
+      jQuery("#errors").append(err.responseText)
+    })
+}
+get_notifications();
 
 
 
--- template-notifications.php
+++ template-notifications.php
@@ -2,6 +2,10 @@
 /*
 Template Name: Notifications
 */
+
+// query to get new notifications
+$dt_notifications = Disciple_Tools_Notifications::get_notifications();
+$dt_user = wp_get_current_user();
 ?>
 
 <?php get_header(); ?>
@@ -12,110 +16,89 @@ dt_print_breadcrumbs(
     __( "Notifications" ),
     false
 );
-
-
-global $post;
 ?>
 
-<div id="content">
+    <div id="content">
 
-    <div id="inner-content" class="grid-x grid-margin-x">
+        <div id="inner-content" class="grid-x grid-margin-x">
 
-        <div class="large-2 medium-12 small-12 cell "></div>
+            <div class="large-2 medium-12 small-12 cell "></div>
 
-        <div class="large-8 medium-12 small-12 cell ">
+            <div class="large-8 medium-12 small-12 cell ">
 
+                <div class="bordered-box">
 
-            <div class="bordered-box">
-
-
-                <div class="grid-x">
-                    <div class="cell">
-                        <div class="grid-x grid-margin-x " style="border-bottom: 1px solid #ccc;">
-                            <div class="small-4 medium-5 cell"><span class="badge alert notification-count"
-                                                                     style="display:none;">&nbsp;</span>
-                                <strong>New</strong></div>
-                            <div class="small-4 medium-2 cell">
-                                <div class="expanded small button-group" style="text-align:center;">
-                                    <a href="#" class="button">All</a>
-                                    <a href="#" class="button hollow">Unread</a>
+                    <div id="notification-list" class="grid-x">
+                        <div class="cell">
+                            <div class="grid-x grid-margin-x " style="border-bottom: 1px solid #ccc;">
+                                <div class="small-4 medium-5 cell"><span class="badge alert notification-count"
+                                                                         style="display:none;">&nbsp;</span>
+                                    <strong>New</strong></div>
+                                <div class="small-4 medium-2 cell">
+                                    <div class="expanded small button-group" style="text-align:center;">
+                                        <a onclick="get_notifications()" class="button">All</a>
+                                        <a onclick="get_notifications()" class="button hollow">Unread</a>
+                                    </div>
                                 </div>
-                            </div>
-                            <div class="small-4 medium-5 cell" style="text-align:right;">
+                                <div class="small-4 medium-5 cell" style="text-align:right;">
                                     <span class="hide-for-small-only">
                                         <a onclick="mark_all_viewed()">Mark All as Read</a>  -
                                         <a href="<?php echo esc_url( home_url( '/' ) ); ?>settings/">Settings</a>
                                     </span>
-                                <span class="show-for-small-only">
+                                    <span class="show-for-small-only">
                                         <a onclick="mark_all_viewed()">Mark All</a>
                                     </span>
-                            </div>
-                        </div>
-                    </div>
-
-                    <?php
-
-                    // query to get new notifications
-                    $dt_notifications = Disciple_Tools_Notifications::get_notifications( [ 'user_id' => get_current_user_id() ] );
-
-                    // display new notifications
-                    if( $dt_notifications[ 'status' ] == true ) {
-                        foreach( $dt_notifications[ 'result' ] as $notification ) {
-                            ?>
-                            <div class="cell hover">
-                                <div class="grid-x grid-margin-x grid-padding-y bottom-border">
-                                    <div class="small-1 cell"><img src="http://via.placeholder.com/50x50"
-                                                                   width="50px" height="50px"/></div>
-                                    <div class="auto cell">
-                                        <?php echo wp_kses( $notification[ 'notification_note' ], wp_kses_allowed_html( 'post' ) ); ?>
-                                        <br>
-                                        <span
-                                            class="grey"><?php echo esc_html( $notification[ 'pretty_time' ] ); ?></span>
-                                    </div>
-                                    <div class="small-1 cell">
-                                        <?php if( $notification[ 'is_new' ] ) : ?>
-                                            <a id="mark-viewed-<?php echo esc_attr( $notification[ 'id' ] ); ?>"
-                                               class="mark-viewed"
-                                               onclick="mark_viewed(<?php echo esc_attr( $notification[ 'id' ] ); ?>);">
-                                                <span class="badge " style="float:right;">&nbsp;</span>
-                                            </a>
-                                        <?php endif; ?>
-                                    </div>
                                 </div>
                             </div>
-                            <?php
-                        }
-                    }
-
-                    ?>
-                    <?php
-                    // TODO Add infinite scroll or a click loader for the next 50 records.
-                    // TODO Add notification bubble to top bar
-
-                    ?>
-                    <div class="cell">
-                        <div class="grid-x grid-margin-x grid-padding-y">
-                            <div class="cell" style="text-align:center;">
-
-
-                            </div>
                         </div>
+
+                        <?php
+                        // display new notifications
+                        if( $dt_notifications[ 'status' ] == true ) {
+                            foreach( $dt_notifications[ 'result' ] as $notification ) {
+                                ?>
+                                <div class="cell">
+                                    <div class="grid-x grid-margin-x grid-padding-y bottom-border">
+                                        <div class="cell medium-1 hide-for-small-only">
+                                            <img src="http://via.placeholder.com/50x50?text=icon" width="50px"
+                                                 height="50px"/>
+                                        </div>
+                                        <div class="auto cell">
+                                            <?php echo wp_kses( $notification[ 'notification_note' ], wp_kses_allowed_html( 'post' ) ); ?>
+                                            <br>
+                                            <span
+                                                class="grey"><?php echo esc_html( $notification[ 'pretty_time' ] ); ?></span>
+                                        </div>
+                                        <div class="small-2 medium-1 cell padding-5">
+                                            <?php if( $notification[ 'is_new' ] ) : ?>
+                                                <a id="mark-viewed-<?php echo esc_attr( $notification[ 'id' ] ); ?>"
+                                                   class="mark-viewed button small"
+                                                   style="border-radius:100px; margin: .7em 0 0;"
+                                                   onclick="mark_viewed(<?php echo esc_attr( $notification[ 'id' ] ); ?>);">
+                                                    <i class="fi-check"></i>
+                                                </a>
+                                            <?php endif; ?>
+                                        </div>
+                                    </div>
+                                </div>
+                                <?php
+                            }
+                        }
+
+                        ?>
                     </div>
 
                 </div>
 
+
             </div>
 
+            <div class="large-2 medium-12 small-12 cell "></div>
 
-        </div>
+        </div> <!-- end #inner-content -->
 
-        <div class="large-2 medium-12 small-12 cell "></div>
-
-    </div> <!-- end #inner-content -->
-
-</div> <!-- end #content -->
+    </div> <!-- end #content -->
 
 <?php
-
 
 get_footer();
