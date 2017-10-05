10be50da7872ef1410e0b2d946d5d02b85a4d1c1
Chris Wynn <chris@chasm.solutions>
2017-10-05 13:58:07 -0600

    Added metrics page layout and sample scripting
    Remade the metrics page
    Added metrics.js
    Added enqueue
    Put in some placeholder content so we can confirm UI/UX

--- assets/functions/menu.php
+++ assets/functions/menu.php
@@ -17,25 +17,25 @@ function disciple_tools_top_nav_desktop() {
 
         // User is multiplier or role of higher position
         $html .= '<li><a href="'.home_url( '/' ).'">Dashboard</a></li>';
-        $html .= '<li><a href="'.home_url( '/' ).'contacts/">Contacts</a></li>';
-        $html .= '<li><a href="'.home_url( '/' ).'groups/">Groups</a></li>';
+        $html .= '<li><a href="'.home_url( '/contacts/' ).'">Contacts</a></li>';
+        $html .= '<li><a href="'.home_url( '/groups/' ).'">Groups</a></li>';
 
 
     } elseif ( user_can( get_current_user_id(), 'read_progress' ) ) {
 
-        $html .= '<li><a href="'.home_url( '/' ).'about-us/">About Us</a></li>';
-        $html .= '<li><a href="'.home_url( '/' ).'prayer/">Prayer Guide</a></li>';
-        $html .= '<li><a href="'.home_url( '/' ).'progress/">Project Updates</a></li>';
-        $html .= '<li><a href="'.home_url( '/' ).'settings/">Settings</a></li>';
+        $html .= '<li><a href="'.home_url( '/about-us/' ).'">About Us</a></li>';
+        $html .= '<li><a href="'.home_url( '/prayer/' ).'">Prayer Guide</a></li>';
+        $html .= '<li><a href="'.home_url( '/progress/' ).'">Project Updates</a></li>';
+        $html .= '<li><a href="'.home_url( '/settings/' ).'">Settings</a></li>';
 
 
     } elseif ( user_can( get_current_user_id(), 'read_prayer' )) {
 
         /* user is prayer supporter */
 
-        $html .= '<li><a href="'.home_url( '/' ).'about-us/">About Us</a></li>';
-        $html .= '<li><a href="'.home_url( '/' ).'prayer/">Prayer Guide</a></li>';
-        $html .= '<li><a href="'.home_url( '/' ).'settings/">Settings</a></li>';
+        $html .= '<li><a href="'.home_url( '/about-us/' ).'">About Us</a></li>';
+        $html .= '<li><a href="'.home_url( '/prayer/' ).'">Prayer Guide</a></li>';
+        $html .= '<li><a href="'.home_url( '/settings/' ).'">Settings</a></li>';
 
 
     } else {
@@ -62,8 +62,8 @@ function disciple_tools_top_nav_mobile() {
     $html = '';
 
     if( user_can( get_current_user_id(), 'access_contacts' ) ) {
-        $html .= '<li><a href="' . home_url( '/' ) . 'groups/"><i class="fi-torsos-all"></i></a></li>';
-        $html .= '<li><a href="' . home_url( '/' ) . 'contacts/"><i class="fi-address-book"></i></a></li>';
+        $html .= '<li><a href="' . home_url( '/groups/' ) . '"><i class="fi-torsos-all"></i></a></li>';
+        $html .= '<li><a href="' . home_url( '/contacts/' ) . '"><i class="fi-address-book"></i></a></li>';
         $html .= '<li><a href="' . home_url( '/' ) . '"><i class="fi-graph-pie"></i></a></li>';
     }
 
@@ -93,16 +93,14 @@ function disciple_tools_off_canvas_nav() {
 
     // User is multiplier or role of higher position
 //        $html .= '<li><a href="'.home_url( '/' ).'">Dashboard</a></li>';
-        $html .= '<li><a href="'.home_url( '/' ).'contacts/">Contacts</a></li>';
-        $html .= '<li><a href="'.home_url( '/' ).'groups/">Groups</a></li>';
-        $html .= '<li><a href="'.home_url( '/' ).'locations/">Locations</a></li>';
-        $html .= '<li><a href="'.home_url( '/' ).'workers/">Workers</a></li>';
-        $html .= '<li><a href="'.home_url( '/' ).'peoplegroups/">People Groups</a></li>';
+        $html .= '<li><a href="'.home_url( '/contacts/' ).'">Contacts</a></li>';
+        $html .= '<li><a href="'.home_url( '/groups/' ).'">Groups</a></li>';
+        $html .= '<li><a href="'.home_url( '/locations/' ).'">Locations</a></li>';
+        $html .= '<li><a href="'.home_url( '/workers/' ).'">Workers</a></li>';
         $html .= '<li>&nbsp;</li>';
-        $html .= '<li><a href="'.home_url( '/' ).'metrics/">Metrics</a></li>';
-        $html .= '<li><a href="'.home_url( '/' ).'resources/">Resources</a></li>';
-        $html .= '<li><a href="'.home_url( '/' ).'notifications/">Notifications</a></li>';
-        $html .= '<li><a href="'.home_url( '/' ).'settings/">Settings</a></li>';
+        $html .= '<li><a href="'.home_url( '/metrics/' ).'">Metrics</a></li>';
+        $html .= '<li><a href="'.home_url( '/notifications/' ).'">Notifications</a></li>';
+        $html .= '<li><a href="'.home_url( '/settings/' ).'">Settings</a></li>';
         $html .= '<li>&nbsp;</li>';
 
         $html .= '
@@ -120,10 +118,10 @@ function disciple_tools_off_canvas_nav() {
         $html .= '<div class="menu-centered">
                     <ul class="vertical medium-horizontal menu sticky is-stuck is-at-top" data-accordion-menu>';
 
-        $html .= '<li><a href="'.home_url( '/' ).'about-us/">About Us</a></li>';
-        $html .= '<li><a href="'.home_url( '/' ).'prayer/">Prayer Guide</a></li>';
-        $html .= '<li><a href="'.home_url( '/' ).'project/">Project Updates</a></li>';
-        $html .= '<li><a href="'.home_url( '/' ).'settings/">Settings</a></li>';
+        $html .= '<li><a href="'.home_url( '/about-us/' ).'">About Us</a></li>';
+        $html .= '<li><a href="'.home_url( '/prayer/' ).'">Prayer Guide</a></li>';
+        $html .= '<li><a href="'.home_url( '/project/' ).'">Project Updates</a></li>';
+        $html .= '<li><a href="'.home_url( '/settings/' ).'">Settings</a></li>';
 
         $html .= '</ul></div>';
 
@@ -134,9 +132,9 @@ function disciple_tools_off_canvas_nav() {
         $html .= '<div class="menu-centered">
                     <ul class="vertical medium-horizontal menu sticky is-stuck is-at-top" data-accordion-menu>';
 
-        $html .= '<li><a href="'.home_url( '/' ).'about-us/">About Us</a></li>';
-        $html .= '<li><a href="'.home_url( '/' ).'prayer/">Prayer Guide</a></li>';
-        $html .= '<li><a href="'.home_url( '/' ).'settings/">Settings</a></li>';
+        $html .= '<li><a href="'.home_url( '/about-us/' ).'">About Us</a></li>';
+        $html .= '<li><a href="'.home_url( '/prayer/' ).'">Prayer Guide</a></li>';
+        $html .= '<li><a href="'.home_url( '/settings/' ).'">Settings</a></li>';
 
         $html .= '</ul></div>';
 
@@ -145,10 +143,18 @@ function disciple_tools_off_canvas_nav() {
     }
 
     echo $html;
-    
+
 }
 
+/**
+ * Class Off_Canvas_Menu_Walker
+ */
 class Off_Canvas_Menu_Walker extends Walker_Nav_Menu {
+    /**
+     * @param string $output
+     * @param int    $depth
+     * @param array  $args
+     */
     function start_lvl( &$output, $depth = 0, $args = array() ) {
         $indent = str_repeat( "\t", $depth );
         $output .= "\n$indent<ul class=\"vertical menu\">\n";
@@ -189,7 +195,14 @@ function disciple_tools_footer_links_fallback() {
     /* You can put a default here if you like */
 }
 
-// Add Foundation active class to menu
+/**
+ *  Add Foundation active class to menu
+ *
+ * @param $classes
+ * @param $item
+ *
+ * @return array
+ */
 function required_active_nav_class( $classes, $item ) {
     if ( $item->current == 1 || $item->current_item_ancestor == true ) {
         $classes[] = 'active';
