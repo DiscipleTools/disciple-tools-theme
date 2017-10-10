4c94e21de0fef4ef375b680c7a4b90f7531f9d72
Burning Sun <burntsun@users.noreply.github.com>
2017-10-10 14:12:27 +0100

    PHPCS fixes in menu.php

--- assets/functions/menu.php
+++ assets/functions/menu.php
@@ -10,39 +10,33 @@ register_nav_menus(
 // The Top Menu
 function disciple_tools_top_nav_desktop() {
 
-    $html = '';
-
     if( user_can( get_current_user_id(), 'access_contacts' ) ) {
-
-
         // User is multiplier or role of higher position
-        $html .= '<li><a href="'.home_url( '/' ).'">Dashboard</a></li>';
-        $html .= '<li><a href="'.home_url( '/contacts/' ).'">Contacts</a></li>';
-        $html .= '<li><a href="'.home_url( '/groups/' ).'">Groups</a></li>';
+        ?>
+        <li><a href="<?php echo esc_url( home_url( '/' ) ); ?>">Dashboard</a></li>
+        <li><a href="<?php echo esc_url( home_url( '/contacts/' ) ); ?>">Contacts</a></li>
+        <li><a href="<?php echo esc_url( home_url( '/groups/' ) ); ?>">Groups</a></li>
 
-
+        <?php
     } elseif ( user_can( get_current_user_id(), 'read_progress' ) ) {
+        ?>
+        <li><a href="<?php echo esc_url( home_url( '/about-us/' ) ); ?>">About Us</a></li>
+        <li><a href="<?php echo esc_url( home_url( '/prayer/' ) ); ?>">Prayer Guide</a></li>
+        <li><a href="<?php echo esc_url( home_url( '/progress/' ) ); ?>">Project Updates</a></li>
+        <li><a href="<?php echo esc_url( home_url( '/settings/' ) ); ?>">Settings</a></li>
 
-        $html .= '<li><a href="'.home_url( '/about-us/' ).'">About Us</a></li>';
-        $html .= '<li><a href="'.home_url( '/prayer/' ).'">Prayer Guide</a></li>';
-        $html .= '<li><a href="'.home_url( '/progress/' ).'">Project Updates</a></li>';
-        $html .= '<li><a href="'.home_url( '/settings/' ).'">Settings</a></li>';
-
-
+        <?php
     } elseif ( user_can( get_current_user_id(), 'read_prayer' )) {
-
         /* user is prayer supporter */
+        ?>
+        <li><a href="<?php echo esc_url( home_url( '/about-us/' ) ); ?>">About Us</a></li>
+        <li><a href="<?php echo esc_url( home_url( '/prayer/' ) ); ?>">Prayer Guide</a></li>
+        <li><a href="<?php echo esc_url( home_url( '/settings/' ) ); ?>">Settings</a></li>
 
-        $html .= '<li><a href="'.home_url( '/about-us/' ).'">About Us</a></li>';
-        $html .= '<li><a href="'.home_url( '/prayer/' ).'">Prayer Guide</a></li>';
-        $html .= '<li><a href="'.home_url( '/settings/' ).'">Settings</a></li>';
-
-
+        <?php
     } else {
         /* redirect to registered page */
     }
-
-    echo $html;
 
 
     //    wp_nav_menu(array(
@@ -52,27 +46,24 @@ function disciple_tools_top_nav_desktop() {
     //        'theme_location' => 'main-nav',        			// Where it's located in the theme
     //        'depth' => 5,                                   // Limit the depth of the nav
     //        'fallback_cb' => false,                         // Fallback function (see below)
-    //        'walker' => new Topbar_Menu_Walker()
+    //        'walker' => new DT_Topbar_Menu_Walker()
     //    ));
 
 }
 
 
 function disciple_tools_top_nav_mobile() {
-    $html = '';
-
     if( user_can( get_current_user_id(), 'access_contacts' ) ) {
-        $html .= '<li><a href="' . home_url( '/groups/' ) . '"><i class="fi-torsos-all"></i></a></li>';
-        $html .= '<li><a href="' . home_url( '/contacts/' ) . '"><i class="fi-address-book"></i></a></li>';
-        $html .= '<li><a href="' . home_url( '/' ) . '"><i class="fi-graph-pie"></i></a></li>';
+        ?>
+        <li><a href="<?php echo esc_url( home_url( '/groups/' ) ); ?>"><i class="fi-torsos-all"></i></a></li>
+        <li><a href="<?php echo esc_url( home_url( '/contacts/' ) ); ?>"><i class="fi-address-book"></i></a></li>
+        <li><a href="<?php echo esc_url( home_url( '/' ) ); ?>"><i class="fi-graph-pie"></i></a></li>
+        <?php
     }
-
-    echo $html;
-
 }
 
 // Big thanks to Brett Mason (https://github.com/brettsmason) for the awesome walker
-class Topbar_Menu_Walker extends Walker_Nav_Menu {
+class DT_Topbar_Menu_Walker extends Walker_Nav_Menu {
     function start_lvl( &$output, $depth = 0, $args = array() ) {
         $indent = str_repeat( "\t", $depth );
         $output .= "\n$indent<ul class=\"menu\">\n";
@@ -82,74 +73,78 @@ class Topbar_Menu_Walker extends Walker_Nav_Menu {
 // The Off Canvas Menu
 function disciple_tools_off_canvas_nav() {
 
-    $html = '';
-
     if( user_can( get_current_user_id(), 'access_contacts' ) ) {
+        ?>
+        <ul class="vertical menu sticky is-stuck is-at-top" data-accordion-menu>
 
-        $html .= '<ul class="vertical menu sticky is-stuck is-at-top" data-accordion-menu>';
+            <li><span class="title">Disciple Tools</span></li>
+            <li><hr /></li>
 
-        $html .= '<li><span class="title">Disciple Tools</span></li>
-                    <li><hr /></li>';
-
+        <?php
     // User is multiplier or role of higher position
-//        $html .= '<li><a href="'.home_url( '/' ).'">Dashboard</a></li>';
-        $html .= '<li><a href="'.home_url( '/contacts/' ).'">Contacts</a></li>';
-        $html .= '<li><a href="'.home_url( '/groups/' ).'">Groups</a></li>';
-        $html .= '<li><a href="'.home_url( '/locations/' ).'">Locations</a></li>';
-        $html .= '<li><a href="'.home_url( '/workers/' ).'">Workers</a></li>';
-        $html .= '<li>&nbsp;</li>';
-        $html .= '<li><a href="'.home_url( '/metrics/' ).'">Metrics</a></li>';
-        $html .= '<li><a href="'.home_url( '/notifications/' ).'">Notifications</a></li>';
-        $html .= '<li><a href="'.home_url( '/settings/' ).'">Settings</a></li>';
-        $html .= '<li>&nbsp;</li>';
+//        <li><a href="<   ?php echo esc_url( home_url( '/' ) ); ?    >">Dashboard</a></li>
+        ?>
+            <li><a href="<?php echo esc_url( home_url( '/contacts/' ) ); ?>">Contacts</a></li>
+            <li><a href="<?php echo esc_url( home_url( '/groups/' ) ); ?>">Groups</a></li>
+            <li><a href="<?php echo esc_url( home_url( '/locations/' ) ); ?>">Locations</a></li>
+            <li><a href="<?php echo esc_url( home_url( '/workers/' ) ); ?>">Workers</a></li>
+            <li>&nbsp;</li>
+            <li><a href="<?php echo esc_url( home_url( '/metrics/' ) ); ?>">Metrics</a></li>
+            <li><a href="<?php echo esc_url( home_url( '/notifications/' ) ); ?>">Notifications</a></li>
+            <li><a href="<?php echo esc_url( home_url( '/settings/' ) ); ?>">Settings</a></li>
+            <li>&nbsp;</li>
 
-        $html .= '
+
             <li>
-                <form role="search" method="get" class="search-form" action="'. home_url( '/' ) .'">
-                    <input type="search" class="small" placeholder="' . esc_attr_x( 'Search...', 'disciple_tools' ) . '" value="' . get_search_query() . '" name="s" title="'. esc_attr_x( 'Search for:', 'disciple_tools' ).'" />
-                    <input type="hidden" class=" button small" value="'. esc_attr_x( 'Search', 'disciple_tools' ) .'" />
+                <form role="search" method="get" class="search-form" action="<?php echo esc_url( home_url( '/' ) ); ?>">
+                    <input type="search" class="small" placeholder="<?php echo esc_attr_x( 'Search...', 'disciple_tools' ) ?>" value="<?php echo get_search_query() ?>" name="s" title="<?php echo esc_attr_x( 'Search for:', 'disciple_tools' ) ?>" />
+                    <input type="hidden" class=" button small" value="<?php echo esc_attr_x( 'Search', 'disciple_tools' ) ?>" />
                 </form>
-            </li>';
+            </li>
+       </ul>
 
-        $html .= '</ul>';
+<?php
 
     } elseif ( user_can( get_current_user_id(), 'read_progress' ) ) {
 
-        $html .= '<div class="menu-centered">
-                    <ul class="vertical medium-horizontal menu sticky is-stuck is-at-top" data-accordion-menu>';
+        ?>
+        <div class="menu-centered">
+                    <ul class="vertical medium-horizontal menu sticky is-stuck is-at-top" data-accordion-menu>
 
-        $html .= '<li><a href="'.home_url( '/about-us/' ).'">About Us</a></li>';
-        $html .= '<li><a href="'.home_url( '/prayer/' ).'">Prayer Guide</a></li>';
-        $html .= '<li><a href="'.home_url( '/project/' ).'">Project Updates</a></li>';
-        $html .= '<li><a href="'.home_url( '/settings/' ).'">Settings</a></li>';
+        <li><a href="<?php echo esc_url( home_url( '/about-us/' ) ); ?>">About Us</a></li>
+        <li><a href="<?php echo esc_url( home_url( '/prayer/' ) ); ?>">Prayer Guide</a></li>
+        <li><a href="<?php echo esc_url( home_url( '/project/' ) ); ?>">Project Updates</a></li>
+        <li><a href="<?php echo esc_url( home_url( '/settings/' ) ); ?>">Settings</a></li>
 
-        $html .= '</ul></div>';
+        </ul></div>
+        <?php
 
     } elseif ( user_can( get_current_user_id(), 'read_prayer' )) {
 
         /* user is prayer supporter */
+        ?>
 
-        $html .= '<div class="menu-centered">
-                    <ul class="vertical medium-horizontal menu sticky is-stuck is-at-top" data-accordion-menu>';
+        <div class="menu-centered">
+                    <ul class="vertical medium-horizontal menu sticky is-stuck is-at-top" data-accordion-menu>
 
-        $html .= '<li><a href="'.home_url( '/about-us/' ).'">About Us</a></li>';
-        $html .= '<li><a href="'.home_url( '/prayer/' ).'">Prayer Guide</a></li>';
-        $html .= '<li><a href="'.home_url( '/settings/' ).'">Settings</a></li>';
+        <li><a href="<?php echo esc_url( home_url( '/about-us/' ) ); ?>">About Us</a></li>
+        <li><a href="<?php echo esc_url( home_url( '/prayer/' ) ); ?>">Prayer Guide</a></li>
+        <li><a href="<?php echo esc_url( home_url( '/settings/' ) ); ?>">Settings</a></li>
 
-        $html .= '</ul></div>';
+        </ul></div>
+
+        <?php
 
     } else {
         /* redirect to registered page */
     }
 
-    echo $html;
-
 }
 
 /**
- * Class Off_Canvas_Menu_Walker
+ * Class DT_Off_Canvas_Menu_Walker
  */
-class Off_Canvas_Menu_Walker extends Walker_Nav_Menu {
+class DT_Off_Canvas_Menu_Walker extends Walker_Nav_Menu {
     /**
      * @param string $output
      * @param int    $depth
@@ -203,10 +198,10 @@ function disciple_tools_footer_links_fallback() {
  *
  * @return array
  */
-function required_active_nav_class( $classes, $item ) {
+function dt_required_active_nav_class( $classes, $item ) {
     if ( $item->current == 1 || $item->current_item_ancestor == true ) {
         $classes[] = 'active';
     }
     return $classes;
 }
-add_filter( 'nav_menu_css_class', 'required_active_nav_class', 10, 2 );
+add_filter( 'nav_menu_css_class', 'dt_required_active_nav_class', 10, 2 );
