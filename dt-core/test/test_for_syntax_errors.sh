2c55d0bdcffb00834fe08a44c2937ea4b8e6f2a7
Chris Chasm <chris@chasm.solutions>
2018-01-09 11:16:47 -0700

    Changed depth of functions.php for php 5

--- dt-core/test/test_for_syntax_errors.sh
+++ dt-core/test/test_for_syntax_errors.sh
@@ -3,7 +3,7 @@
 cd "$(dirname "${BASH_SOURCE[0]}")/../"
 
 if [ "$(php -r 'echo version_compare( phpversion(), "7.0", ">=" ) ? 1 : 0;')" != 1 ] ; then
-    php -l functions.php
+    php -l ../functions.php
     exit
 fi
 
