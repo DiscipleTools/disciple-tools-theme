dc1abbd2b36f9b78ae7ba28109c960547b541f60
corsac <corsac@hushmail.com>
2018-01-15 11:32:58 +0100

    Improve add comment box and button. closes #376

--- dt-assets/parts/loop-activity-comment.php
+++ dt-assets/parts/loop-activity-comment.php
@@ -1,15 +1,19 @@
-<div>
-    <textarea dir="auto" rows="4" id="comment-input"></textarea>
 
-    <button id="add-comment-button" class="button loader"><?php esc_html_e( 'Add', 'disciple_tools' )?></button>
-</div>
-<div class="section-selector">
-        <button class="section-button" onclick="display_activity_comment('all')"><?php esc_html_e( 'ALL', 'disciple_tools' )?> | </button>
-        <button class="section-button" onclick="display_activity_comment('comments')"><?php esc_html_e( 'COMMENTS', 'disciple_tools' )?> | </button>
-        <button class="section-button" onclick="display_activity_comment('activity')"><?php esc_html_e( 'ACTIVITY', 'disciple_tools' )?></button>
-        <br>
-<hr>
-</div>
-<div style="overflow-y:scroll" id="comments-wrapper">
+<div class="grid-y">
+    <div class="cell grid-x grid-margin-x" id="add-comment-section">
+        <textarea class="auto cell" dir="auto" rows="4" id="comment-input" placeholder="<?php esc_html_e( "Write comment or note here", 'disciple_tools' ) ?>"></textarea>
+        <div class="shrink cell">
+            <button id="add-comment-button" class="button loader">
+                <img src="<?php echo esc_html( get_template_directory_uri() . '/dt-assets/images/send.svg' ) ?>"/>
+            </button>
+        </div>
+    </div>
+    <div class="section-selector cell">
+            <button class="section-button" onclick="display_activity_comment('all')"><?php esc_html_e( 'ALL', 'disciple_tools' )?> | </button>
+            <button class="section-button" onclick="display_activity_comment('comments')"><?php esc_html_e( 'COMMENTS', 'disciple_tools' )?> | </button>
+            <button class="section-button" onclick="display_activity_comment('activity')"><?php esc_html_e( 'ACTIVITY', 'disciple_tools' )?></button>
+    </div>
+    <div style="overflow-y:scroll" id="comments-wrapper" class="cell">
 
+    </div>
 </div>
