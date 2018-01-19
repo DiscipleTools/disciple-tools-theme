061038a71913c9303d969000f2151d3e1cc283cc
corsac <corsac@hushmail.com>
2018-01-19 10:21:11 +0100

    Fix reference to removed function

--- dt-assets/functions/menu.php
+++ dt-assets/functions/menu.php
@@ -107,11 +107,11 @@ function disciple_tools_off_canvas_nav()
                 <a href="<?php echo esc_url( home_url( '/groups/' ) ); ?>"><?php esc_html_e( "Groups" ); ?></a>
             </li>
 
-            <?php if ( Disciple_Tools_Locations::get_standard_locations_count( 0 ) > 0 ) : ?>
-                <!--<li>
-                    <a href="<?php /*echo esc_url( home_url( '/locations/' ) ); */?>"><?php /*esc_html_e( "Locations" ); */?></a>
-                </li>-->
-            <?php endif; ?>
+<!--            --><?php //if ( Disciple_Tools_Locations::get_standard_locations_count( 0 ) > 0 ) : ?>
+<!--                <!--<li>-->
+<!--                    <a href="--><?php ///*echo esc_url( home_url( '/locations/' ) ); */?><!--">--><?php ///*esc_html_e( "Locations" ); */?><!--</a>-->
+<!--                </li>-->-->
+<!--            --><?php //endif; ?>
 
             <?php if ( dt_get_user_team_members_list( get_current_user_id() ) ) : // check if part of team, if not don't show link ?>
                 <li>
