17898d1cea30a22676ee063fa35623e2279c56f0
Chris Chasm <chris@chasm.solutions>
2018-02-04 20:52:51 +0100

    Protected the foreach against error over empty $groups.
    This error was showing up on a fresh install.

--- dt-core/migrations/0003-group-stats.php
+++ dt-core/migrations/0003-group-stats.php
@@ -2,40 +2,43 @@
 
 class Disciple_Tools_Migration_0003 extends Disciple_Tools_Migration {
     public function up() {
+
         $groups_instance = new Disciple_Tools_Groups();
         $groups = $groups_instance::get_viewable_groups();
-        foreach ( $groups as $group ){
-            $meta_fields = get_post_custom( $group->ID );
-            foreach ( $meta_fields as $meta_key => $meta_value ) {
-                if ( $meta_key == 'group_status' ) {
-                    if ( $meta_value[0] == "active_pre_group" ||
-                         $meta_value[0] == "active_group" ||
-                         $meta_value[0] == "active_church" ){
-                        $new_status = "active";
-                        update_post_meta( $group->ID, "group_status", $new_status );
-                    }
-                    if ( $meta_value[0] == "no_value" ||
-                         $meta_value[0] == "inactive_pre_group" ||
-                         $meta_value[0] == "inactive_group" ||
-                         $meta_value[0] == "inactive_church" ){
-                        $new_status = "inactive";
-                        update_post_meta( $group->ID, "group_status", $new_status );
-                    }
-                    if ( $meta_value[0] == "active_group" ||
-                         $meta_value[0] == "inactive_group" ){
-                        $type = "group";
-                        update_post_meta( $group->ID, "group_type", $type );
-                    }
-                    if ( $meta_value[0] == "active_pre_group" ||
-                         $meta_value[0] == "inactive_pre_group" ||
-                         $meta_value[0] == "no_value" ){
-                        $type = "pre-group";
-                        update_post_meta( $group->ID, "group_type", $type );
-                    }
-                    if ( $meta_value[0] == "active_church" ||
-                         $meta_value[0] == "inactive_church" ){
-                        $type = "church";
-                        update_post_meta( $group->ID, "group_type", $type );
+        if( count( $groups ) > 0 ) {
+            foreach ( $groups as $group ){
+                $meta_fields = get_post_custom( $group->ID );
+                foreach ( $meta_fields as $meta_key => $meta_value ) {
+                    if ( $meta_key == 'group_status' ) {
+                        if ( $meta_value[0] == "active_pre_group" ||
+                             $meta_value[0] == "active_group" ||
+                             $meta_value[0] == "active_church" ){
+                            $new_status = "active";
+                            update_post_meta( $group->ID, "group_status", $new_status );
+                        }
+                        if ( $meta_value[0] == "no_value" ||
+                             $meta_value[0] == "inactive_pre_group" ||
+                             $meta_value[0] == "inactive_group" ||
+                             $meta_value[0] == "inactive_church" ){
+                            $new_status = "inactive";
+                            update_post_meta( $group->ID, "group_status", $new_status );
+                        }
+                        if ( $meta_value[0] == "active_group" ||
+                             $meta_value[0] == "inactive_group" ){
+                            $type = "group";
+                            update_post_meta( $group->ID, "group_type", $type );
+                        }
+                        if ( $meta_value[0] == "active_pre_group" ||
+                             $meta_value[0] == "inactive_pre_group" ||
+                             $meta_value[0] == "no_value" ){
+                            $type = "pre-group";
+                            update_post_meta( $group->ID, "group_type", $type );
+                        }
+                        if ( $meta_value[0] == "active_church" ||
+                             $meta_value[0] == "inactive_church" ){
+                            $type = "church";
+                            update_post_meta( $group->ID, "group_type", $type );
+                        }
                     }
                 }
             }
