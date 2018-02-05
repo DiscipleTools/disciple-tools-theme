ba15ab07b81e8ebdae513715836c7d8e86c3b0e5
Chris Chasm <chris@chasm.solutions>
2018-02-05 15:27:40 +0100

    Added error handling

--- dt-contacts/contacts.php
+++ dt-contacts/contacts.php
@@ -159,7 +159,11 @@ class Disciple_Tools_Contacts extends Disciple_Tools_Posts
         if (get_current_user_id()) {
             $defaults["assigned_to"] = sprintf( "user-%d", get_current_user_id() );
         } else {
-            $defaults["assigned_to"] = sprintf( "user-%d", dt_get_base_user( true ) );
+            $base_id = dt_get_base_user( true );
+            if ( is_wp_error( $base_id ) ) {
+                $base_id = 1; // if base user is corrupted, set it to admin
+            }
+            $defaults["assigned_to"] = sprintf( "user-%d", $base_id );
         }
 
         if (in_array( "dispatcher", $current_roles, true ) || in_array( "marketer", $current_roles, true )) {
@@ -182,10 +186,11 @@ class Disciple_Tools_Contacts extends Disciple_Tools_Posts
             "meta_input"  => $fields,
         ];
 
-//      @todo this seems to be a share check. But this is a new contact creation. Permission to create, should be handled earlier.
-//        if ( isset( $fields["assigned-to"] ) && ( ! get_current_user_id() || $fields["assigned-to"] !== sprintf( "user-%d", get_current_user_id() ) )) {
-//            return new WP_Error( __FUNCTION__, __( "You may not assign a new contact to this user" ), [ 'status' => 403 ] );
-//        }
+        //      @todo this seems to be a share check. But this is a new contact creation. Permission to create, should be handled earlier.
+        //        if ( isset( $fields["assigned-to"] ) && ( ! get_current_user_id() || $fields["assigned-to"] !== sprintf( "user-%d", get_current_user_id() ) )) {
+        //            return new WP_Error( __FUNCTION__, __( "You may not assign a new contact to this user" ), [ 'status' => 403 ] );
+        //        }
+
 
         $post_id = wp_insert_post( $post );
 
