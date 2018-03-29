c221a4f8891f0da7a5c14c34df6066cc79523dce
Chris Chasm <chris@chasm.solutions>
2018-03-29 14:55:49 -0600

    Upgraded the site link system

--- functions.php
+++ functions.php
@@ -374,7 +374,8 @@ else {
             require_once( get_template_directory() . '/dt-workflows/index.php' );
             $this->workflows = Disciple_Tools_Workflows::instance();
 
-            require_once( get_template_directory() . '/dt-core/admin/site-link-system.php' );
+            require_once( get_template_directory() . '/dt-core/admin/site-link-post-type.php' );
+            Site_Link_System::instance( 5, 'dashicons-admin-links' );
 
             /**
              * Admin panel
