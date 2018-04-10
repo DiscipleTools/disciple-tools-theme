cd610f44f0497aac8271f8f0e0bf7c2a6c6eca98
Chris Chasm <chris@chasm.solutions>
2018-04-10 10:16:47 -0600

    Cleaned up 403
    https://github.com/DiscipleTools/disciple-tools-theme/issues/475

--- 403.php
+++ 403.php
@@ -20,12 +20,11 @@
                             $type = __( "group", "disciple_tools" );
                         }
                         $id = GET_THE_ID();
-                        echo sprintf( esc_html__( 'Sorry, you don\'t have permission to view the %1$s with id %2$s.', 'disciple_tools' ), esc_html( $type ), esc_html( $id ) );
-                        echo '<br>';
-                        echo __('Request permission from your administrator.', 'zume');
+                        echo sprintf( esc_html__( 'Sorry, you don\'t have permission to view the %1$s with id %2$s.', 'disciple_tools' ), esc_html( $type ), esc_html( $id ) ) . ' ';
+                        echo esc_html__( 'Request permission from your administrator.', 'zume' );
                         // @todo Add a request for permission form here, which could leave a comment on the record, and notify the owner of the record to share the record.
+                        echo '<p><a href="javascript:history.back(1);">'. esc_html__( 'Back', 'zume' ) .'</a></p>';
                         ?>
-
                     </p>
                 </section> <!-- end article section -->
             </div>
