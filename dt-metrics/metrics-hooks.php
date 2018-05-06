1b40a3d7b3083aead535c04715313c259b33a8c8
Chris Chasm <chris@chasm.solutions>
2018-05-06 09:16:58 -0600

    Added sample data for charts

--- dt-metrics/metrics-hooks.php
+++ dt-metrics/metrics-hooks.php
@@ -25,7 +25,7 @@ class Disciple_Tools_Metrics_Hooks
     public function __construct()
     {
         new Disciple_Tools_Metrics_Personal();
-        new Disciple_Tools_Metrics_Project();
+//        new Disciple_Tools_Metrics_Project(); @todo add back
 
         add_action( 'wp_enqueue_scripts', [ $this, 'enqueue_google' ], 100 );
     }
@@ -42,78 +42,148 @@ class Disciple_Tools_Metrics_Hooks
 
 abstract class Disciple_Tools_Metrics_Hooks_Base
 {
-
-    public function __construct()
-    {
-    }
+    public function __construct() {}
 
 }
 
 class Disciple_Tools_Metrics_Personal extends Disciple_Tools_Metrics_Hooks_Base
 {
-    /**
-     * Add New URL Endpoint
-     *
-     * @see functions.php:213
-     * @param $template_for_url
-     *
-     * @return mixed
-     */
-    public function add_url( $template_for_url ) {
-        $template_for_url['metrics/project'] = 'template-metrics.php';
-        return $template_for_url;
+    public function __construct() {
+        add_filter( 'dt_metrics_top_menu', [ $this, 'add_overview' ], 10 );
+        add_filter( 'dt_metrics_menu_my_contacts', [ $this, 'add_contacts_menu' ], 10 );
+        add_filter( 'dt_metrics_menu_my_groups', [ $this, 'add_groups_menu' ], 10 );
+        add_action( 'wp_enqueue_scripts', [ $this, 'scripts' ], 99 );
+
+        parent::__construct();
     }
 
-    public function menu( $content ) {
+    public function add_contacts_menu( $content ) {
         $content .= '
-            <li><a href="'. site_url( '/metrics/project/' ) .'#dt_overview" onclick="show_zume_project()">' .  esc_html__( 'Project', 'disciple_tools' ) . '</a>
-                <ul class="menu vertical nested is-active">
-                    <li><a href="'. site_url( '/metrics/project/' ) .'#dt_overview" onclick="show_zume_project()">' .  esc_html__( 'Overview', 'disciple_tools' ) . '</a></li>
-                    <li><a href="'. site_url( '/metrics/project/' ) .'#dt_contacts" onclick="show_zume_locations()">' .  esc_html__( 'Contacts', 'disciple_tools' ) . '</a></li>
-                </ul>
-            </li>';
+            <li><a href="'. site_url( '/metrics/' ) .'#my_contacts_progress" onclick="my_contacts_progress()">' .  esc_html__( 'Progress', 'disciple_tools' ) . '</a></li>
+            ';
         return $content;
     }
 
-    /**
-     * Load scripts for the plugin
-     */
+    public function add_groups_menu( $content ) {
+        $content .= '
+            <li><a href="'. site_url( '/metrics/' ) .'#my_groups_progress" onclick="my_groups_progress()">' .  esc_html__( 'Progress', 'disciple_tools' ) . '</a></li>
+            ';
+        return $content;
+    }
+
+    public function add_overview( $content ) {
+        $content .= '
+            <li><a href="'. site_url( '/metrics/' ) .'#overview" onclick="overview()">' .  esc_html__( 'Overview', 'disciple_tools' ) . '</a></li>
+            ';
+        return $content;
+    }
+
     public function scripts() {
         $url_path = trim( parse_url( add_query_arg( array() ), PHP_URL_PATH ), '/' );
 
-        if ( 'metrics/project' === $url_path ) {
-            wp_enqueue_script( 'dt_project_metrics_script', get_stylesheet_directory_uri() . '/dt-metrics/metrics.js', [
+        if ( 'metrics' === $url_path ) {
+            wp_enqueue_script( 'dt_metrics_personal_script', get_stylesheet_directory_uri() . '/dt-metrics/metrics-personal.js', [
                 'jquery',
                 'jquery-ui-core',
             ], filemtime( get_theme_file_path() . '/dt-metrics/metrics-personal.js' ), true );
 
             wp_localize_script(
-                'dt_project_metrics_script', 'dtMetrics', [
+                'dt_metrics_personal_script', 'dtMetricsPersonal', [
                     'root' => esc_url_raw( rest_url() ),
-                    'plugin_uri' => Disciple_Tools::instance()->theme_url,
+                    'plugin_uri' => disciple_tools()->theme_url,
                     'nonce' => wp_create_nonce( 'wp_rest' ),
                     'current_user_login' => wp_get_current_user()->user_login,
                     'current_user_id' => get_current_user_id(),
                     'map_key' => dt_get_option( 'map_key' ),
                     'translations' => [
-                        "zume_project" => __( "Zúme Overview", "dt_zume" ),
-                        "zume_groups" => __( "Zúme Groups", "dt_zume" ),
-                        "zume_people" => __( "Zúme People", "dt_zume" ),
-                        "zume_locations" => __( "Zúme Locations", "dt_zume" ),
-                    ]
+                        "my_contacts_progress" => __( "My Contacts Progress"),
+                        "my_groups_progress" => __( "My Groups Progress"),
+                    ],
+                    'overview' => $this->overview(),
+                    'my_contacts' => $this->my_contacts(),
+                    'my_groups' => $this->my_groups(),
                 ]
             );
         }
     }
 
-    public function __construct() {
-        add_filter( 'dt_templates_for_urls', [ $this, 'add_url' ] );
-        add_filter( 'dt_metrics_menu', [ $this, 'menu' ], 1 );
-        add_action( 'wp_enqueue_scripts', [ $this, 'scripts' ], 999 );
+    public function overview() {
+        return [
+            'translations' => [
+                'title' => __('Overview'),
+            ],
+            'hero_stats' => [
+                'total_contacts' => 100,
+                'total_groups' => 20,
+            ],
+            'contacts_progress' => [
+                [ 'Step', 'Contacts', ['role' => 'annotation' ] ],
+                [ 'Contact Attempt Needed', 10, 10 ],
+                [ 'Contact Attempted', 10, 10 ],
+                [ 'Contact Established', 10, 10 ],
+                [ 'First Meeting Scheduled', 10, 10 ],
+                [ 'First Meeting Complete', 10, 10 ],
+                [ 'Ongoing Meetings', 10, 10 ],
+                [ 'Being Coached', 23, 23 ],
+            ],
+            'groups_progress' => [
+                [ 'Step', 'Contacts', ['role' => 'annotation' ] ],
+                [ 'Contact Attempt Needed', 10, 10 ],
+                [ 'Contact Attempted', 10, 10 ],
+                [ 'Contact Established', 10, 10 ],
+                [ 'First Meeting Scheduled', 10, 10 ],
+                [ 'First Meeting Complete', 40, 40 ],
+                [ 'Ongoing Meetings', 50, 50 ],
+                [ 'Being Coached', 23, 23 ],
+            ],
+        ];
+    }
 
-        parent::__construct();
+    public function my_contacts() {
+        return [
+            'translations' => [
+                'title' => __('My Contacts'),
+            ],
+            'hero_stats' => [
+                'total_contacts' => 100,
+                'this_month' => 23,
+            ],
+            'progress' => [
+                [ 'Step', 'Contacts', ['role' => 'annotation' ] ],
+                [ 'Contact Attempt Needed', 10, 10 ],
+                [ 'Contact Attempted', 20, 20 ],
+                [ 'Contact Established', 100, 100 ],
+                [ 'First Meeting Scheduled', 10, 10 ],
+                [ 'First Meeting Complete', 10, 10 ],
+                [ 'Ongoing Meetings', 10, 10 ],
+                [ 'Being Coached', 23, 23 ],
+            ],
+        ];
+    }
+
+    public function my_groups() {
+        return [
+            'translations' => [
+                'title' => __('My Groups'),
+            ],
+            'hero_stats' => [
+                'total_groups' => 20,
+                'this_month' => 12,
+            ],
+            'progress' => [
+                [ 'Step', 'Contacts', ['role' => 'annotation' ] ],
+                [ 'Contact Attempt Needed', 10, 10 ],
+                [ 'Contact Attempted', 80, 80 ],
+                [ 'Contact Established', 10, 10 ],
+                [ 'First Meeting Scheduled', 40, 40 ],
+                [ 'First Meeting Complete', 10, 10 ],
+                [ 'Ongoing Meetings', 10, 10 ],
+                [ 'Being Coached', 23, 23 ],
+            ],
+        ];
     }
 }
+
 
 class Disciple_Tools_Metrics_Project extends Disciple_Tools_Metrics_Hooks_Base
 {
---
+++ dt-metrics/metrics-personal.js
@@ -0,0 +1,257 @@
+jQuery(document).ready(function() {
+    console.log( dtMetricsPersonal )
+
+    if( ! window.location.hash || '#overview' === window.location.hash  ) {
+        overview()
+    }
+    if( '#my_contacts_progress' === window.location.hash  ) {
+        my_contacts_progress()
+    }
+    if( '#my_groups_progress' === window.location.hash  ) {
+        my_groups_progress()
+    }
+})
+
+function overview() {
+    "use strict";
+    let chartDiv = jQuery('#chart')
+    let overview = dtMetricsPersonal.overview
+    chartDiv.empty().html(`
+        <span class="section-header">`+ dtMetricsPersonal.overview.translations.title +`</span>
+        <span style="float:right; font-size:1.5em;color:#3f729b;"><a data-open="dt-project-legend"><i class="fi-info"></i></a></span>
+        <div class="medium reveal" id="dt-project-legend" data-reveal>`+ legend() +`<button class="close-button" data-close aria-label="Close modal" type="button">
+                        <span aria-hidden="true">&times;</span>
+                    </button></div>
+        
+        <br><br>
+        <div class="grid-x grid-padding-x grid-padding-y">
+            <div class="cell">
+                <div class="grid-x callout">
+                    <div class="medium-3 cell center">
+                    <h4>Total Contacts<br><span id="total_contacts"></span></h4>
+                    </div>
+                    <div class="medium-3 cell center" style="border-left: 1px solid #ccc">
+                    <h4>Total Groups<br><span id="total_groups"></span></h4>
+                    </div>
+                </div>
+            </div>
+            <div class="cell">
+            <div id="my_contacts_progress" style="height: 500px; margin: 0 1em 1.2em; "></div>
+            </div> 
+            <div class="cell">
+            <div id="my_groups_progress" style="height: 500px; margin: 0 1em 1.2em; "></div>
+            </div>
+        </div>
+        `)
+
+    let hero = overview.hero_stats
+    jQuery('#total_contacts').append( numberWithCommas( hero.total_contacts ) )
+    jQuery('#total_groups').append( numberWithCommas( hero.total_groups ) )
+
+    // build charts
+    google.charts.load('current', {'packages':['corechart', 'bar']});
+
+    google.charts.setOnLoadCallback(drawMyContactsProgress);
+    google.charts.setOnLoadCallback(drawMyGroupsProgress);
+
+    function drawMyContactsProgress() {
+
+        let data = google.visualization.arrayToDataTable( overview.contacts_progress );
+
+        let options = {
+            bars: 'horizontal',
+            chartArea: {
+                left: '25%',
+                top: '0%',
+                width: "80%",
+                height: "90%" },
+            vAxis: {
+                title: 'steps',
+            },
+            hAxis: {
+                title: 'number of contacts',
+            },
+            title: "Number of groups according to their member count",
+            legend: {position: "none"},
+            colors: ['blue'],
+        };
+
+        let chart = new google.visualization.BarChart(document.getElementById('my_contacts_progress'));
+        chart.draw(data, options);
+    }
+
+    function drawMyGroupsProgress() {
+
+        let data = google.visualization.arrayToDataTable( overview.groups_progress );
+
+        let options = {
+            bars: 'horizontal',
+            chartArea: {
+                left: '25%',
+                top: '0%',
+                width: "80%",
+                height: "90%" },
+            vAxis: {
+                title: 'steps',
+            },
+            hAxis: {
+                title: 'number of contacts',
+            },
+            title: "Number of groups according to their member count",
+            legend: {position: "none"},
+            colors: ['green'],
+        };
+
+        let chart = new google.visualization.BarChart(document.getElementById('my_groups_progress'));
+        chart.draw(data, options);
+    }
+}
+
+function my_contacts_progress() {
+    "use strict";
+    let chartDiv = jQuery('#chart')
+    let myContacts = dtMetricsPersonal.my_contacts
+    chartDiv.empty().html(`
+        <span class="section-header">`+ myContacts.translations.title +`</span>
+        <span style="float:right; font-size:1.5em;color:#3f729b;"><a data-open="dt-project-legend"><i class="fi-info"></i></a></span>
+        <div class="medium reveal" id="dt-project-legend" data-reveal>`+ legend() +`<button class="close-button" data-close aria-label="Close modal" type="button">
+                        <span aria-hidden="true">&times;</span>
+                    </button></div>
+        
+        <br><br>
+        <div class="grid-x grid-padding-x grid-padding-y">
+            <div class="cell">
+                <div class="grid-x callout">
+                    <div class="medium-3 cell center">
+                    <h4>Total Contacts<br><span id="total_contacts"></span></h4>
+                    </div>
+                    <div class="medium-3 cell center" style="border-left: 1px solid #ccc">
+                    <h4>This Month<br><span id="this_month"></span></h4>
+                    </div>
+                </div>
+            </div>
+            <div class="cell">
+            <div id="contacts_progress" style="height: 500px; margin: 0 1em 1.2em; "></div>
+            </div>
+        </div>
+        `)
+
+    let hero = myContacts.hero_stats
+    jQuery('#total_contacts').append( numberWithCommas( hero.total_contacts ) )
+
+    // build charts
+    google.charts.load('current', {'packages':['corechart', 'bar']});
+
+    google.charts.setOnLoadCallback(drawMyContactsProgress);
+
+    function drawMyContactsProgress() {
+
+        let data = google.visualization.arrayToDataTable( myContacts.progress );
+
+        let options = {
+            bars: 'horizontal',
+            chartArea: {
+                left: '25%',
+                top: '0%',
+                width: "80%",
+                height: "90%" },
+            vAxis: {
+                title: 'steps',
+            },
+            hAxis: {
+                title: 'number of contacts',
+            },
+            title: "Number of groups according to their member count",
+            legend: {position: "none"},
+            colors: ['blue'],
+        };
+
+        let chart = new google.visualization.BarChart(document.getElementById('contacts_progress'));
+        chart.draw(data, options);
+    }
+}
+
+function my_groups_progress() {
+    "use strict";
+    let chartDiv = jQuery('#chart')
+    let myGroups = dtMetricsPersonal.my_groups
+    chartDiv.empty().html(`
+        <span class="section-header">`+ myGroups.translations.title +`</span>
+        <span style="float:right; font-size:1.5em;color:#3f729b;"><a data-open="dt-project-legend"><i class="fi-info"></i></a></span>
+        <div class="medium reveal" id="dt-project-legend" data-reveal>`+ legend() +`<button class="close-button" data-close aria-label="Close modal" type="button">
+                        <span aria-hidden="true">&times;</span>
+                    </button></div>
+        
+        <br><br>
+        <div class="grid-x grid-padding-x grid-padding-y">
+            <div class="cell">
+                <div class="grid-x callout">
+                    <div class="medium-3 cell center">
+                    <h4>Total Groups<br><span id="total_groups"></span></h4>
+                    </div>
+                    <div class="medium-3 cell center" style="border-left: 1px solid #ccc">
+                    <h4>This Month<br><span id="this_month"></span></h4>
+                    </div>
+                </div>
+            </div>
+            <div class="cell">
+            <div id="my_contacts_progress" style="height: 500px; margin: 0 1em 1.2em; "></div>
+            </div>
+        </div>
+        `)
+
+    let hero = myGroups.hero_stats
+    jQuery('#total_groups').append( numberWithCommas( hero.total_groups ) )
+
+    // build charts
+    google.charts.load('current', {'packages':['corechart', 'bar']});
+
+    google.charts.setOnLoadCallback(drawMyContactsProgress);
+
+    function drawMyContactsProgress() {
+
+        let data = google.visualization.arrayToDataTable( myGroups.progress );
+
+        let options = {
+            bars: 'horizontal',
+            chartArea: {
+                left: '25%',
+                top: '0%',
+                width: "80%",
+                height: "90%" },
+            vAxis: {
+                title: 'steps',
+            },
+            hAxis: {
+                title: 'number of contacts',
+            },
+            title: "Number of groups according to their member count",
+            legend: {position: "none"},
+            colors: ['blue'],
+        };
+
+        let chart = new google.visualization.BarChart(document.getElementById('my_contacts_progress'));
+        chart.draw(data, options);
+    }
+}
+
+function legend() {
+    return `<h2>Chart Legend</h2><hr>
+            <dl>
+            <dt>Registered</dt><dd>Groups or people who have registered on Zumeproject.com</dd>
+            <dt>Engaged</dt><dd>Groups or people who have registered on Zumeproject.com</dd>
+            <dt>Trained</dt><dd>Trained groups and people have been through the entire Zúme training.</dd>
+            <dt>Active</dt><dd>Active groups and people have finished a session in the last 30 days. Active in month charts measure according to the month listed. It is the same 'active' behavior, but broken up into different time units.</dd>
+            <dt>Hours of Training</dt><dd>Hours of completed sessions for groups or people.</dd>
+            <dt>Countries</dt><dd>In the overview page, "Countries" counts number of countries with trained groups.</dd>
+            <dt>Translations</dt><dd>Translations counts the number of translations installed in ZúmeProject.com.</dd>
+            </dl>`
+}
+
+function numberWithCommas(x) {
+    x = x.toString();
+    let pattern = /(-?\d+)(\d{3})/;
+    while (pattern.test(x))
+        x = x.replace(pattern, "$1,$2");
+    return x;
+}
