5069f48dbda95b8cc524d2902c2040e9353f7267
Samuel Twain <joshuapralls@gmail.com>
2018-05-18 09:36:17 +0100

    fixed spacing again

--- dt-notifications/notifications-email.php
+++ dt-notifications/notifications-email.php
@@ -93,7 +93,7 @@ class Disciple_Tools_Notifications_Email extends Disciple_Tools_Async_Task
          */
         // @codingStandardsIgnoreLine
         $id = get_user_by( "email", sanitize_email( $_POST[0]['email'] ) );
-        if( ( metadata_exists( "user", $id->ID, "default_password_nag" ) || metadata_exists( "user", $id->id, "session_tokens" ) ) && wp_unslash( $_POST[ 'action' ] ) == 'dt_async_email_notification' && isset( $_POST[ '_nonce' ] ) && $this->verify_async_nonce( sanitize_key( wp_unslash( $_POST[ '_nonce' ] ) ) ) )  {
+        if ( ( metadata_exists( "user", $id->ID, "default_password_nag" ) || metadata_exists( "user", $id->id, "session_tokens" ) ) && wp_unslash( $_POST['action'] ) == 'dt_async_email_notification' && isset( $_POST['_nonce'] ) && $this->verify_async_nonce( sanitize_key( wp_unslash( $_POST['_nonce'] ) ) ) )  {
 
             // @codingStandardsIgnoreLine
             wp_mail( sanitize_email( $_POST[ 0 ][ 'email' ] ), sanitize_text_field( wp_unslash( $_POST[ 0 ][ 'subject' ] ) ), sanitize_textarea_field( wp_unslash( $_POST[ 0 ][ 'message' ] ) ) );
