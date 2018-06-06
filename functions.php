3fe1dae4786a964202bf6f7256b6299fad13f452
corsac <corsac@hushmail.com>
2018-06-06 12:02:13 +0100

    0.8.2

--- functions.php
+++ functions.php
@@ -152,7 +152,7 @@ else {
              * Prepare variables
              */
             $this->token = 'disciple_tools';
-            $this->version = '0.8.1';
+            $this->version = '0.8.2';
             $this->migration_number = 6;
 
             $this->theme_url = get_template_directory_uri() . '/';
