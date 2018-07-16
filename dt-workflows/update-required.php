b4a5e38d4fd6a5cafcf36a422ced53ad4250b51b
corsac <corsac@hushmail.com>
2018-07-16 17:07:42 +0100

    Update Needed add the assigned to user in the comment

--- dt-workflows/update-required.php
+++ dt-workflows/update-required.php
@@ -60,7 +60,9 @@ class Disciple_Tools_Update_Needed_Async extends Disciple_Tools_Async_Task {
                     esc_sql( $setting["seeker_path"] )
                 ), OBJECT );
                 foreach ( $contacts_need_update as $contact ) {
-                    Disciple_Tools_Contacts::add_comment( $contact->ID, $setting["comment"], false, "comment", 0 );
+                    $user_name = ( "@" . dt_get_assigned_name( $contact->ID, true ) . " " ) ?? "";
+                    $comment = $user_name . $setting["comment"];
+                    Disciple_Tools_Contacts::add_comment( $contact->ID, $comment, false, "comment", 0 );
                     Disciple_Tools_contacts::update_contact( $contact->ID, [ "requires_update" => "yes" ], false );
                 }
             }
