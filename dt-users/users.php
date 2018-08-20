1aca5134345397e2765690e3a7d583c66ed55c15
Samuel Twain <joshuapralls@gmail.com>
2018-08-20 08:38:32 -0400

    took out force comment username change

--- dt-users/users.php
+++ dt-users/users.php
@@ -257,18 +257,6 @@ class Disciple_Tools_Users
         if ( !empty( $args['nickname'] ) && $current_user->display_name != $args['nickname'] ) {
             //TODO CHECK FOR DUB NICKNAMES
 
-            //change old nickname comment values to new values
-            $wpdb->query(
-                $wpdb->prepare(
-                    "
-                    UPDATE $wpdb->comments
-                    SET comment_content = REPLACE(comment_content, %s, %s)
-                    WHERE user_id = %d
-                    ",
-                    $current_user->display_name, $args['nickname'], $current_user->ID
-                )
-            );
-
             //set display name to nickname
             $user_id = wp_update_user( array(
                 'ID' => $args['ID'],
