2e94e1366bc25cf6446e102418b29c093ecde308
Burning Sun <burntsun@users.noreply.github.com>
2018-08-24 14:30:41 +0100

    Test .js files using eslint

--- dt-assets/js/footer-scripts.js
+++ dt-assets/js/footer-scripts.js
@@ -9,10 +9,10 @@ jQuery(document).ready(function() {
     // Remove empty P tags created by WP inside of Accordion and Orbit
     jQuery('.accordion p:empty, .orbit p:empty').remove();
 
-	 // Makes sure last grid item floats left
-	jQuery('.archive-grid .columns').last().addClass( 'end' );
+   // Makes sure last grid item floats left
+  jQuery('.archive-grid .columns').last().addClass( 'end' );
 
-	// Adds Flex Video to YouTube and Vimeo Embeds
+  // Adds Flex Video to YouTube and Vimeo Embeds
   jQuery('iframe[src*="youtube.com"], iframe[src*="vimeo.com"]').each(function() {
     if ( jQuery(this).innerWidth() / jQuery(this).innerHeight() > 1.5 ) {
       jQuery(this).wrap("<div class='widescreen flex-video'/>");
@@ -33,5 +33,5 @@ jQuery(document).ready(function($) {
 
     // This adds padding to the top of the offcanvas menu, if the wp admin bar is turned on for the profile.
     $( "#wpadminbar" ).addClass( "add" );
-    
+
 });
