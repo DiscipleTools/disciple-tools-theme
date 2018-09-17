ea3b82908b9a24620be980e4bb4d59ac7fa609f0
corsacca <corsacca@gmail.com>
2018-09-17 17:13:19 +0100

    phpcs and translations

--- dt-assets/parts/merge-details.php
+++ dt-assets/parts/merge-details.php
@@ -2,25 +2,20 @@
 
 ( function () {
 
-    function array_contains(array $array, string $string) {
-        foreach($array as $item) {
-            if(is_array($item)) {
-                if(array_contains($item, $string)) {
+    function dt_array_contains( array $array, string $string ) {
+        foreach ( $array as $item ) {
+            if ( is_array( $item ) ) {
+                if ( dt_array_contains( $item, $string ) ) {
                     return true;
                 }
-            } elseif(!is_object($item)) {
-                if(strpos($item, $string) !== false) {
+            } elseif ( !is_object( $item ) ) {
+                if ( strpos( $item, $string ) !== false ) {
                     return true;
                 }
             }
         }
         return false;
     }
-
-    $contact = Disciple_Tools_Contacts::get_contact( get_the_ID(), true );
-    $channel_list = Disciple_Tools_Contacts::get_channel_list();
-    $current_user = wp_get_current_user();
-    $contact_fields = Disciple_Tools_Contacts::get_contact_fields();
 
     ?>
     <div class="reveal" id="merge-dupe-edit" style="border-radius:10px; padding:0px; padding-bottom:20px; border: 1px solid #3f729b;;" data-reveal>
@@ -35,14 +30,14 @@
         <form action="<?php echo esc_url( site_url( '/contacts/' . get_the_ID() ) ); ?>" id='form-dismiss' method="POST">
             <input type='hidden' name='dt_contact_nonce' value="<?php echo esc_attr( wp_create_nonce() ); ?>">
             <input type='hidden' name='dismiss' value='1'/>
-            <input type='hidden' id="dismiss-id" name='id' value='<?php echo esc_html( $value ); ?>'/>
+            <input type='hidden' id="dismiss-id" name='id' value=''/>
             <input type='hidden' name='currentId' value='<?php echo get_the_ID(); ?>'/>
             <input type='submit' style='display: none' value='Dismiss'/>
         </form>
         <form action="<?php echo esc_url( site_url( '/contacts/' . get_the_ID() ) ); ?>" id='form-unsure' method="POST">
             <input type='hidden' name='dt_contact_nonce' value="<?php echo esc_attr( wp_create_nonce() ); ?>">
             <input type='hidden' name='unsure' value='1'/>
-            <input type='hidden' id="unsure-id" name='id' value='<?php echo esc_html( $value ); ?>'/>
+            <input type='hidden' id="unsure-id" name='id' value=''/>
             <input type='hidden' name='currentId' value='<?php echo get_the_ID(); ?>'/>
             <input type='submit' style='display: none' value='Unsure'/>
         </form>
@@ -53,10 +48,10 @@
         loadDuplicates();
         function loadDuplicates() {
             var template_dir = "<?php echo esc_url( get_template_directory_uri() ); ?>";
-            var site_url = "<?php echo esc_url(site_url());?>";
+            var site_url = "<?php echo esc_url( site_url() );?>";
             var contact_id = "<?php echo get_the_ID(); ?>";
 
-            $display_fields = $(".display-fields");
+            let $display_fields = $(".display-fields");
             $display_fields.append("<h4 style='text-align: center; font-size: 1.25rem; font-weight: bold; padding:10px 0px 0px; margin-bottom: 0px;'>Original Contact</h4>");
             API.get_post('contact', contact_id).done(function(response) {
                 var contact = response;
@@ -111,9 +106,16 @@
 
                 if(dupes.length) {
                     $duplicates = $display_fields.find('#duplicates_list');
-                    $duplicates.append("<h4 style='text-align: center; font-size: 1.25rem; font-weight: bold; padding:20px 0px 0px; margin-bottom: 0px;'>Possible Duplicates</h4>");
+                    $duplicates.append("<h4 style='text-align: center; font-size: 1.25rem; font-weight: bold; padding:20px 0px 0px; margin-bottom: 0px;'><?php esc_html_e( "Possible Duplicates", 'disciple_tools' ) ?></h4>");
 
-                    var unsure_dismiss_html = "<div style='display:inline-block; width: 100%;'><form method='POST' id='form-unsure-dismiss' action='"+site_url+"/contacts/"+contact_id+"'><input type='hidden' name='dt_contact_nonce' value='<?php echo esc_attr(wp_create_nonce());?>'/><input type='hidden' name='id' value='<?php echo get_the_ID(); ?>'/><a style='float: right; margin-left: 10%;' onclick='dismiss_all();'>Dismiss All</a><a style='float: right;' onclick='unsure_all();'>Unsure All</a><input type='submit' id='unsure-dismiss-submit' style='display: none;' value='submit'/></form></div>";
+
+                    var unsure_dismiss_html = `<div style='display:inline-block; width: 100%;'>
+                        <form method='POST' id='form-unsure-dismiss' action='${site_url}/contacts/"+contact_id+"'>
+                        <input type='hidden' name='dt_contact_nonce' value='<?php echo esc_attr( wp_create_nonce() );?>'/>
+                        <input type='hidden' name='id' value='<?php echo get_the_ID(); ?>'/>
+                        <a style='float: right; margin-left: 10%;' onclick='dismiss_all();'><?php esc_html_e( "Dismiss All", 'disciple_tools' ) ?></a>
+                        <a style='float: right;' onclick='unsure_all();'><?php esc_html_e( "Unsure All", 'disciple_tools' ) ?></a>
+                        <input type='submit' id='unsure-dismiss-submit' style='display: none;' value='submit'/></form></div>`;
 
                     $duplicates.append(unsure_dismiss_html);
 
@@ -150,11 +152,19 @@
                                     }
                                 });
                             });
-                            duplicate_contact_html.append("<button class='mergelinks' onclick='$(\"#dismiss-id\").val(" + id + "); $(\"#form-dismiss input[type=submit]\").click();' style='float: right; padding-left: 10%;'><a>Dismiss</a></button>");
+                            duplicate_contact_html.append("<button class='mergelinks' onclick='$(\"#dismiss-id\").val(" + id + "); $(\"#form-dismiss input[type=submit]\").click();' style='float: right; padding-left: 10%;'><a><?php esc_html_e( "Dismiss", 'disciple_tools' ) ?></a></button>");
 
-                            duplicate_contact_html.append("<button class='mergelinks' onclick='$(\"#unsure-id\").val(" + id + "); $(\"#form-unsure input[type=submit]\").click();' style='float: right; padding-left: 10%;'><a>Unsure</a></button>");
+                            duplicate_contact_html.append("<button class='mergelinks' onclick='$(\"#unsure-id\").val(" + id + "); $(\"#form-unsure input[type=submit]\").click();' style='float: right; padding-left: 10%;'><a><?php esc_html_e( "Unsure", 'disciple_tools' ) ?></a></button>");
 
-                            duplicate_contact_html.append("<form action='"+site_url+"/contacts/mergedetails' method='post'><input type='hidden' name='dt_contact_nonce' value='<?php echo esc_attr(wp_create_nonce()); ?>'/><input type='hidden' name='currentid' value='"+contact_id+"'/><input type='hidden' name='dupeid' value='"+id+"'/><button type='submit' style='float:right; padding-left: 10%;'><a>Merge</a></button></form>");
+                            duplicate_contact_html.append(`<form action='${site_url}/contacts/mergedetails' method='post'>
+                                <input type='hidden' name='dt_contact_nonce' value='<?php echo esc_attr( wp_create_nonce() ); ?>'/>
+                                <input type='hidden' name='currentid' value='${contact_id}'/>
+                                <input type='hidden' name='dupeid' value='${id}'/>
+                                <button type='submit' style='float:right; padding-left: 10%;'>
+                                    <a><?php esc_html_e( 'Merge', 'disciple_tools' ) ?></a>
+                                </button>
+                                </form>`
+                            );
 
                             $duplicates.append(duplicate_contact_html);
                         });
@@ -163,7 +173,7 @@
 
                 if(duplicates.unsure) {
                     $unsure = $display_fields.find('#unsure_list');
-                    $unsure.append("<h4 style='text-align: center; font-size: 1.25rem; font-weight: bold; padding: 20px 0px 0px; margin-bottom: 0px;'>Unsure Duplicates</h4>");
+                    $unsure.append("<h4 style='text-align: center; font-size: 1.25rem; font-weight: bold; padding: 20px 0px 0px; margin-bottom: 0px;'><?php esc_html_e( "Unsure Duplicates", 'disciple_tools' ) ?></h4>");
 
                     $.each(duplicates.unsure, function(key, id) {
                         API.get_post('contact', id).done(function(dupe) {
