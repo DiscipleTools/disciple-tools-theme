f31d0d9a936e9c163d4a3b9c761b4a068808d7a7
Chris Chasm <chris@chasm.solutions>
2018-09-24 16:01:57 -0600

    Added network folder and base files

---
+++ dt-network/network-endpoints.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Rest Endpoints for the network feature of Disciple Tools
+ *
+ * @class      Disciple_Tools_Notifications
+ * @version    0.1.0
+ * @since      0.1.0
+ * @package    Disciple_Tools
+ */
+
+if ( !defined( 'ABSPATH' ) ) {
+    exit; // Exit if accessed directly
+}
+
---
+++ dt-network/network.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Core functions to power the network features of Disciple tools
+ *
+ * @class      Disciple_Tools_Notifications
+ * @version    0.1.0
+ * @since      0.1.0
+ * @package    Disciple_Tools
+ */
+
+if ( !defined( 'ABSPATH' ) ) {
+    exit; // Exit if accessed directly
+}
