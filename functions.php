84e47a02b425888b542662f123a619aaddb55477
Chris Chasm <chris@chasm.solutions>
2018-09-25 06:25:16 -0600

    Removed old commented code for old api keys and old activation/deactivation registrations.

--- functions.php
+++ functions.php
@@ -29,13 +29,7 @@ if ( version_compare( phpversion(), '7.0', '<' ) ) {
 }
 else {
 
-
-    /**
-     * Activation, Deactivation, and Multisite
-     */
-//    register_activation_hook( __FILE__, 'dt_activate' );
-//    register_deactivation_hook( __FILE__, 'dt_deactivate' );
-
+    
     /**
      * Adds the Disciple_Tools Class and runs database and roles version checks.
      */
@@ -176,11 +170,6 @@ else {
             require_once( get_template_directory() . '/dt-core/admin/config-site-defaults.php' ); // Force required site configurations
             require_once( get_template_directory() . '/dt-core/wp-async-request.php' ); // Async Task Processing
 
-            /**
-             * Rest API Support
-             */
-//            require_once( get_template_directory() . '/dt-core/integrations/class-api-keys.php' ); // API keys for remote access // @todo remove and integrate with Site_Link_System
-//            $this->api_keys = Disciple_Tools_Api_Keys::instance(); // @todo remove and integrate with Site_Link_System
 
             require_once( get_template_directory() . '/dt-core/admin/restrict-rest-api.php' ); // sets authentication requirement for rest end points. Disables rest for pre-wp-4.7 sites.
             require_once( get_template_directory() . '/dt-core/admin/restrict-xml-rpc-pingback.php' ); // protect against DDOS attacks.
