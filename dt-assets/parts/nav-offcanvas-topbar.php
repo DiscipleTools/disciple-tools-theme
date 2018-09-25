9bcd7ef64c7a7a582fed8d2b7c5a6e38dcff7a16
Chris Chasm <chris@chasm.solutions>
2018-09-25 10:04:30 -0600

    Added admin to the offscreen menu and changed permissions from 'read' to 'manage_dt' for admin link

--- dt-assets/parts/nav-offcanvas-topbar.php
+++ dt-assets/parts/nav-offcanvas-topbar.php
@@ -59,7 +59,7 @@
 
                         <li><a href="<?php echo esc_url( site_url( '/' ) ) . 'settings/'; ?>"><?php esc_html_e( 'Settings', 'disciple_tools' )?></a></li>
 
-                        <?php if ( user_can( get_current_user_id(), 'read' ) ) : ?>
+                        <?php if ( user_can( get_current_user_id(), 'manage_dt' ) ) : ?>
                             <li><a href="<?php echo esc_url( get_admin_url() ); ?>"><?php esc_html_e( "Admin" ); ?></a></li>
                         <?php endif; ?>
                         <li><a href="https://disciple-tools.readthedocs.io/en/latest/index.html" target="_blank" rel="noreferrer"><?php esc_html_e( 'Help' ) ?></a></li>
