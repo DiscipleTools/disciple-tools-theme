710c6d79a5341b599f3354c58a17bef4dd37e709
Chris Chasm <chris@chasm.solutions>
2018-12-14 12:51:15 -0700

    fixed capitalization inconsistency

--- dt-assets/parts/modals/modal-new-contact.php
+++ dt-assets/parts/modals/modal-new-contact.php
@@ -1,20 +1,20 @@
 <div class="reveal" id="create-contact-modal" data-reveal data-reset-on-close>
 
-    <p class="lead"><?php esc_html_e( 'Create contact', 'disciple_tools' )?></p>
+    <p class="lead"><?php esc_html_e( 'Create Contact', 'disciple_tools' )?></p>
 
     <form class="js-create-contact hide-after-contact-create">
         <label for="title">
-            <?php esc_html_e( "Name of contact", "disciple_tools" ); ?>
+            <?php esc_html_e( "Name of Contact", "disciple_tools" ); ?>
         </label>
         <input name="title" type="text" placeholder="<?php esc_html_e( "Name", "disciple_tools" ); ?>" required aria-describedby="name-help-text">
         <p class="help-text" id="name-help-text"><?php esc_html_e( "This is required", "disciple_tools" ); ?></p>
 
         <div style="text-align: center">
-            <button class="button loader js-create-contact-button" type="submit"><?php esc_html_e( "Create contact", "disciple_tools" ); ?></button>
+            <button class="button loader js-create-contact-button" type="submit"><?php esc_html_e( "Create Contact", "disciple_tools" ); ?></button>
         </div>
     </form>
 
-    <p class="reveal-after-contact-create" style="display: none"><?php esc_html_e( "contact Created", 'disciple_tools' ) ?>: <span id="new-contact-link"></span></p>
+    <p class="reveal-after-contact-create" style="display: none"><?php esc_html_e( "Contact Created", 'disciple_tools' ) ?>: <span id="new-contact-link"></span></p>
 
 
     <div class="grid-x">
@@ -26,14 +26,14 @@
             if ( is_singular( "contacts" )){
                 esc_html_e( 'Return to Contact', 'disciple_tools' );
             } elseif ( is_singular( "contacts" )){
-                esc_html_e( 'Return to contact', 'disciple_tools' );
+                esc_html_e( 'Return to Contact', 'disciple_tools' );
             } else {
                 esc_html_e( 'Return', 'disciple_tools' );
             }
             ?>
         </button>
         <a class="button reveal-after-contact-create" id="go-to-contact" style="display: none">
-            <?php esc_html_e( 'Edit new contact', 'disciple_tools' )?>
+            <?php esc_html_e( 'Edit New Contact', 'disciple_tools' )?>
         </a>
         <button class="close-button" data-close aria-label="Close modal" type="button">
             <span aria-hidden="true">&times;</span>
