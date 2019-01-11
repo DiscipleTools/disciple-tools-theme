e75c5cefb93e231a6c6524a1e5798debe972d92d
Chris Chasm <chris@chasm.solutions>
2019-01-11 11:21:31 -0700

    - Notes on purpose of functions added - is_multisite() wrapper added for the functions page.

--- dt-core/multisite.php
+++ dt-core/multisite.php
@@ -3,31 +3,45 @@
  * Multi-site specific configurations
  */
 
-function dt_network_site_url_to_specific_blog( $url, $path, $scheme ){
-    if ( is_multisite() ) {
+if ( is_multisite() ) {
+    /**
+     * Makes sure the login link presented on the wp-activate.php page is to the subdomain, not the root domain.
+     *
+     * @param $url
+     * @param $path
+     * @param $scheme
+     *
+     * @return string
+     */
+    function dt_network_site_url_to_specific_blog( $url, $path, $scheme ){
+
         global $pagenow;
         if ( 'wp-activate.php' === substr( $pagenow, 0, 15 ) ) {
             $url = site_url();
         }
-    }
-    return $url;
-}
-add_filter('network_site_url', 'dt_network_site_url_to_specific_blog', 3, 999 );
 
-function dt_custom_activate_head(){
-    ?>
-    <style>
-        .wp-activate-container {
-            width: 80%;
-            margin: 0 auto;
-            padding: 2em;
-            color:black;
-        }
-        .wp-activate-container h2 {
-            color:black;
-        }
-    </style>
-    <script></script>
-    <?php
+        return $url;
+    }
+    add_filter('network_site_url', 'dt_network_site_url_to_specific_blog', 3, 999 );
+
+    /**
+     * Adds some simple styling to the head of the wp-activate.php page
+     */
+    function dt_custom_activate_head(){
+        ?>
+        <style>
+            .wp-activate-container {
+                width: 80%;
+                margin: 0 auto;
+                padding: 2em;
+                color:black;
+            }
+            .wp-activate-container h2 {
+                color:black;
+            }
+        </style>
+        <script></script>
+        <?php
+    }
+    add_action( 'activate_wp_head', 'dt_custom_activate_head' );
 }
-add_action( 'activate_wp_head', 'dt_custom_activate_head' );
