64240e0f5dc2f8e52b24af0052fde90030f41e44
Chris Chasm <chris@chasm.solutions>
2019-04-18 08:49:31 -0600

    Removed the multisite filters to the migration, now that the strategy has moved back to a per site installation.

--- dt-mapping/migrations/0002-add-table-geonames-to-db.php
+++ dt-mapping/migrations/0002-add-table-geonames-to-db.php
@@ -5,64 +5,62 @@ class DT_Mapping_Module_Migration_0002 extends DT_Mapping_Module_Migration {
      * @throws \Exception
      */
     public function up() {
-        $ms_migration_number = false;
-        if ( is_multisite() ) {
-            $ms_migration_number = (int) get_site_option( 'dt_mapping_module_multisite_migration_number', true );
+        global $wpdb;
+        $table = $wpdb->prefix . 'dt_geonames';
+        $file = 'dt_geonames.tsv';
+        $expected = 48851;
+
+        // TEST for expected tables\
+        $wpdb->query( "SHOW TABLES LIKE '$table'" );
+        if ( $wpdb->num_rows < 1 ) {
+            error_log( 'Failed to find ' . $table );
+            dt_write_log( $wpdb->num_rows );
+            dt_write_log( $wpdb );
+            throw new Exception( 'Failed to find ' . $table );
         }
 
-        if ( ! is_multisite() || $ms_migration_number < 2 ) {
-            global $wpdb;
-            $table = $wpdb->prefix . 'dt_geonames';
-            $file = 'dt_geonames.tsv';
-            $expected = 48851;
+        // CHECK IF DB INSTALLED
+        $rows = (int) $wpdb->get_var( "SELECT count(*) FROM $table" );
+        if ( $rows >= $expected ) {
+            /* Test if database is already created */
+            error_log( 'database already installed' );
+        } elseif ( $rows < $expected ) {
+            $wpdb->query( "TRUNCATE $table" );
 
-            // TEST for expected tables\
-            $wpdb->query( "SHOW TABLES LIKE '$table'" );
-            if ( $wpdb->num_rows < 1 ) {
-                error_log( 'Failed to find ' . $table );
-                dt_write_log( $wpdb->num_rows );
-                dt_write_log( $wpdb );
-                throw new Exception( 'Failed to find ' . $table );
+            // TEST for presence of source files
+            $dir = wp_upload_dir();
+            $uploads_dir = trailingslashit( $dir['basedir'] );
+            if ( ! file_exists( $uploads_dir . "geonames/" . $file ) ) {
+                error_log( 'Failed to find ' . $file );
+                throw new Exception( 'Failed to find ' . $file );
             }
 
-            // CHECK IF DB INSTALLED
+            // LOAD geonames data
+            $wpdb->query( "
+                LOAD DATA LOCAL INFILE '{$uploads_dir}geonames/{$file}'
+                INTO TABLE $table
+                FIELDS TERMINATED BY '\t'
+                LINES TERMINATED BY '\n'
+                (geonameid,name,asciiname,alternatenames,latitude,longitude,feature_class,feature_code,country_code,cc2,admin1_code,admin2_code,admin3_code,admin4_code,population,elevation,dem,timezone,modification_date,parent_id,country_geonameid,admin1_geonameid,admin2_geonameid,admin3_geonameid,level,alt_name,alt_name_changed,alt_population,is_custom_location)
+                " );
+
+            // TEST
             $rows = (int) $wpdb->get_var( "SELECT count(*) FROM $table" );
-            if ( $rows >= $expected ) {
-                /* Test if database is already created */
-                error_log( 'database already installed' );
-            } elseif ( $rows < $expected ) {
-                $wpdb->query( "TRUNCATE $table" );
-
-                // TEST for presence of source files
-                $dir = wp_upload_dir();
-                $uploads_dir = trailingslashit( $dir['basedir'] );
-                if ( ! file_exists( $uploads_dir . "geonames/" . $file ) ) {
-                    error_log( 'Failed to find ' . $file );
-                    throw new Exception( 'Failed to find ' . $file );
+            if ( $rows === $expected ) {
+                error_log( 'success install of geonames data' );
+                if ( is_multisite() ) {
+                    update_site_option( 'dt_mapping_module_multisite_migration_number', 2 );
                 }
+            } elseif ( $rows > $expected ) {
+                error_log( 'success, but additional records found' );
+                if ( is_multisite() ) {
+                    update_site_option( 'dt_mapping_module_multisite_migration_number', 2 );
+                }
+            } elseif ( $rows < $expected ) {
+                 // fail over install
+                dt_write_log($wpdb->last_error );
 
-                // LOAD geonames data
-                $wpdb->query( "
-                    LOAD DATA LOCAL INFILE '{$uploads_dir}geonames/{$file}'
-                    INTO TABLE $table
-                    FIELDS TERMINATED BY '\t'
-                    LINES TERMINATED BY '\n'
-                    (geonameid,name,asciiname,alternatenames,latitude,longitude,feature_class,feature_code,country_code,cc2,admin1_code,admin2_code,admin3_code,admin4_code,population,elevation,dem,timezone,modification_date,parent_id,country_geonameid,admin1_geonameid,admin2_geonameid,admin3_geonameid,level,alt_name,alt_name_changed,alt_population,is_custom_location)
-                    " );
 
-                // TEST
-                $rows = (int) $wpdb->get_var( "SELECT count(*) FROM $table" );
-                if ( $rows === $expected ) {
-                    error_log( 'success install of geonames data' );
-                    if ( is_multisite() ) {
-                        update_site_option( 'dt_mapping_module_multisite_migration_number', 2 );
-                    }
-                } elseif ( $rows > $expected ) {
-                    error_log( 'success, but additional records found' );
-                    if ( is_multisite() ) {
-                        update_site_option( 'dt_mapping_module_multisite_migration_number', 2 );
-                    }
-                } elseif ( $rows < $expected ) {
 
 //                    $file_location = $uploads_dir . "geonames/" . $file;
 //                    $fp = fopen('mydata.csv', 'r');
@@ -78,14 +76,10 @@ class DT_Mapping_Module_Migration_0002 extends DT_Mapping_Module_Migration {
 //
 //                    fclose($fp);
 
-
-
-
-                    error_log( 'fail. missing minimum records expected. ' . $expected . ' expected. ' . $rows . ' found.' );
-                    throw new Exception( 'fail. missing minimum records expected. ' . $expected . ' expected. ' . $rows . ' found.' );
-                } // failed to find records.
-            }
-        } // un-migrated
+                error_log( 'fail. missing minimum records expected. ' . $expected . ' expected. ' . $rows . ' found.' );
+                throw new Exception( 'fail. missing minimum records expected. ' . $expected . ' expected. ' . $rows . ' found.' );
+            } // failed to find records.
+        }
     }
 
     public function down() {
