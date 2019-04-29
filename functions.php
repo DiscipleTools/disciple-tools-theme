e71b0ffce9d3021f9713e8e99015ab6b6360eb88
Chris Chasm <chris@chasm.solutions>
2019-04-29 17:19:45 -0600

    Moved geocode api

--- functions.php
+++ functions.php
@@ -290,7 +290,7 @@ else {
             require_once( get_template_directory() . '/dt-mapping/mapping-module-config.php' ); // configuration file for mapping module
             DT_Mapping_Module_Config::instance();
             require_once( get_template_directory() . '/dt-mapping/mapping.php' ); // load for mapping module
-            require_once( get_template_directory() . '/dt-mapping/geocoders/google-geocode-api.php' );
+
 
             // dt-locations
             require_once( get_template_directory() . '/dt-locations/locations-post-type.php' );
