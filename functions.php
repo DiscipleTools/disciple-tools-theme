39fb074e612053b36b428f3f4893db7a5d040809
corsacca <corsacca@gmail.com>
2019-06-06 15:40:31 +0100

    0.21.2

--- functions.php
+++ functions.php
@@ -141,7 +141,7 @@ else {
              * Prepare variables
              */
             $this->token = 'disciple_tools';
-            $this->version = '0.21.1';
+            $this->version = '0.21.2';
             $this->migration_number = 24;
 
 
