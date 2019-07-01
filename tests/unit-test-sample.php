82424adcf1209e96eb99fd4ccf16999d6a96c52a
corsacca <corsacca@gmail.com>
2019-07-01 16:07:15 +0100

    Tests: clean up sample test.

--- tests/unit-test-sample.php
+++ tests/unit-test-sample.php
@@ -17,15 +17,4 @@ class SampleTest extends WP_UnitTestCase {
         // Replace this with some actual testing code.
         $this->assertTrue( true );
     }
-
-    public static function tearDownAfterClass(){
-//        global $wpdb;
-//        $wpdb->query( "DROP TABLE IF EXISTS $wpdb->dt_activity_log" );
-//        $wpdb->query( "DROP TABLE IF EXISTS $wpdb->dt_geonames" );
-//        $wpdb->query( "DROP TABLE IF EXISTS $wpdb->dt_notifications" );
-//        $wpdb->query( "DROP TABLE IF EXISTS $wpdb->dt_reports" );
-//        $wpdb->query( "DROP TABLE IF EXISTS $wpdb->dt_reportmeta" );
-//        $wpdb->query( "DROP TABLE IF EXISTS $wpdb->dt_reportmeta" );
-//        $wpdb->query( "DROP TABLE IF EXISTS $wpdb->dt_share" );
-    }
 }
