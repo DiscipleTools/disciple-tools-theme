583a14599f0ae7ebbc9273e7eb1cfb4c747da369
Kensley Lewis <klpsupport@gmail.com>
2019-11-14 21:03:19 -0500

    - Allow Content Fiter panels to open and close - Set all panels to be closed by default - Allow optional callbacks

--- dt-assets/js/footer-scripts.js
+++ dt-assets/js/footer-scripts.js
@@ -1,20 +1,54 @@
-jQuery(document).foundation();
 /*
 These functions make sure WordPress
 and Foundation play nice together.
 */
+jQuery(document).foundation();
 
-jQuery(document).ready(function() {
+
+function contactFilterAccordion() {
+  /**
+   * Managing the Contact Filters Accordion
+   * Helpful Resource Guides
+   * https://foundation.zurb.com/sites/docs/javascript.html
+   * https://foundation.zurb.com/sites/docs/javascript.html#programmatic-use
+   * https://foundation.zurb.com/sites/docs/accordion-menu.html
+   * https://foundation.zurb.com/sites/docs/v/5.5.3/javascript.html
+   * https://www.sitepoint.com/foundation-6-menu-component/
+   */
+
+  //Set speed and expansion options for the Contact Filter accordion
+  var $accordion = new Foundation.Accordion($('#list-filter-tabs'), {
+    slideSpeed: 100,
+    multiExpand: true,
+    allowAllClosed: true
+  });
+
+  //(optional) set Contact Filter accordion panels to all be closed by default
+  jQuery('#list-filter-tabs').find('.accordion-item.is-active').removeClass('is-active').find('.accordion-content').css({ 'display': "" });
+
+  //(optional) set a callback when a panel opens
+  $('#list-filter-tabs').on('down.zf.accordion menu', function () {});
+
+  //(optional) set a callback when a panel closes
+  $('#list-filter-tabs').on('up.zf.accordion menu', function () {});
+
+}
+
+
+jQuery(document).ready(function () {
+
+  //initialize Contact Filters accordion function
+  contactFilterAccordion();
 
   // Remove empty P tags created by WP inside of Accordion and Orbit
   jQuery('.accordion p:empty, .orbit p:empty').remove();
 
   // Makes sure last grid item floats left
-  jQuery('.archive-grid .columns').last().addClass( 'end' );
+  jQuery('.archive-grid .columns').last().addClass('end');
 
   // Adds Flex Video to YouTube and Vimeo Embeds
-  jQuery('iframe[src*="youtube.com"], iframe[src*="vimeo.com"]').each(function() {
-    if ( jQuery(this).innerWidth() / jQuery(this).innerHeight() > 1.5 ) {
+  jQuery('iframe[src*="youtube.com"], iframe[src*="vimeo.com"]').each(function () {
+    if (jQuery(this).innerWidth() / jQuery(this).innerHeight() > 1.5) {
       jQuery(this).wrap("<div class='widescreen flex-video'/>");
     } else {
       jQuery(this).wrap("<div class='flex-video'/>");
