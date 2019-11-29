adb8af75ed1d578d101e824699ac470620d4d7dc
corsacca <corsacca@gmail.com>
2019-11-29 13:16:29 +0100

    Add quick contact create to relations, baptisms and coaching typeaheads.

--- dt-assets/parts/modals/modal-new-group.php
+++ dt-assets/parts/modals/modal-new-group.php
@@ -4,23 +4,26 @@
 
     <form class="js-create-group hide-after-group-create">
         <label for="title">
-            <?php esc_html_e( "Name of group", "disciple_tools" ); ?>
+            <?php esc_html_e( "Name of Group", "disciple_tools" ); ?>
         </label>
         <input name="title" type="text" placeholder="<?php esc_html_e( "Name", "disciple_tools" ); ?>" required aria-describedby="name-help-text">
-        <p class="help-text" id="name-help-text"><?php esc_html_e( "This is required", "disciple_tools" ); ?></p>
 
-        <div style="text-align: center">
+        <div>
             <button class="button loader js-create-group-button" type="submit"><?php esc_html_e( "Create Group", "disciple_tools" ); ?></button>
+            <button class="button button-cancel clear hide-after-group-create" data-close aria-label="Close reveal" type="button">
+                <?php esc_html_e( 'Cancel', 'disciple_tools' )?>
+            </button>
         </div>
     </form>
 
     <p class="reveal-after-group-create" style="display: none"><?php esc_html_e( "Group Created", 'disciple_tools' ) ?>: <span id="new-group-link"></span></p>
 
 
+    <hr class="reveal-after-group-create" style="display: none">
     <div class="grid-x">
-        <button class="button button-cancel clear hide-after-group-create" data-close aria-label="Close reveal" type="button">
-            <?php esc_html_e( 'Cancel', 'disciple_tools' )?>
-        </button>
+        <a class="button reveal-after-group-create" id="go-to-group" style="display: none">
+            <?php esc_html_e( 'Edit new Group', 'disciple_tools' )?>
+        </a>
         <button class="button reveal-after-group-create button-cancel clear" data-close type="button" id="create-group-return" style="display: none">
             <?php
             if ( is_singular( "contacts" )){
@@ -32,9 +35,6 @@
             }
             ?>
         </button>
-        <a class="button reveal-after-group-create" id="go-to-group" style="display: none">
-            <?php esc_html_e( 'Edit new Group', 'disciple_tools' )?>
-        </a>
         <button class="close-button" data-close aria-label="Close modal" type="button">
             <span aria-hidden="true">&times;</span>
         </button>
