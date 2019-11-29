c9c2b73c8960160212fcf869beab6282623d1bf1
Mike Allbutt <yo@ur.id.au>
2019-11-28 20:34:53 -0600

    duplicate translatable; zume to disciple_tools; removed commented out from css
    made duplicate translatable; replaced zume to disciple_tools; removed comment in css (which greneracted a new min.css file)

--- template-view-duplicates.php
+++ template-view-duplicates.php
@@ -19,7 +19,7 @@ $dt_duplicates = wp_unslash( $dt_contacts->get_all_duplicates() );
 
             <main id="main" class="large-12 medium-12 cell" role="main">
                 <div class="bordered-box">
-                    <h3><?php esc_html_e( 'Duplicate Contacts', 'zume' ) ?><button class="help-button float-right" data-section="duplicates-template-help-text">
+                    <h3><?php esc_html_e( 'Duplicate Contacts', 'disciple_tools' ) ?><button class="help-button float-right" data-section="duplicates-template-help-text">
                         <img class="help-icon" src="<?php echo esc_html( get_template_directory_uri() . '/dt-assets/images/help.svg' ) ?>"/>
                     </button></h3>
 
@@ -29,7 +29,7 @@ $dt_duplicates = wp_unslash( $dt_contacts->get_all_duplicates() );
                         foreach ( $dt_duplicates as $dt_id => $dt_duplicate ) {
                             if ($dt_duplicate['count'] <= 0) { continue; }
                             echo "<form name='merge_".esc_html( $dt_id )."' method='POST' action='".esc_html( site_url() )."/contacts/".esc_html( $dt_id )."'><input type='hidden' name='dt_contact_nonce' value='".esc_attr( wp_create_nonce() )."'/></form><tr id='".esc_html( $dt_id )."'><td><a>".esc_html( $dt_duplicate['name'] )."</a></td><td>
-                            <a>".esc_html( $dt_duplicate['count'] )." ".esc_html( 'duplicates' )."</a></td></tr>";
+                            <a>".esc_html( $dt_duplicate['count'] )." ".esc_html( 'duplicates', 'disciple_tools' )."</a></td></tr>";
                         }
                         ?>
                     </table>
