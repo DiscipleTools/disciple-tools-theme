0c34cf2a20daa18e1ad176b20a33ac8f6f185ec9
Mike Allbutt <yo@ur.id.au>
2019-12-02 06:55:53 -0600

    New favicons and header code and removal of old system

---
+++ dt-assets/favicons/README.md
@@ -0,0 +1,23 @@
+# Your Favicon Package
+
+This package was generated with [RealFaviconGenerator](https://realfavicongenerator.net/) [v0.16](https://realfavicongenerator.net/change_log#v0.16)
+
+## Install instructions
+
+To install this package:
+
+Extract this package in <code>&lt;web site&gt;/wp-content/themes/disciple-tools-theme/dt-assets/favicons/</code>. If your site is <code>http://www.example.com</code>, you should be able to access a file named <code>http://www.example.com/wp-content/themes/disciple-tools-theme/dt-assets/favicons/favicon.ico</code>.
+
+Insert the following code in the `head` section of your pages:
+
+    <link rel="apple-touch-icon" sizes="180x180" href="/wp-content/themes/disciple-tools-theme/dt-assets/favicons/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="/wp-content/themes/disciple-tools-theme/dt-assets/favicons/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="/wp-content/themes/disciple-tools-theme/dt-assets/favicons/favicon-16x16.png">
+    <link rel="manifest" href="/wp-content/themes/disciple-tools-theme/dt-assets/favicons/site.webmanifest">
+    <link rel="mask-icon" href="/wp-content/themes/disciple-tools-theme/dt-assets/favicons/safari-pinned-tab.svg" color="#3f729b">
+    <link rel="shortcut icon" href="/wp-content/themes/disciple-tools-theme/dt-assets/favicons/favicon.ico">
+    <meta name="msapplication-TileColor" content="#3f729b">
+    <meta name="msapplication-config" content="/wp-content/themes/disciple-tools-theme/dt-assets/favicons/browserconfig.xml">
+    <meta name="theme-color" content="#3f729b">
+
+*Optional* - Check your favicon with the [favicon checker](https://realfavicongenerator.net/favicon_checker)
--- header.php
+++ header.php
@@ -14,30 +14,19 @@
 
         <!-- If Site Icon isn't set in customizer -->
         <?php if ( ! function_exists( 'has_site_icon' ) || ! has_site_icon() ) { ?>
-            <link rel="apple-touch-icon-precomposed" sizes="57x57" href="<?php echo esc_url( get_template_directory_uri() ); ?>/dt-assets/images/favicon/apple-touch-icon-57x57.png">
-            <link rel="apple-touch-icon-precomposed" sizes="114x114" href="<?php echo esc_url( get_template_directory_uri() ); ?>/dt-assets/images/favicon/apple-touch-icon-114x114.png">
-            <link rel="apple-touch-icon-precomposed" sizes="72x72" href="<?php echo esc_url( get_template_directory_uri() ); ?>/dt-assets/images/favicon/apple-touch-icon-72x72.png">
-            <link rel="apple-touch-icon-precomposed" sizes="144x144" href="<?php echo esc_url( get_template_directory_uri() ); ?>/dt-assets/images/favicon/apple-touch-icon-144x144.png">
-            <link rel="apple-touch-icon-precomposed" sizes="60x60" href="<?php echo esc_url( get_template_directory_uri() ); ?>/dt-assets/images/favicon/apple-touch-icon-60x60.png">
-            <link rel="apple-touch-icon-precomposed" sizes="120x120" href="<?php echo esc_url( get_template_directory_uri() ); ?>/dt-assets/images/favicon/apple-touch-icon-120x120.png">
-            <link rel="apple-touch-icon-precomposed" sizes="76x76" href="<?php echo esc_url( get_template_directory_uri() ); ?>/dt-assets/images/favicon/apple-touch-icon-76x76.png">
-            <link rel="apple-touch-icon-precomposed" sizes="152x152" href="<?php echo esc_url( get_template_directory_uri() ); ?>/dt-assets/images/favicon/apple-touch-icon-152x152.png">
-            <link rel="icon" type="image/png" href="<?php echo esc_url( get_template_directory_uri() ); ?>/dt-assets/images/favicon/favicon-196x196.png" sizes="196x196">
-            <link rel="icon" type="image/png" href="<?php echo esc_url( get_template_directory_uri() ); ?>/dt-assets/images/favicon/favicon-96x96.png" sizes="96x96">
-            <link rel="icon" type="image/png" href="<?php echo esc_url( get_template_directory_uri() ); ?>/dt-assets/images/favicon/favicon-32x32.png" sizes="32x32">
-            <link rel="icon" type="image/png" href="<?php echo esc_url( get_template_directory_uri() ); ?>/dt-assets/images/favicon/favicon-16x16.png" sizes="16x16">
-            <link rel="icon" type="image/png" href="<?php echo esc_url( get_template_directory_uri() ); ?>/dt-assets/images/favicon/favicon-128.png" sizes="128x128">
-            <meta name="application-name" content="DT">
-            <meta name="msapplication-TileColor" content="#FFFFFF">
-            <meta name="msapplication-TileImage" content="<?php echo esc_url( get_template_directory_uri() ); ?>/dt-assets/images/favicon/mstile-144x144.png">
-            <meta name="msapplication-square70x70logo" content="<?php echo esc_url( get_template_directory_uri() ); ?>/dt-assets/images/favicon/mstile-70x70.png">
-            <meta name="msapplication-square150x150logo" content="<?php echo esc_url( get_template_directory_uri() ); ?>/dt-assets/images/favicon/mstile-150x150.png">
-            <meta name="msapplication-wide310x150logo" content="<?php echo esc_url( get_template_directory_uri() ); ?>/dt-assets/images/favicon/mstile-310x150.png">
-            <meta name="msapplication-square310x310logo" content="<?php echo esc_url( get_template_directory_uri() ); ?>/dt-assets/images/favicon/mstile-310x310.png">
+
+            <link rel="apple-touch-icon" sizes="180x180" href="<?php echo esc_url( get_template_directory_uri() ); ?>/dt-assets/favicons/apple-touch-icon.png">
+            <link rel="icon" type="image/png" sizes="32x32" href="<?php echo esc_url( get_template_directory_uri() ); ?>/dt-assets/favicons/favicon-32x32.png">
+            <link rel="icon" type="image/png" sizes="16x16" href="<?php echo esc_url( get_template_directory_uri() ); ?>/dt-assets/favicons/favicon-16x16.png">
+            <link rel="manifest" href="<?php echo esc_url( get_template_directory_uri() ); ?>/dt-assets/favicons/site.webmanifest">
+            <link rel="mask-icon" href="<?php echo esc_url( get_template_directory_uri() ); ?>/dt-assets/favicons/safari-pinned-tab.svg" color="#3f729b">
+            <link rel="shortcut icon" href="<?php echo esc_url( get_template_directory_uri() ); ?>/dt-assets/favicons/favicon.ico">
+            <meta name="msapplication-TileColor" content="#3f729b">
+            <meta name="msapplication-config" content="<?php echo esc_url( get_template_directory_uri() ); ?>/dt-assets/favicons/browserconfig.xml">
+            <meta name="theme-color" content="#3f729b">
         <?php } ?>
 
-        <title>
-        <?php
+        <title><?php
         if ( is_single() ) {
             single_post_title( '', true );
         } else if ( is_archive() ){
@@ -45,8 +34,7 @@
         } else {
             bloginfo( 'name' );
         }
-        ?>
-        </title>
+        ?></title>
 
         <?php wp_head(); ?>
 
