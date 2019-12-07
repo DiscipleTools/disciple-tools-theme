7f6c9eed97bcce0567e6a534c213d0c892d591d3
corsacca <corsacca@gmail.com>
2019-12-07 07:09:16 +0100

    Change for string translation.

--- dt-assets/parts/modals/modal-new-group.php
+++ dt-assets/parts/modals/modal-new-group.php
@@ -33,7 +33,7 @@
             ?>
         </button>
         <a class="button reveal-after-group-create" id="go-to-group" style="display: none">
-            <?php esc_html_e( 'Edit new Group', 'disciple_tools' )?>
+            <?php esc_html_e( 'Edit New Group', 'disciple_tools' )?>
         </a>
         <button class="close-button" data-close aria-label="Close modal" type="button">
             <span aria-hidden="true">&times;</span>
