c4285090ed2c20c2f3a40d5cd5364a80d2891c03
corsacca <corsacca@gmail.com>
2019-12-17 22:01:18 +0100

    Fix incorrect database queries

--- dt-contacts/contacts.php
+++ dt-contacts/contacts.php
@@ -384,7 +384,7 @@ class Disciple_Tools_Contacts extends Disciple_Tools_Posts
             $ignore_keys = preg_grep( "/$field/", $ignore );
             $sql = "delete
                 from
-                    wp_postmeta
+                    $wpdb->postmeta
                 where
                     post_id = %d and
                     meta_key like %s";
@@ -1022,7 +1022,7 @@ class Disciple_Tools_Contacts extends Disciple_Tools_Posts
                 select
                     *
                 from
-                    wp_posts p join wp_postmeta m on p.ID = m.post_id
+                    $wpdb->posts p join $wpdb->postmeta m on p.ID = m.post_id
                 where
                     p.post_type = %s and
                     m.meta_key = %s
@@ -1177,8 +1177,8 @@ class Disciple_Tools_Contacts extends Disciple_Tools_Posts
                 select
                     *
                 from
-                    wp_posts p join
-                    wp_postmeta m on p.ID = m.post_id
+                    $wpdb->posts p join
+                    $wpdb->postmeta m on p.ID = m.post_id
                 where
                     ID != %d and
                     (meta_key regexp %s and meta_key not like %s) and
