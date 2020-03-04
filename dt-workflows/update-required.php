391636cc2f8137371e4abeca2556a8260f853325
corsacca <corsacca@gmail.com>
2020-03-04 16:59:32 +0100

    Change Update needed reminders workflow to trigger of of last user activity from the activity log.

--- dt-workflows/update-required.php
+++ dt-workflows/update-required.php
@@ -49,12 +49,11 @@ class Disciple_Tools_Update_Needed_Async extends Disciple_Tools_Async_Task {
                     FROM $wpdb->posts
                     LEFT JOIN $wpdb->postmeta AS mt1 ON ($wpdb->posts.ID = mt1.post_id AND mt1.meta_key = 'requires_update' )
                     LEFT JOIN $wpdb->postmeta AS mt2 ON ( $wpdb->posts.ID = mt2.post_id )
-                    LEFT JOIN $wpdb->postmeta AS mt3 ON ( $wpdb->posts.ID = mt3.post_id )
                     LEFT JOIN $wpdb->postmeta AS mt4 ON ( $wpdb->posts.ID = mt4.post_id )
                     LEFT JOIN $wpdb->postmeta AS contact_type ON ( $wpdb->posts.ID = contact_type.post_id AND contact_type.meta_key = 'type' )
                     WHERE ( mt1.meta_value = '' OR mt1.meta_value = '0' OR mt1.meta_key IS NULL )
                     AND ( mt2.meta_key = 'overall_status' AND mt2.meta_value = %s )
-                    AND ( mt3.meta_key = 'last_modified' AND mt3.meta_value <= %d )
+                    AND %d >= ( SELECT MAX( hist_time ) FROM $wpdb->dt_activity_log WHERE object_id = $wpdb->posts.ID and user_id != 0 ) 
                     AND ( mt4.meta_key = 'seeker_path' AND mt4.meta_value = %s )
                     AND $wpdb->posts.post_type = 'contacts' AND $wpdb->posts.post_status = 'publish'
                     AND $wpdb->posts.ID NOT IN (
@@ -97,10 +96,9 @@ class Disciple_Tools_Update_Needed_Async extends Disciple_Tools_Async_Task {
                     FROM $wpdb->posts
                     LEFT JOIN $wpdb->postmeta AS mt1 ON ($wpdb->posts.ID = mt1.post_id AND mt1.meta_key = 'requires_update' )
                     LEFT JOIN $wpdb->postmeta AS mt2 ON ( $wpdb->posts.ID = mt2.post_id )
-                    LEFT JOIN $wpdb->postmeta AS mt3 ON ( $wpdb->posts.ID = mt3.post_id )
                     WHERE ( mt1.meta_value = '' OR mt1.meta_value = '0' OR mt1.meta_value IS NULL )
                     AND ( mt2.meta_key = 'group_status' AND mt2.meta_value = %s )
-                    AND ( mt3.meta_key = 'last_modified' AND mt3.meta_value <= %d )
+                    AND %d >= ( SELECT MAX( hist_time ) FROM $wpdb->dt_activity_log WHERE object_id = $wpdb->posts.ID and user_id != 0 )
                     AND $wpdb->posts.post_type = 'groups' AND $wpdb->posts.post_status = 'publish'
                     GROUP BY $wpdb->posts.ID ORDER BY $wpdb->posts.post_date DESC LIMIT 0, 50",
                     esc_sql( $setting["status"] ),
