53fb109b2cc8378038e700d31e417da5976d2324
Micah Mills <micahmills@gmail.com>
2020-04-17 10:31:34 +0300

    removes console.log

--- dt-assets/js/comments.js
+++ dt-assets/js/comments.js
@@ -108,8 +108,7 @@ jQuery(document).ready(function($) {
       </div>
     <div class="activity-text">
     <% _.forEach(activity, function(a){
-        if (a.comment){
-          console.log(a); %>
+        if (a.comment){ %>
             <div dir="auto" class="comment-bubble <%- a.comment_ID %>">
               <div class="comment-text" dir=auto><%= a.text.replace(/\\n/g, '</div><div class="comment-text" dir=auto>') /* not escaped on purpose */ %></div>
             <% if ( commentsSettings.google_translate_key !== "" ) { %>
