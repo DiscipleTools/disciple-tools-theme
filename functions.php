bbab16dbc32b350307f21f947a1e288200b04910
corsacca <corsacca@gmail.com>
2020-04-28 12:20:33 +0100

    Bump version 0.29.0. Update translations. Add Dutch, Chinese (simplified) and Chinese (traditional). Fix issue with updating user profile.

--- functions.php
+++ functions.php
@@ -142,7 +142,7 @@ else {
              * Prepare variables
              */
             $this->token = 'disciple_tools';
-            $this->version = '0.28.0';
+            $this->version = '0.29.0';
             $this->migration_number = 29;
 
 
