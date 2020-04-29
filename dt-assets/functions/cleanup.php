02a97a7cb9973623616717a6109146f763167e7d
Chris Chasm <chris@chasm.solutions>
2020-04-29 12:50:18 -0600

    Added multisite password reset functions

--- dt-assets/functions/cleanup.php
+++ dt-assets/functions/cleanup.php
@@ -218,3 +218,36 @@ function dt_disable_emojis_remove_dns_prefetch( $urls, $relation_type ) {
     return $urls;
 }
 
+/**
+ * Force password reset to remain on current site for multi-site installations.
+ */
+if ( is_multisite() ) {
+    add_filter("lostpassword_url", function ($url, $redirect) {
+
+        $args = array( 'action' => 'lostpassword' );
+
+        if ( !empty($redirect) )
+            $args['redirect_to'] = $redirect;
+        return add_query_arg( $args, site_url('wp-login.php') );
+    }, 10, 2);
+    // fixes other password reset related urls
+    add_filter( 'network_site_url', function($url, $path, $scheme) {
+
+        if (stripos($url, "action=lostpassword") !== false)
+            return site_url('wp-login.php?action=lostpassword', $scheme);
+
+        if (stripos($url, "action=resetpass") !== false)
+            return site_url('wp-login.php?action=resetpass', $scheme);
+
+        return $url;
+    }, 10, 3 );
+    // fixes URLs in email that goes out.
+    add_filter("retrieve_password_message", function ($message, $key) {
+        return str_replace(get_site_url(1), get_site_url(), $message);
+    }, 10, 2);
+    // fixes email title
+    add_filter("retrieve_password_title", function($title) {
+        return "[" . wp_specialchars_decode(get_option('blogname'), ENT_QUOTES) . "] Password Reset";
+    });
+}
+
