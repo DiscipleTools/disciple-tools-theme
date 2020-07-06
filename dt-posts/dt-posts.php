817d9747bff14a4a81f11be26d6e49a4123392b4
micahmills <micahmills@gmail.com>
2020-07-06 13:15:38 +0300

    Comments: fix xss issue. (#998)
    * removes escaping from the Javascript so that the PHP gets the unescaped HTML and then applies wp_kses_post() to it to strip out malicious attributes and tags.
    
    * handles historic comment data that is escaped in JS.
    
    * Allowing limited, sanitized HTML in comments
    
    * TravisCI fixes
    
    * Comments: escape JS by building the comment as a text element.

--- dt-posts/dt-posts.php
+++ dt-posts/dt-posts.php
@@ -9,6 +9,19 @@ class DT_Posts extends Disciple_Tools_Posts {
     }
 
     /**
+     * Specifies which HTML tags are permissible in comments.
+     */
+    private static $allowable_comment_tags = array(
+        'a' => array(
+          'href' => array(),
+          'title' => array()
+        ),
+        'br' => array(),
+        'em' => array(),
+        'strong' => array(),
+    );
+
+    /**
      * Get settings on the post type
      *
      * @param string $post_type
@@ -667,7 +680,7 @@ class DT_Posts extends Disciple_Tools_Posts {
         foreach ( $comments as $comment ){
             $comment_data = [
                 'comment_post_ID'      => $post_id,
-                'comment_content'      => $comment,
+                'comment_content'      => wp_kses($comment, self::$allowable_comment_tags),
                 'user_id'              => $user_id,
                 'comment_author'       => $args["comment_author"] ?? $user->display_name,
                 'comment_author_url'   => $args["comment_author_url"] ?? "",
@@ -757,7 +770,7 @@ class DT_Posts extends Disciple_Tools_Posts {
                 "comment_date" => $comment->comment_date,
                 "comment_date_gmt" => $comment->comment_date_gmt,
                 "gravatar" => preg_replace( "/^http:/i", "https:", $url ),
-                "comment_content" => wp_kses_post( $comment->comment_content ),
+                "comment_content" => wp_kses( $comment->comment_content, self::$allowable_comment_tags),
                 "user_id" => $comment->user_id,
                 "comment_type" => $comment->comment_type,
                 "comment_post_ID" => $comment->comment_post_ID
