f5425ddf0d1d63ea06c6cd535ed5c9700e55d469
corsacca <corsacca@gmail.com>
2020-09-03 18:34:25 +0100

    Slightly more accurate transfer bot name redaction.

--- dt-contacts/contacts-transfer.php
+++ dt-contacts/contacts-transfer.php
@@ -420,26 +420,29 @@ function dt_get_comments_with_redacted_user_data( $post_id ) {
     foreach ( $comments as $index => $comment ) {
         $comment_content = $comment->comment_content;
 
-        // replace @mentions with user number
-        preg_match_all( '/\@\[(.*?)\]\((.+?)\)/', $comment_content, $matches );
-        foreach ( $matches[0] as $match_key => $match ){
-            $comment_content = str_replace( $match, '@' . $redacted_note . '_' . $matches[2][$match_key], $comment_content );
-        }
-
         // replace non-@mention references to login names, display names, or user emails
         foreach ( $users as $user ) {
-            if ( ! empty( $user->data->user_login ) ) {
-                $comment_content = str_replace( $user->data->user_login, '(' . $name_note . ')', $comment_content );
+            if ( !empty( $user->data->user_login ) ) {
+                $comment_content = str_replace( ' ' . $user->data->user_login, '(' . $name_note . ')', $comment_content );
+                $comment_content = str_replace( $user->data->user_login . ' ', '(' . $name_note . ')', $comment_content );
             }
-            if ( ! empty( $user->data->display_name ) ) {
-                $comment_content = str_replace( $user->data->display_name, '(' . $name_note . ')', $comment_content );
+            if ( !empty( $user->data->display_name ) ) {
+                $comment_content = str_replace( ' ' . $user->data->display_name, '(' . $name_note . ')', $comment_content );
+                $comment_content = str_replace( $user->data->display_name . ' ', '(' . $name_note . ')', $comment_content );
             }
-            if ( ! empty( $user->data->user_nicename ) ) {
-                $comment_content = str_replace( $user->data->user_nicename, '(' . $name_note . ')', $comment_content );
+            if ( !empty( $user->data->user_nicename ) ) {
+                $comment_content = str_replace( $user->data->user_nicename . ' ', '(' . $name_note . ')', $comment_content );
+                $comment_content = str_replace( ' ' . $user->data->user_nicename, '(' . $name_note . ')', $comment_content );
             }
             if ( ! empty( $user->data->user_email ) ) {
                 $comment_content = str_replace( $user->data->user_email, '(' . $email_note . ')', $comment_content );
             }
+        }
+
+        // replace @mentions
+        preg_match_all( '/@[0-9a-zA-Z](\.?[0-9a-zA-Z])*/', $comment_content, $matches );
+        foreach ( $matches[0] as $match_key => $match ){
+            $comment_content = str_replace( $match, '@' . $name_note, $comment_content );
         }
 
         // replace duplicate notes
