85a3c85eed90ab802c1838cbd20392a78549191b
Chris Chasm <chris@chasm.solutions>
2020-12-04 16:26:35 -0700

    phpcs

--- dt-core/global-functions.php
+++ dt-core/global-functions.php
@@ -330,7 +330,7 @@ if ( ! defined( 'DT_FUNCTIONS_READY' ) ){
         if ( isset( $fields[$field_key]["type"] ) && empty( $fields[$field_key]["custom_display"] ) && empty( $fields[$field_key]["hidden"] ) ) {
             $field_type = $fields[$field_key]["type"];
             $required_tag = ( isset( $fields[$field_key]["required"] ) && $fields[$field_key]["required"] === true ) ? 'required' : '';
-            $allowed_types = apply_filters('dt_render_field_for_display_allowed_types', [ 'key_select', 'multi_select', 'date', 'datetime', 'text', 'number', 'connection', 'location', 'location_meta', 'communication_channel' ] );
+            $allowed_types = apply_filters( 'dt_render_field_for_display_allowed_types', [ 'key_select', 'multi_select', 'date', 'datetime', 'text', 'number', 'connection', 'location', 'location_meta', 'communication_channel' ] );
             if ( !in_array( $field_type, $allowed_types ) ){
                 return;
             }
