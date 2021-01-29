add7065ba641f5be7de77ff0d5994cbe44945dcc
corsacca <corsacca@gmail.com>
2021-01-29 12:07:06 +0100

    travis

--- dt-assets/js/footer-scripts.js
+++ dt-assets/js/footer-scripts.js
@@ -3,8 +3,7 @@ These functions make sure WordPress
 and Foundation play nice together.
 */
 if (Foundation.MediaQuery.current == 'small') {
-  console.log($('.title-bar'));
-  $('.title-bar').removeAttr('data-sticky').removeClass('is-anchored is-at-bottom').attr('style', ''); ;
+  $('.title-bar').removeAttr('data-sticky').removeClass('is-anchored is-at-bottom').attr('style', '');
 }
 
 jQuery(document).foundation();
