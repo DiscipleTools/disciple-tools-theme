0db9eeba6addc47661552413e456ddc65b1224e0
micahmills <micahmills@gmail.com>
2021-02-02 12:17:23 +0300

    Record name viewable when scrolling on mobile. Name tile merged into details bar. (#1129)
    * fixes spacing on contact filters tile
    
    * fixes Church Health SVG size in Firefox
    
    * Made faith milestone buttons in the bulk edit box more legible.
    
    * changed cell width
    
    * added title to mobile second bar.
    
    * change header bars responsiveness
    
    * Moved the items from the contact type tile to the second bar
    
    * removed contact-type tile because the info is now in the second bar
    
    * adds a link to comments section to the comment notification. Fixes #1117
    
    * fixes #1025
    
    * Fixes Travis error
    
    * This doesn't quite fix #1010 but it does add the ellipsis so it doesn't look intentionally cut off.
    
    * oops forgot the style.min.css
    
    * fixes but that caused blue top bar to not appear if the page was loaded scrolled down the page
    
    * added back the color coded icons based on record type
    
    * Made the title in the second bar editable again.
    
    * Possibly a fix for #929
    
    * reversing previous commit, because I pulled it out and put in in a separate PR.
    
    * travis
    
    * rebuild scripts
    
    * hides the #comment-activity-section anchor link on large screens

--- single-template.php
+++ single-template.php
@@ -46,44 +46,25 @@ if ( ! current_user_can( 'access_' . $dt_post_type ) ) {
                     <!-- Requires update block -->
                     <section class="cell small-12 update-needed-notification"
                              style="display: <?php echo esc_html( ( isset( $dt_post['requires_update'] ) && $dt_post['requires_update'] === true ) ? "block" : "none" ) ?> ">
-                        <div class="bordered-box detail-notification-box" style="background-color:#F43636">
-                            <h4>
-                                <img src="<?php echo esc_html( get_template_directory_uri() . '/dt-assets/images/alert-circle-exc.svg' ) ?>"/>
-                                <?php echo esc_html( sprintf( __( 'This %s needs an update.', 'disciple_tools' ), strtolower( $post_settings["label_singular"] ) ) ) ?>
-                            </h4>
-                            <p><?php esc_html_e( 'Please provide an update by posting a comment.', 'disciple_tools' )?></p>
-                        </div>
+                        <a href="#comment-activity-section" class="hide-for-large">
+                            <div class="bordered-box detail-notification-box" style="background-color:#F43636">
+                                <h4>
+                                    <img src="<?php echo esc_html( get_template_directory_uri() . '/dt-assets/images/alert-circle-exc.svg' ) ?>"/>
+                                    <?php echo esc_html( sprintf( __( 'This %s needs an update.', 'disciple_tools' ), strtolower( $post_settings["label_singular"] ) ) ) ?>
+                                </h4>
+                                <p><?php esc_html_e( 'Please provide an update by posting a comment.', 'disciple_tools' )?></p>
+                            </div>
+                        </a>
+                        <div class="bordered-box detail-notification-box show-for-large" style="background-color:#F43636">
+                                <h4>
+                                    <img src="<?php echo esc_html( get_template_directory_uri() . '/dt-assets/images/alert-circle-exc.svg' ) ?>"/>
+                                    <?php echo esc_html( sprintf( __( 'This %s needs an update.', 'disciple_tools' ), strtolower( $post_settings["label_singular"] ) ) ) ?>
+                                </h4>
+                                <p><?php esc_html_e( 'Please provide an update by posting a comment.', 'disciple_tools' )?></p>
+                            </div>
                     </section>
 
                     <?php do_action( 'dt_record_notifications_section', $post_type, $dt_post ); ?>
-
-                    <!-- Name section -->
-                    <?php $type_color = isset( $dt_post['type'], $post_settings["fields"]["type"]["default"][$dt_post['type']["key"]]["color"] ) ?
-                        $post_settings["fields"]["type"]["default"][$dt_post['type']["key"]]["color"] : "#000000";
-                    ?>
-                    <section class="cell" id="contact-type">
-                        <div class="bordered-box detail-notification-box" style="color:black; border: 2px solid <?php echo esc_html( $type_color ); ?>">
-                            <?php $picture = apply_filters( 'dt_record_picture', null, $post_type, $post_id );
-                            if ( !empty( $picture ) ) : ?>
-                                <img src="<?php echo esc_html( $picture )?>" style="height:50px; vertical-align:middle">
-                            <?php else : ?>
-                                <i class="fi-torso large" style="padding-bottom: 1.2rem; color:<?php echo esc_html( $type_color ); ?>"></i>
-                            <?php endif; ?>
-                            <span id="title" style="margin:0 10px" contenteditable="true" class="title dt_contenteditable item-details-header"><?php the_title_attribute(); ?></span>
-                            <?php do_action( 'dt_post_record_name_tagline' ); ?>
-                            <?php if ( isset( $dt_post["type"]["label"] ) ) : ?>
-                            <a data-open="contact-type-modal" style="font-size: 10px"><?php echo esc_html( $dt_post["type"]["label"] ?? "" )?> <?php esc_html_e( 'Record', 'disciple_tools' ); ?></a>
-                            <?php endif; ?>
-                            <span style="font-size: 10px">
-                                <?php echo esc_html( sprintf( _x( 'Created on %s', 'Created on the 21st of August', 'disciple_tools' ), $dt_post["post_date"]["formatted"] ) );
-                                if ( $dt_post["post_author_display_name"] ):
-                                    echo esc_html( ' ' . sprintf( _x( 'by %s', '(record created) by multiplier1', 'disciple_tools' ), $dt_post["post_author_display_name"] ) );
-                                endif; ?>
-                            </span>
-                        </div>
-                    </section>
-
-
 
 
                     <?php do_action( 'dt_record_top_above_details', $post_type, $dt_post ); ?>
