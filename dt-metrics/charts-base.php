31961d1180c8cfa5982bc7992eefc9ffbb1b0424
Chris Chasm <chris@chasm.solutions>
2021-02-08 12:13:10 -0700

    phpcs

--- dt-metrics/charts-base.php
+++ dt-metrics/charts-base.php
@@ -16,23 +16,23 @@ abstract class DT_Metrics_Chart_Base
     /**
      * Disciple_Tools_Counter constructor.
      */
-public function __construct() {
-    $this->base_slug = str_replace( ' ', '', trim( strtolower( $this->base_slug ) ) );
-    $url_path = dt_get_url_path();
+    public function __construct() {
+        $this->base_slug = str_replace( ' ', '', trim( strtolower( $this->base_slug ) ) );
+        $url_path = dt_get_url_path();
 
-    if ( strpos( $url_path, 'metrics' ) === 0 ) {
-        if ( !$this->has_permission() ){
-            return;
-        }
-        add_filter( 'dt_metrics_menu', [ $this, 'base_menu' ], 20 ); //load menu links
+        if ( strpos( $url_path, 'metrics' ) === 0 ) {
+            if ( !$this->has_permission() ){
+                return;
+            }
+            add_filter( 'dt_metrics_menu', [ $this, 'base_menu' ], 20 ); //load menu links
 
-        if ( strpos( $url_path, "metrics/$this->base_slug/$this->slug" ) === 0 ) {
-            add_filter( 'dt_templates_for_urls', [ $this, 'base_add_url' ] ); // add custom URLs
-            add_action( 'wp_enqueue_scripts', [ $this, 'base_scripts' ], 99 );
+            if ( strpos( $url_path, "metrics/$this->base_slug/$this->slug" ) === 0 ) {
+                add_filter( 'dt_templates_for_urls', [ $this, 'base_add_url' ] ); // add custom URLs
+                add_action( 'wp_enqueue_scripts', [ $this, 'base_scripts' ], 99 );
+            }
         }
+
     }
-
-}
 
     /**
      * Build the menu out on the metrics page
@@ -42,23 +42,23 @@ public function __construct() {
      *
      * @return mixed|string
      */
-public function base_menu( $content ) {
-    $line = '<li><a href="'. site_url( '/metrics/'.$this->base_slug.'/' . $this->slug ) . '">' . $this->title . '</a></li>';
+    public function base_menu( $content ) {
+        $line = '<li><a href="'. site_url( '/metrics/'.$this->base_slug.'/' . $this->slug ) . '">' . $this->title . '</a></li>';
 
-    $ref = '<ul class="menu vertical nested" id="' . $this->base_slug . '-menu">';
-    $pos = strpos( $content, $ref );
-    if ( $pos === false ){
-        $content .= '
+        $ref = '<ul class="menu vertical nested" id="' . $this->base_slug . '-menu">';
+        $pos = strpos( $content, $ref );
+        if ( $pos === false ){
+            $content .= '
             <li><a href="'. site_url( '/metrics/'. $this->base_slug .'/'. $this->slug ) .'">'.$this->base_title.'</a>
                 <ul class="menu vertical nested" id="' . $this->base_slug . '-menu">'
                     . $line . '
             </ul></li>';
-    } else {
-        $content = substr_replace( $content, $ref . $line, $pos, strlen( $ref ) );
-    }
+        } else {
+            $content = substr_replace( $content, $ref . $line, $pos, strlen( $ref ) );
+        }
 
-    return $content;
-}
+        return $content;
+    }
 
 
     /**
@@ -68,70 +68,70 @@ public function base_menu( $content ) {
      *
      * @return mixed
      */
-public function base_add_url( $template_for_url ) {
-    $template_for_url["metrics/$this->base_slug/$this->slug"] = 'template-metrics.php';
-    return $template_for_url;
-}
+    public function base_add_url( $template_for_url ) {
+        $template_for_url["metrics/$this->base_slug/$this->slug"] = 'template-metrics.php';
+        return $template_for_url;
+    }
 
-public function base_scripts() {
-    wp_localize_script(
-        'dt_'.$this->base_slug.'_script', 'wpMetricsBase', [
+    public function base_scripts() {
+        wp_localize_script(
+            'dt_'.$this->base_slug.'_script', 'wpMetricsBase', [
             'slug' => $this->base_slug,
             'root' => esc_url_raw( rest_url() ),
             'plugin_uri' => plugin_dir_url( __DIR__ ),
             'nonce' => wp_create_nonce( 'wp_rest' ),
             'current_user_login' => wp_get_current_user()->user_login,
             'current_user_id' => get_current_user_id()
-        ]
-    );
-}
+            ]
+        );
+    }
 
-public function has_permission(){
-    $permissions = $this->permissions;
-    $pass = count( $permissions ) === 0;
-    foreach ( $this->permissions as $permission ){
-        if ( current_user_can( $permission ) ){
-            $pass = true;
+    public function has_permission(){
+        $permissions = $this->permissions;
+        $pass = count( $permissions ) === 0;
+        foreach ( $this->permissions as $permission ){
+            if ( current_user_can( $permission ) ){
+                $pass = true;
+            }
         }
+        return $pass;
     }
-    return $pass;
-}
 
-public function my_list() {
-    $list = Disciple_Tools_Posts::search_viewable_post( 'contacts', [ "assigned_to" => [ "shared", "me" ] ] );
-    if ( is_wp_error( $list ) ) {
-        return [];
+    public function my_list() {
+        $list = Disciple_Tools_Posts::search_viewable_post( 'contacts', [ "assigned_to" => [ "shared", "me" ] ] );
+        if ( is_wp_error( $list ) ) {
+            return [];
+        }
+        $my_list = [];
+        foreach ( $list['posts'] as $post ) {
+            $my_list[] = $post->ID;
+        }
+        return $my_list;
     }
-    $my_list = [];
-    foreach ( $list['posts'] as $post ) {
-        $my_list[] = $post->ID;
-    }
-    return $my_list;
-}
 
-public function my_groups_list() {
-    $list = Disciple_Tools_Posts::search_viewable_post( 'groups', [ "assigned_to" => [ "shared", "me" ] ] );
-    if ( is_wp_error( $list ) ) {
-        return [];
+    public function my_groups_list() {
+        $list = Disciple_Tools_Posts::search_viewable_post( 'groups', [ "assigned_to" => [ "shared", "me" ] ] );
+        if ( is_wp_error( $list ) ) {
+            return [];
+        }
+        $my_list = [];
+        foreach ( $list['posts'] as $post ) {
+            $my_list[] = $post->ID;
+        }
+        return $my_list;
     }
-    $my_list = [];
-    foreach ( $list['posts'] as $post ) {
-        $my_list[] = $post->ID;
-    }
-    return $my_list;
-}
 
-public function _empty_geojson() {
-    return array(
+    public function _empty_geojson() {
+        return array(
         'type' => 'FeatureCollection',
         'features' => []
-    );
-}
+        );
+    }
 
-public function _no_results() {
-    return '<p>'. esc_attr( 'No Results', 'disciple_tools' ) .'</p>';
-}
-public function _circular_structure_error( $wp_error) {
+    public function _no_results() {
+        return '<p>'. esc_attr( 'No Results', 'disciple_tools' ) .'</p>';
+    }
+    public function _circular_structure_error( $wp_error) {
         $link = false;
         $data = $wp_error->get_error_data();
 
