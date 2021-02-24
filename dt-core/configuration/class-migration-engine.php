c3f333bfe45eb4f34f5a27272d430a390abc13d3
Chris Chasm <chris@chasm.solutions>
2021-02-24 20:12:46 +0100

    Enlarge Value Column in the DT_Reports table to handle larger numbers (#1223)
    * Reports: Upgraded the size of the value column from INT(11) to BIGINT (22)
    
    * Reports: Added table variable.

--- dt-core/configuration/class-migration-engine.php
+++ dt-core/configuration/class-migration-engine.php
@@ -12,7 +12,7 @@ if ( !defined( 'ABSPATH' ) ) {
 class Disciple_Tools_Migration_Engine
 {
 
-    public static $migration_number = 39;
+    public static $migration_number = 40;
 
     protected static $migrations = null;
 
