b1214d1e275e117afd937469247203cd23293cb7
squigglybob <nathinabob@gmail.com>
2021-03-26 13:18:50 +0000

    feat: dropdown ready for adding emoji's into it

--- dt-assets/js/comments.js
+++ dt-assets/js/comments.js
@@ -115,12 +115,18 @@ jQuery(document).ready(function($) {
           <% is_Comment = true; %>
             <div dir="auto" class="comment-bubble <%- a.comment_ID %>">
               <div class="comment-text" title="<%- date %>" dir=auto><%= a.text.replace(/\\n/g, '</div><div class="comment-text" dir=auto>') /* not escaped on purpose */ %></div>
+              <button class="icon-button reactions__button" aria-label="Add your reaction" aria-haspopup="menu" role="button" data-toggle="react-to-<%- a.comment_ID %>">
+                <svg class="octicon octicon-smiley" viewBox="0 0 16 16" width="16" height="16" aria-hidden="true"><path fill-rule="evenodd" d="M1.5 8a6.5 6.5 0 1113 0 6.5 6.5 0 01-13 0zM8 0a8 8 0 100 16A8 8 0 008 0zM5 8a1 1 0 100-2 1 1 0 000 2zm7-1a1 1 0 11-2 0 1 1 0 012 0zM5.32 9.636a.75.75 0 011.038.175l.007.009c.103.118.22.222.35.31.264.178.683.37 1.285.37.602 0 1.02-.192 1.285-.371.13-.088.247-.192.35-.31l.007-.008a.75.75 0 111.222.87l-.614-.431c.614.43.614.431.613.431v.001l-.001.002-.002.003-.005.007-.014.019a1.984 1.984 0 01-.184.213c-.16.166-.338.316-.53.445-.63.418-1.37.638-2.127.629-.946 0-1.652-.308-2.126-.63a3.32 3.32 0 01-.715-.657l-.014-.02-.005-.006-.002-.003v-.002h-.001l.613-.432-.614.43a.75.75 0 01.183-1.044h.001z"></path></svg>
+              </button>
+              <div class="dropdown-pane reactions__dropdown" data-position="bottom" data-alignment="right" id="react-to-<%- a.comment_ID %>" data-dropdown>
+                Emoji selection
+              </div>
             </div>
             <% if ( commentsSettings.google_translate_key !== ""  && is_Comment && !has_Comment_ID && activity[0].comment_type !== 'duplicate' ) { %>
               <div class="translation-bubble" dir=auto></div>
             <% } %>
             <p class="comment-controls">
-               <% if ( a.comment_ID ) { %>
+               <% if ( a.is_own_comment ) { %>
                 <% has_Comment_ID = true %>
                   <a class="open-edit-comment" data-id="<%- a.comment_ID %>" data-type="<%- a.comment_type %>" style="margin-right:5px">
                       <img src="${commentsSettings.template_dir}/dt-assets/images/edit-blue.svg">
@@ -332,7 +338,8 @@ jQuery(document).ready(function($) {
         gravatar,
         text:d.object_note || formatComment(d.comment_content),
         comment: !!d.comment_content,
-        comment_ID : d.user_id === commentsSettings.current_user_id ? d.comment_ID : false,
+        comment_ID : d.comment_ID,
+        is_own_comment: d.user_id === commentsSettings.current_user_id,
         comment_type : d.comment_type,
         action: d.action
       }
@@ -359,6 +366,21 @@ jQuery(document).ready(function($) {
         activity: array
       }))
     }
+    document.querySelectorAll('#comments-wrapper [data-toggle]').forEach((element) => {
+      const dropdownId = $(element).data('toggle')
+      const dropdownElement = document.querySelector(`#${dropdownId}`)
+      element.addEventListener('click', (e) => {
+        const style = getComputedStyle(dropdownElement)
+        element.toggleAttribute('open')
+        if (style.visibility === 'hidden') {
+          dropdownElement.style.visibility = 'visible'
+          dropdownElement.style.display = 'block'
+        } else {
+          dropdownElement.style.visibility = 'hidden'
+          dropdownElement.style.display = 'none'
+        }
+      })
+    })
   }
 
   function baptismTimestamptoDate(match, timestamp) {
