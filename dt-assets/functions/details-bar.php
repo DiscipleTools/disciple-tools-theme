e89e07f6bd8c7ecd6fe62714ef0f870819515593
Carl Wuensche <carl@wuensche.ca>
2021-04-07 09:13:43 -0700

    Added icon image titles hover text and translated.

--- dt-assets/functions/details-bar.php
+++ dt-assets/functions/details-bar.php
@@ -41,12 +41,12 @@ function dt_print_details_bar(
                         </div>
                         <div class="cell grid-x shrink center-items">
                             <a class="section-chevron navigation-left" href="javascript:void(0)">
-                                <img width="25" height="20" alt="previous record" src="<?php echo esc_url( get_template_directory_uri() . "/dt-assets/images/chevron_left.svg" ) ?>">
+                                <img width="25" height="20" title="<?php esc_attr_e( 'previous record', 'disciple_tools' ); ?>" src="<?php echo esc_url( get_template_directory_uri() . "/dt-assets/images/chevron_left.svg" ) ?>">
                             </a>
                         </div>
                         <div class="cell shrink center-items">
                             <a href="javascript:void(0)" class="navigation-right section-chevron">
-                                <img width="25" height="20" alt="next record" src="<?php echo esc_url( get_template_directory_uri() . "/dt-assets/images/chevron_right.svg" ) ?>">
+                                <img width="25" height="20" title="<?php esc_attr_e( 'next record', 'disciple_tools' ); ?>" src="<?php echo esc_url( get_template_directory_uri() . "/dt-assets/images/chevron_right.svg" ) ?>">
                             </a>
                         </div>
                         <div class="cell grid-x shrink center-items">
@@ -143,12 +143,12 @@ function dt_print_details_bar(
             <div class="grid-x align-center" style="align-items: center">
                 <div class="cell shrink center-items">
                     <a class="section-chevron navigation-left" href="javascript:void(0)">
-                        <img width="15" height="10" alt="previous record" src="<?php echo esc_url( get_template_directory_uri() . "/dt-assets/images/chevron_left.svg" ) ?>">
+                        <img width="15" height="10" title="<?php esc_attr_e( 'previous record', 'disciple_tools' ); ?>" src="<?php echo esc_url( get_template_directory_uri() . "/dt-assets/images/chevron_left.svg" ) ?>">
                     </a>
                 </div>
                 <div class="cell shrink center-items">
                     <a href="javascript:void(0)" class="navigation-right section-chevron">
-                        <img width="15" height="10" alt="next record" src="<?php echo esc_url( get_template_directory_uri() . "/dt-assets/images/chevron_right.svg" ) ?>">
+                        <img width="15" height="10" title="<?php esc_attr_e( 'next record', 'disciple_tools' ); ?>" src="<?php echo esc_url( get_template_directory_uri() . "/dt-assets/images/chevron_right.svg" ) ?>">
                     </a>
                 </div>
 
