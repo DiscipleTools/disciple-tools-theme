6361d97616d69ac88c7eac3cda610ca9662b3b61
Mike Allbutt <yo@ur.id.au>
2021-05-13 22:07:22 -0500

    issue 1366 text size
    fix #1366

--- dt-assets/functions/details-bar.php
+++ dt-assets/functions/details-bar.php
@@ -97,7 +97,7 @@ function dt_print_details_bar(
                             <?php if ( isset( $dt_post["type"]["label"] ) ) : ?>
                                 <a data-open="contact-type-modal" style="font-size: 10px"><?php echo esc_html( $dt_post["type"]["label"] ?? "" )?> <?php esc_html_e( 'Record', 'disciple_tools' ); ?></a>
                             <?php endif; ?>
-                                <span class="details-bar-created-on"></span>
+                                <span class="details-bar-created-on" style="font-size: 10px"></span>
                                 <?php if ( $dt_post["post_author_display_name"] ):
                                     echo esc_html( ' ' . sprintf( _x( 'by %s', '(record created) by multiplier1', 'disciple_tools' ), $dt_post["post_author_display_name"] ) );
                                 endif; ?>
