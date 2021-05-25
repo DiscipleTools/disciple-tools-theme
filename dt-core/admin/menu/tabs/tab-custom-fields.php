b3a7f22dee87db342b312fbefabbf7531088076f
cwuensche <cwuensche@users.noreply.github.com>
2021-05-25 07:21:55 -0700

    Update tab-custom-fields.php

--- dt-core/admin/menu/tabs/tab-custom-fields.php
+++ dt-core/admin/menu/tabs/tab-custom-fields.php
@@ -495,11 +495,11 @@ class Disciple_Tools_Tab_Custom_Fields extends Disciple_Tools_Abstract_Menu_Base
                                         <button type="submit" style="margin-top:1em;" class="button" name="restore_icon"
                                                 value="<?php echo esc_attr( $key ); ?>"><?php esc_html_e( 'Restore to Default', 'disciple_tools' ); ?></button>
                                     <?php
-                                        endif;
+                                            endif;
                                     ?>
                                 </td>
                             <?php
-                                endif;
+                                    endif;
                             ?>
                             <td>
                                 <button class="button small expand_translations">
