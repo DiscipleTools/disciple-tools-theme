e36a66701c2cc966d10b85245af64551eeb00f1d
Chris Chasm <chris@chasm.solutions>
2021-05-25 11:15:56 -0600

    Blank Template: Added back some icons for default settings on mobile and web browsers, but can be overridden with a filter. Couple documentation notes.

--- template-blank.php
+++ template-blank.php
@@ -3,6 +3,9 @@
 Template Name: Blank
 */
 
+/**
+ * Access control for non-logged in
+ */
 if ( ! apply_filters( 'dt_blank_access', false ) ){
     esc_html_e( 'Access to this page not permitted', 'disciple_tools' );
     exit;
@@ -14,16 +17,33 @@ if ( ! apply_filters( 'dt_blank_access', false ) ){
 
 <head>
     <meta charset="utf-8">
-
-    <!-- Force IE to use the latest rendering engine available -->
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-
-    <!-- Mobile Meta -->
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta class="foundation-mq">
 
+    <?php
+    /**
+     * Default colors and mobile icons are provided, but can be overridden using this filter
+     * These default settings are sufficient, unless you are building a progressing web app
+     * then you can override them.
+     */
+    $dt_override_header_meta = apply_filters( 'dt_override_header_meta', false ); if ( ! $dt_override_header_meta ) : ?>
+    <meta name="apple-mobile-web-app-title" content="Disciple.Tools">
+    <link rel="apple-touch-icon" sizes="180x180" href="<?php echo esc_url( get_template_directory_uri() ); ?>/dt-assets/favicons/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="<?php echo esc_url( get_template_directory_uri() ); ?>/dt-assets/favicons/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="<?php echo esc_url( get_template_directory_uri() ); ?>/dt-assets/favicons/favicon-16x16.png">
+    <link rel="mask-icon" href="<?php echo esc_url( get_template_directory_uri() ); ?>/dt-assets/favicons/safari-pinned-tab.svg" color="#3f729b">
+    <link rel="shortcut icon" href="<?php echo esc_url( get_template_directory_uri() ); ?>/dt-assets/favicons/favicon.ico">
+    <meta name="msapplication-TileColor" content="#3f729b">
+    <meta name="msapplication-TileImage" content="<?php echo esc_url( get_template_directory_uri() ); ?>/dt-assets/favicons/mstile-144x144.png">
+    <meta name="msapplication-config" content="<?php echo esc_url( get_template_directory_uri() ); ?>/dt-assets/favicons/browserconfig.xml">
+    <meta name="theme-color" content="#3f729b">
+    <?php endif; ?>
+
+
     <title><?php echo esc_html( apply_filters( 'dt_blank_title', __( 'Form', 'disciple_tools' ) ) ) ?></title>
 
+    <!-- Page Custom Header-->
     <?php do_action( 'dt_blank_head' ) ?>
 
 </head>
