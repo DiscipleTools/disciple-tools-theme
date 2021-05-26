b16ca1925562f1fd039087df5d4edde568c069ae
corsacca <corsacca@gmail.com>
2021-05-26 18:28:13 +0100

    WP Admin: include lodash on extensions, options and utilities pages

--- dt-core/admin/admin-enqueue-scripts.php
+++ dt-core/admin/admin-enqueue-scripts.php
@@ -71,6 +71,7 @@ function dt_options_scripts() {
             'jquery',
             'jquery-ui-core',
             'jquery-ui-sortable',
+            'lodash'
         ], filemtime( disciple_tools()->admin_js_path . 'dt-options.js' ), true );
         wp_register_style( 'jquery-ui', 'https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.12.1/jquery-ui.css' );
         wp_enqueue_style( 'jquery-ui' );
