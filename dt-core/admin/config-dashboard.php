4bc40bfe6ce14d08dd47572d22854322b291c98e
Prykon <dariomanoukian@gmail.com>
2021-07-28 16:55:11 -0300

    dismiss and un-dismiss functionality done

--- dt-core/admin/config-dashboard.php
+++ dt-core/admin/config-dashboard.php
@@ -126,11 +126,45 @@ final class Disciple_Tools_Dashboard
         return $query_args;
     }
 
+    public function dt_dashboard_tile() {
+        // Check for a dismissed item button click
+        if ( ! empty( $_POST['dismiss'] ) && ! empty( $_POST['setup_wizard_nonce'] ) && wp_verify_nonce( $_POST['setup_wizard_nonce'], 'update_setup_wizard_items' ) ) {
+            $item_label = esc_sql( $_POST['dismiss'] );
+            $dt_setup_wizard_options = get_option( 'dt_setup_wizard_options', null );
 
-    public function dt_dashboard_tile(){
+            // Create the option and populate it if it doesn't exist and/or is empty
+            if ( empty( $dt_setup_wizard_options ) ) {
+                $dt_setup_wizard_options = $item_label;
+                update_option( 'dt_setup_wizard_options', $dt_setup_wizard_options );
+            } else {
+                $dt_setup_wizard_options = explode( ';', $dt_setup_wizard_options );
+                if ( ! in_array( $item_label, $dt_setup_wizard_options ) ) {
+                    $dt_setup_wizard_options[] = $item_label;
+                    $dt_setup_wizard_options = implode( ';', $dt_setup_wizard_options );
+                    update_option( 'dt_setup_wizard_options', $dt_setup_wizard_options );
+                }
+            }
+        }
+
+        // Check for an un-dismissed item button click
+        else if ( ! empty( $_POST['undismiss'] ) && ! empty( $_POST['setup_wizard_nonce'] ) && wp_verify_nonce( $_POST['setup_wizard_nonce'], 'update_setup_wizard_items' ) ) {
+            $item_label = esc_sql( $_POST['undismiss'] );
+            $dt_setup_wizard_options = get_option( 'dt_setup_wizard_options', null );
+            if ( ! empty( $dt_setup_wizard_options ) ) {
+                $dt_setup_wizard_options = explode( ';', $dt_setup_wizard_options );
+                foreach ( $dt_setup_wizard_options as $opt_key => $opt_value ) {
+                    if ( $opt_value === $item_label ) {
+                        unset( $dt_setup_wizard_options[$opt_key] );
+                    }
+                }
+                $dt_setup_wizard_options = implode( ';', $dt_setup_wizard_options );
+                update_option( 'dt_setup_wizard_options', $dt_setup_wizard_options );
+            }
+        }
+
         wp_add_dashboard_widget('dt_setup_wizard', 'Disciple.Tools Setup Wizard', function (){
 
-            $setup_options = get_option( "dt_setup_options", [] );
+            $setup_options = get_option( "dt_setup_wizard_options", [] );
             $default = [
                 "base_email" => [
                     "label" => "Base User",
@@ -150,32 +184,135 @@ final class Disciple_Tools_Dashboard
             }
 
             ?><p>Completed <?php echo esc_html( $completed ); ?> of <?php echo esc_html( sizeof( $dt_setup_wizard_items ) ); ?> tasks</p>
-            <table class="widefat striped">
-                <thead>
-                    <tr>
-                        <th></th>
-                        <th>Name</th>
-                        <th>Link</th>
-                        <th>Complete</th>
-                    </tr>
-                </thead>
-                <tbody>
-                <?php
-                foreach ( $dt_setup_wizard_items as $item_key => $item_value ) :?>
-                    <tr>
-                        <td><?php echo esc_html( array_search( $item_key, array_keys( $dt_setup_wizard_items ) ) +1 ); ?>.</td>
-                        <td><?php echo esc_html( $item_value["label"] ); ?></td>
-                        <td>Update <a href="<?php echo esc_html( $item_value["link"] ); ?>">here</a></td>
-                        <td>
-                            <?php if ( $item_value["complete"] ) :?>
+            <style>
+                .wizard_chevron_open {
+                    position: relative;
+                    width: 7px;
+                    height: 7px;
+                    border-width: 0 2px 2px 0;
+                    border-style: solid;
+                    transform: rotate(45deg);
+                    margin: auto;
+                }
+                .wizard_chevron_close {
+                    position: relative;
+                    width: 7px;
+                    height: 7px;
+                    border-width: 0 2px 2px 0;
+                    border-style: solid;
+                    transform: rotate(225deg);
+                    margin: auto;
+                }
+                .toggle_chevron{
+                    vertical-align: middle !important;
+                    cursor:pointer;
+                }
+                .wizard_description{
+                    position: relative;
+                    height: 200px;
+                    top: 15px;
+                    left: 15px;
+                }
+            </style>
+            <form method="POST">
+                <?php wp_nonce_field( 'update_setup_wizard_items', 'setup_wizard_nonce' ); ?>
+                <table class="widefat striped">
+                    <thead>
+                        <tr>
+                            <th></th>
+                            <th>Name</th>
+                            <th>Link</th>
+                            <th>Complete</th>
+                            <th></th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                    <?php
+                    $row_count = 0;
+                    foreach ( $dt_setup_wizard_items as $item_key => $item_value ) :?>
+                        <tr>
+                            <td><?php echo esc_html( array_search( $item_key, array_keys( $dt_setup_wizard_items ) ) +1 ); ?>.</td>
+                            <td><?php echo esc_html( $item_value["label"] ); ?></td>
+                            <td>Update <a href="<?php echo esc_html( $item_value["link"] ); ?>">here</a></td>
+                            <td>
+                                <?php
+                                if ( $item_value['complete'] ) {
+                                    ?>
                                 <img class="dt-icon" src="<?php echo esc_html( get_template_directory_uri() . '/dt-assets/images/verified.svg' ) ?>"/>
-                            <?php elseif ( empty( $item_value["hide_mark_done"] ) ) : ?>
-                                <button>Mark Done</button></td>
-                            <?php endif; ?>
-                    </tr>
-                <?php endforeach; ?>
-                </tbody>
-            </table>
+                                    <?php
+                                }
+                                // Logic for displaying the 'dismiss' button
+                                if ( $item_value["hide_mark_done"] == false ) {
+                                    $dt_setup_wizard_options = get_option( 'dt_setup_wizard_options', null );
+                                    if ( empty( $dt_setup_wizard_options ) ) {
+                                        ?>
+                                            <button name="dismiss" value="<?php echo esc_attr( $item_key ); ?>">Dismiss</button>
+                                        <?php
+                                    } else {
+                                        $dt_setup_wizard_options = explode( ';', $dt_setup_wizard_options );
+                                        if ( ! in_array( $item_key, $dt_setup_wizard_options ) ) {
+                                            ?>
+                                                <button name="dismiss" value="<?php echo esc_attr( $item_key ); ?>">Dismiss</button>
+                                            <?php
+                                        }
+                                    }
+                                }
+                                ?>
+                            </td>
+                            <td class="toggle_chevron" data-cell="<?php echo esc_attr( $row_count ); ?>">
+                                <div class="wizard_chevron_open"></div>
+                            </td>
+                        </tr>
+                        <tr class="wizard_description" data-row="<?php echo esc_attr( $row_count ); ?>" hidden>
+                            <td colspan="5">
+                                <p>
+                                    <?php echo esc_html( $item_value['description'] ); ?>
+                                </p>
+                                <?php
+                                // Logic for displaying the 'un-dismiss' button
+                                if ( $item_value["hide_mark_done"] == false ) {
+                                    $dt_setup_wizard_options = get_option( 'dt_setup_wizard_options', null );
+                                    if ( ! empty( $dt_setup_wizard_options ) ) {
+                                        $dt_setup_wizard_options = explode( ';', $dt_setup_wizard_options );
+                                        if ( in_array( $item_key, $dt_setup_wizard_options ) ) {
+                                            ?>
+                                                <button name="undismiss" value="<?php echo esc_attr( $item_key ); ?>">Un-dismiss</button>
+                                            <?php
+                                        }
+                                    }
+                                }
+                                ?>
+                            </td>
+                        </tr>
+                            <?php $row_count++; ?>
+                    <?php endforeach; ?>
+                    </tbody>
+                </table>
+            </form>
+            
+            <!-- Prykon Setup Wizard Table -->
+            <script>
+                jQuery( '.toggle_chevron' ).on( 'click', function() {
+                    let class_name = jQuery( this ).children()[0].className;
+                    let cell_number = jQuery( this ).data('cell');
+
+                    // Toggle chevron arrow class names
+                    if ( class_name == 'wizard_chevron_open' ) {
+                        jQuery( this ).children().attr('class', 'wizard_chevron_close');
+                    } else {
+                        jQuery( this ).children().attr('class', 'wizard_chevron_open');
+                    }
+
+                    // Toggle description row visibility
+                    let row = jQuery( "*[data-row='" + cell_number + "']" )[0];
+                    if ( row.hidden == true ) {
+                        row.hidden = false;
+                    } else {
+                        row.hidden = true;
+                    }
+                });
+            </script>
+            <!-- Prykon Setup Wizard Table End -->
             <?php
         });
     }
@@ -191,7 +328,7 @@ add_filter( 'dt_setup_wizard_items', function ( $items, $setup_options ){
     $items["https_check"] = [
         "label" => "Upgrade HTTP to HTTPS",
         "description" => "Encrypt your traffic from network sniffers",
-        "link" => admin_url( "options-general.php" ),
+        "link" => esc_url( "https://wordpress.org/support/article/https-for-wordpress/" ),
         "complete" => wp_is_using_https() ? true : false,
         "hide_mark_done" => true
     ];
