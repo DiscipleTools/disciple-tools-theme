27a387b13ba5f69ed71f288bb5fd77b43c1dd730
corsac <corsacca@gmail.com>
2021-08-25 09:34:01 -0700

    Theme Version 1.11.0 and translation update

--- functions.php
+++ functions.php
@@ -149,7 +149,7 @@ else {
              * Prepare variables
              */
             $this->token = 'disciple_tools';
-            $this->version = '1.10.1';
+            $this->version = '1.11.0';
             // $this->migration_number = 38; // moved to Disciple_Tools_Migration_Engine::$migration_number
 
             $this->theme_url = get_template_directory_uri() . '/';
