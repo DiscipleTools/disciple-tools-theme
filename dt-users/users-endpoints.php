33f199f795f3fbe15853785b87aa33f02cd94e26
squigglybob <nathinabob@gmail.com>
2021-09-10 10:29:30 +0100

    fix: add user with multiplier role if the 'user-roles' param exists but is empty

--- dt-users/users-endpoints.php
+++ dt-users/users-endpoints.php
@@ -235,7 +235,7 @@ class Disciple_Tools_Users_Endpoints
             if ( isset( $params["user-user_role"] ) ){
                 $user_roles = [ $params["user-user_role"] ];
             }
-            if ( isset( $params["user-roles"] ) ){
+            if ( isset( $params["user-roles"] ) && !empty( $params["user-roles"] ) ){
                 $user_roles =$params["user-roles"];
             }
             $user_login = $params["user-user_login"] ?? $params["user-email"];
