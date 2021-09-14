0342352c2674193d6c4970852f63d962e49254a3
corsac <corsacca@gmail.com>
2021-09-14 16:12:13 +0100

    Show both english and native names in lang select

--- dt-core/global-functions.php
+++ dt-core/global-functions.php
@@ -447,7 +447,9 @@ if ( ! defined( 'DT_FUNCTIONS_READY' ) ){
             ?>
             <select name="locale">
                 <?php foreach ( $languages as $language ){ ?>
-                    <option value="<?php echo esc_html( $language["language"] ); ?>" <?php selected( $dt_user_locale === $language["language"] ) ?>><?php echo esc_html( $language["native_name"] ); ?></option>
+                    <option value="<?php echo esc_html( $language["language"] ); ?>" <?php selected( $dt_user_locale === $language["language"] ) ?>>
+                        <?php echo esc_html( $language["native_name"] ); ?> - <?php echo esc_html( $language["english_name"] ); ?>
+                    </option>
                 <?php } ?>
             </select>
             <?php
