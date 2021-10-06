bf9943bea3a4f799bca26523cec2e11d666d5e9e
Prykon <dariomanoukian@gmail.com>
2021-10-06 14:07:54 -0300

    Changed 'is church' text to 'church commitment'

--- dt-groups/base-setup.php
+++ dt-groups/base-setup.php
@@ -515,7 +515,7 @@ class DT_Groups_Base extends DT_Module_Base {
                     </div>
                 </div>
                 <div>
-                    <span>Is church:</span>
+                    <span>Church Commitment:</span>
                     <input type="checkbox" id="is-church-switch" class="dt-switch">
                     <label class="dt-switch" for="is-church-switch" style="vertical-align: top;"></label>
                 </div>
