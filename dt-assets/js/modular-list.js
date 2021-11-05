5fb48ede6f4afb34cac7fbe432cba7473434bd64
corsacca <corsacca@gmail.com>
2021-11-05 16:06:34 +0100

    List: Fix window.record_list variable to include all loaded posts

--- dt-assets/js/modular-list.js
+++ dt-assets/js/modular-list.js
@@ -20,6 +20,7 @@
   let mobile_breakpoint = 1024
   let clearSearchButton = $('.search-input__clear-button')
   window.post_type_fields = list_settings.post_type_settings.fields
+  window.records_list = { posts:[], total:0 }
 
   const ALL_ID = '*'
   const ALL_WITHOUT_ID = '-*'
@@ -601,7 +602,8 @@
       } else  {
         items = response.posts || []
       }
-      window.records_list = items // adds global access to current list for plugins
+      window.records_list.posts = items // adds global access to current list for plugins
+      window.records_list.total = response.total
 
       // save
       if (response.hasOwnProperty('posts') && response.posts.length > 0) {
