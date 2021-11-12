8c302a4abe49d9b1b6e3dc84da6ed5824b978870
Chris Chasm <chris@chasm.solutions>
2021-11-12 14:08:30 -0700

    Removed requirement from post_id column

--- dt-core/migrations/0046-add-userid-column-to-reports.php
+++ dt-core/migrations/0046-add-userid-column-to-reports.php
@@ -10,6 +10,8 @@ class Disciple_Tools_Migration_0046 extends Disciple_Tools_Migration {
         }
         // add parent_id
         $wpdb->query( "ALTER TABLE $wpdb->dt_reports ADD `user_id` BIGINT(22) NULL DEFAULT NULL AFTER `id`" );
+
+        $wpdb->query( "ALTER TABLE $wpdb->dt_reports CHANGE `post_id` `post_id` BIGINT(22) DEFAULT NULL;" );
     }
 
     public function down() {
