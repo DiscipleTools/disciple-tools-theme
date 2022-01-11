658cbbde57ed30954318e91f4516591ddf0ca2a7
kodinkat <me@karlkatewu.com>
2022-01-11 12:30:58 +0000

    Reformatted email settings display + added default email and name info

--- dt-core/admin/menu/tabs/tab-general.php
+++ dt-core/admin/menu/tabs/tab-general.php
@@ -224,16 +224,47 @@ class Disciple_Tools_General_Tab extends Disciple_Tools_Abstract_Menu_Base
     public function email_settings(){
         ?>
         <form method="POST">
-            <input type="hidden" name="email_base_subject_nonce" id="email_base_subject_nonce" value="<?php echo esc_attr( wp_create_nonce( 'email_subject' ) )?>" />
-            <label for="email_address"><?php esc_html_e( "Specify notification from email address. Leave blank to use default", 'disciple_tools' ) ?></label>
-            <input name="email_address" id="email_address" value="<?php echo esc_html( dt_get_option( "dt_email_base_address" ) ) ?>" />
+            <input type="hidden" name="email_base_subject_nonce" id="email_base_subject_nonce"
+                   value="<?php echo esc_attr( wp_create_nonce( 'email_subject' ) ) ?>"/>
+
+            <table class="widefat">
+                <tbody>
+                <tr>
+                    <td>
+                        <label
+                            for="email_address"><?php esc_html_e( sprintf( "Specify notification from email address. Leave blank to use default (%s)", apply_filters( 'wp_mail_from', '' ) ), 'disciple_tools' ) ?></label>
+                    </td>
+                    <td>
+                        <input name="email_address" id="email_address"
+                               value="<?php echo esc_html( dt_get_option( "dt_email_base_address" ) ) ?>"/>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <label
+                            for="email_name"><?php esc_html_e( sprintf( "Specify notification from name. Leave blank to use default (%s)", apply_filters( 'wp_mail_from_name', '' ) ), 'disciple_tools' ) ?></label>
+                    </td>
+                    <td>
+                        <input name="email_name" id="email_name"
+                               value="<?php echo esc_html( dt_get_option( "dt_email_base_name" ) ) ?>"/>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <label
+                            for="email_subject"><?php esc_html_e( "Configure the first part of the subject line in email sent by Disciple.Tools", 'disciple_tools' ) ?></label>
+                    </td>
+                    <td>
+                        <input name="email_subject" id="email_subject"
+                               value="<?php echo esc_html( dt_get_option( "dt_email_base_subject" ) ) ?>"/>
+                    </td>
+                </tr>
+                </tbody>
+            </table>
+
             <br>
-            <label for="email_name"><?php esc_html_e( "Specify notification from name. Leave blank to use default", 'disciple_tools' ) ?></label>
-            <input name="email_name" id="email_name" value="<?php echo esc_html( dt_get_option( "dt_email_base_name" ) ) ?>" />
-            <br>
-            <label for="email_subject"><?php esc_html_e( "Configure the first part of the subject line in email sent by Disciple.Tools", 'disciple_tools' ) ?></label>
-            <input name="email_subject" id="email_subject" value="<?php echo esc_html( dt_get_option( "dt_email_base_subject" ) ) ?>" />
-            <span style="float:right;"><button type="submit" class="button float-right"><?php esc_html_e( "Update", 'disciple_tools' ) ?></button></span>
+            <span style="float:right;"><button type="submit"
+                                               class="button float-right"><?php esc_html_e( "Update", 'disciple_tools' ) ?></button></span>
         </form>
         <?php
     }
