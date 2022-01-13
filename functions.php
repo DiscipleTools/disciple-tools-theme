57dfc2cb5e116b039280de995c86d3dd9d68a7de
corsac <corsacca@gmail.com>
2022-01-13 12:27:57 +0100

    D.T v1.21.1 include release notification

--- functions.php
+++ functions.php
@@ -143,7 +143,7 @@ if ( version_compare( phpversion(), '7.0', '<' ) ) {
              * Prepare variables
              */
             $this->token = 'disciple_tools';
-            $this->version = '1.21.0';
+            $this->version = '1.21.1';
             // $this->migration_number = 38; // moved to Disciple_Tools_Migration_Engine::$migration_number
 
             $this->theme_url = get_template_directory_uri() . '/';
