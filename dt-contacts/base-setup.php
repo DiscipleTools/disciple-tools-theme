bc4e4926c0a0aa15e9a55960f4b22215a97378c1
micahmills <micahmills@gmail.com>
2022-01-19 15:03:29 +0300

    Add a "Open With..." box when someone clicks a phone link (#1553)
    * Add a "Open With..." box when someone clicks a phone link on the details pane and creates links for Whatsapp, Signal, iMessage (if on an apple device) and a normal phone link.
    
    * added icons and Viber link
    
    * Fixes issue with multiple phone numbers
    
    * Added messagingServices to $field["contact_phone"] so that services can be added via plugin or some future settings.
    
    Adjusted the JS to loop through the services and include the links.
    
    * removed extra semicolon
    
    * removed deleted variable
    
    * added messaging service names as translatable strings.
    
    * added missed translation strings and removed margin from UL
    
    * Added Phone translation

--- dt-contacts/base-setup.php
+++ dt-contacts/base-setup.php
@@ -196,6 +196,23 @@ class DT_Contacts_Base {
                 "tile" => "details",
                 "customizable" => false,
                 "in_create_form" => true,
+                "messagingServices" => [
+                    'Signal' => [
+                        'name' => __( "Signal", 'disciple_tools' ),
+                        "link" => "https://signal.me/#p/PHONE_NUMBER",
+                        "icon" => get_template_directory_uri() . "/dt-assets/images/signal.svg"
+                    ],
+                    'Viber' => [
+                        'name' => __( "Viber", 'disciple_tools' ),
+                        "link" => "viber://chat?number=PHONE_NUMBER",
+                        "icon" => get_template_directory_uri() . "/dt-assets/images/viber.svg"
+                    ],
+                    'Whatsapp' => [
+                        'name' => __( "Whatsapp", 'disciple_tools' ),
+                        "link" => "https://api.whatsapp.com/send?phone=PHONE_NUMBER_NO_PLUS",
+                        "icon" => get_template_directory_uri() . "/dt-assets/images/signal.svg"
+                    ],
+                ]
             ];
             $fields["contact_email"] = [
                 "name" => __( 'Email', 'disciple_tools' ),
