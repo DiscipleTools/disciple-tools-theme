e1e3cc0f19d3fceb5c2146455648f5967010fd26
kodinkat <me@karlkatewu.com>
2022-02-03 09:55:25 +0000

    Refactored statements to support addition of future pages

--- dt-users/user-management.php
+++ dt-users/user-management.php
@@ -15,16 +15,16 @@ class DT_User_Management
     public function __construct() {
         $url_path = dt_get_url_path();
         if ( self::has_permission() || self::non_admins_can_make_users() ) {
-            if ( strpos( $url_path, 'user-management/user' ) !== false || strpos( $url_path, 'user-management/hover-map' ) !== false || ( strpos( $url_path, 'user-management/add-user' ) !== false && ( current_user_can( "create_users" ) || self::non_admins_can_make_users() ) ) ){
+            if ( str_contains( $url_path, 'user-management/' ) && ( current_user_can( "create_users" ) || self::non_admins_can_make_users() ) ){
                 add_action( 'wp_enqueue_scripts', [ $this, 'scripts' ], 99 );
                 add_filter( 'dt_templates_for_urls', [ $this, 'dt_templates_for_urls' ] );
             }
         }
         if ( self::has_permission() ){
-            if ( strpos( $url_path, 'user-management' ) !== false || strpos( $url_path, 'user-management' ) !== false ) {
+            if ( str_contains( $url_path, 'user-management' ) ) {
                 add_filter( 'dt_metrics_menu', [ $this, 'add_menu' ], 20 );
             }
-            if ( strpos( $url_path, 'user-management/user' ) !== false || ( strpos( $url_path, 'user-management/add-user' ) !== false && ( current_user_can( "create_users" ) ) ) ){
+            if ( str_contains( $url_path, 'user-management/user' ) || ( str_contains( $url_path, 'user-management/add-user' ) && ( current_user_can( "create_users" ) ) ) ){
 
 
                 add_action( 'init', function() {
