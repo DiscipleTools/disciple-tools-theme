d44a7b61cf2bcacb057a5625add316e22943ea28
Craig Wann <craig@codezone.io>
2022-02-28 10:52:26 -0600

    Calling setup capabilities manually after it was moved the init hook.

--- tests/unit-test-capabilities.php
+++ tests/unit-test-capabilities.php
@@ -3,6 +3,7 @@
 class CapabilitiesTest extends WP_UnitTestCase {
     public function test_capabilities_setup() {
         $factory = Disciple_Tools_Capability_Factory::get_instance();
+        $factory->setup_capabilities();
         $capabilities = $factory->get_capabilities();
         $slugs = dt_multi_role_get_capabilities();
 
@@ -18,6 +19,7 @@ class CapabilitiesTest extends WP_UnitTestCase {
     public function test_filter_capabilities() {
         $slugs = [ 'update_any_groups', 'unfiltered_upload' ];
         $factory = Disciple_Tools_Capability_Factory::get_instance();
+        $factory->setup_capabilities();
         $capabilities = $factory->get_capabilities( $slugs );
 
         $this->assertEquals( 2, count( $capabilities ) );
@@ -31,6 +33,7 @@ class CapabilitiesTest extends WP_UnitTestCase {
     public function test_has_capabilities() {
         $slugs = [ 'update_any_groups', 'unfiltered_upload' ];
         $factory = Disciple_Tools_Capability_Factory::get_instance();
+        $factory->setup_capabilities();
         $capabilities = $factory->get_capabilities( $slugs );
 
         $this->assertEquals( 2, count( $capabilities ) );
