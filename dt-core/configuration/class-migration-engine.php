df94ac95fc6d5b0431a4c1c75a9f4eb0243f6f45
Craig Wann <craig@codezone.io>
2022-02-28 09:51:29 -0600

    Reverting migration number bump.

--- dt-core/configuration/class-migration-engine.php
+++ dt-core/configuration/class-migration-engine.php
@@ -12,7 +12,7 @@ if ( !defined( 'ABSPATH' ) ) {
 class Disciple_Tools_Migration_Engine
 {
 
-    public static $migration_number = 47;
+    public static $migration_number = 46;
 
     protected static $migrations = null;
 
