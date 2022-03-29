5c9de5a2b5417fd81a3e6a2c9f2b8ee867e63fda
Mike Allbutt <yo@ur.id.au>
2022-03-28 20:20:30 -0600

    fix "assign the to"

--- dt-contacts/access-module.php
+++ dt-contacts/access-module.php
@@ -70,7 +70,7 @@ class DT_Contacts_Access extends DT_Module_Base {
         ];
         $expected_roles['dispatcher'] = [
             "label" => __( "Dispatcher", "disciple_tools" ),
-            "description" => "Monitor new D.T contacts and assign the to waiting Multipliers",
+            "description" => "Monitor new D.T contacts and assign them to waiting Multipliers",
             "permissions" => wp_parse_args( [
                 'dt_all_access_contacts' => true,
                 'view_project_metrics' => true,
