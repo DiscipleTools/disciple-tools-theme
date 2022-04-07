648a9eec67cf12b5ba4a17545569d1553bf462c2
corsacca <corsacca@gmail.com>
2022-04-07 17:57:19 +0100

    rename jQuery variable

--- dt-reports/magic-url-setup.php
+++ dt-reports/magic-url-setup.php
@@ -97,7 +97,7 @@ class DT_Magic_URL_Setup {
             <a type="button" class="empty-select-button select-button small button reset"><img class="dt-icon" alt="undo" src="<?php echo esc_url( get_template_directory_uri() . '/dt-assets/images/undo.svg' ) ?>" /></a>
         </div>
         <script>
-            jQuery(document).ready(function(){
+            jQuery(document).ready(function($){
                 if ( typeof window.app_key === 'undefined' ){
                     window.app_key = []
                 }
@@ -107,43 +107,43 @@ class DT_Magic_URL_Setup {
                 window.app_key['<?php echo esc_attr( $meta_key ) ?>'] = '<?php echo esc_attr( $key ) ?>'
                 window.app_url['<?php echo esc_attr( $meta_key ) ?>'] = '<?php echo esc_url( site_url() . '/' . $app['root'] . '/' .$app['type'] . '/' ) ?>'
 
-                jQuery('.<?php echo esc_attr( $meta_key ); ?>.select-button.button.copy_to_clipboard').data('value', `${window.app_url['<?php echo esc_attr( $meta_key ) ?>']}${window.app_key['<?php echo esc_attr( $meta_key ) ?>']}`)
-                jQuery('.section-app-links.<?php echo esc_attr( $meta_key ); ?> .view').on('click', function(e){
-                    jQuery('#modal-large-title').empty().html(`<h3 class="section-header"><?php echo esc_html( $app['label'] )  ?></h3><span class="small-text"><?php echo esc_html( $app['description'] ) ?></span><hr>`)
-                    jQuery('#modal-large-content').empty().html(`<iframe src="${window.app_url['<?php echo esc_attr( $meta_key ) ?>']}${window.app_key['<?php echo esc_attr( $meta_key ) ?>']}" style="width:100%;height: ${window.innerHeight - 170}px;border:1px solid lightgrey;"></iframe>`)
-                    jQuery('#modal-large').foundation('open')
+                $('.<?php echo esc_attr( $meta_key ); ?>.select-button.button.copy_to_clipboard').data('value', `${window.app_url['<?php echo esc_attr( $meta_key ) ?>']}${window.app_key['<?php echo esc_attr( $meta_key ) ?>']}`)
+                $('.section-app-links.<?php echo esc_attr( $meta_key ); ?> .view').on('click', function(e){
+                    $('#modal-large-title').empty().html(`<h3 class="section-header"><?php echo esc_html( $app['label'] )  ?></h3><span class="small-text"><?php echo esc_html( $app['description'] ) ?></span><hr>`)
+                    $('#modal-large-content').empty().html(`<iframe src="${window.app_url['<?php echo esc_attr( $meta_key ) ?>']}${window.app_key['<?php echo esc_attr( $meta_key ) ?>']}" style="width:100%;height: ${window.innerHeight - 170}px;border:1px solid lightgrey;"></iframe>`)
+                    $('#modal-large').foundation('open')
                 })
-                jQuery('.section-app-links.<?php echo esc_attr( $meta_key ); ?> .send').on('click', function(e){
-                    jQuery('#modal-small-title').empty().html(`<h3 class="section-header"><?php echo esc_html( $app['label'] )  ?></h3><span class="small-text"><?php echo esc_html__( 'Send a link via email through the system.', 'disciple_tools' ) ?></span><hr>`)
-                    jQuery('#modal-small-content').empty().html(`<div class="grid-x"><div class="cell"><input type="text" class="note <?php echo esc_attr( $meta_key ); ?>" placeholder="Add a note" /><br><button type="button" class="button <?php echo esc_attr( $meta_key ); ?>"><?php echo esc_html__( 'Send email with link', 'disciple_tools' ) ?> <span class="<?php echo esc_attr( $meta_key ); ?> loading-spinner"></span></button></div></div>`)
-                    jQuery('#modal-small').foundation('open')
-                    jQuery('.button.<?php echo esc_attr( $meta_key ); ?>').on('click', function(e){
-                        jQuery('.<?php echo esc_attr( $meta_key ); ?>.loading-spinner').addClass('active')
-                        let note = jQuery('.note.<?php echo esc_attr( $meta_key ); ?>').val()
+                $('.section-app-links.<?php echo esc_attr( $meta_key ); ?> .send').on('click', function(e){
+                    $('#modal-small-title').empty().html(`<h3 class="section-header"><?php echo esc_html( $app['label'] )  ?></h3><span class="small-text"><?php echo esc_html__( 'Send a link via email through the system.', 'disciple_tools' ) ?></span><hr>`)
+                    $('#modal-small-content').empty().html(`<div class="grid-x"><div class="cell"><input type="text" class="note <?php echo esc_attr( $meta_key ); ?>" placeholder="Add a note" /><br><button type="button" class="button <?php echo esc_attr( $meta_key ); ?>"><?php echo esc_html__( 'Send email with link', 'disciple_tools' ) ?> <span class="<?php echo esc_attr( $meta_key ); ?> loading-spinner"></span></button></div></div>`)
+                    $('#modal-small').foundation('open')
+                    $('.button.<?php echo esc_attr( $meta_key ); ?>').on('click', function(e){
+                        $('.<?php echo esc_attr( $meta_key ); ?>.loading-spinner').addClass('active')
+                        let note = $('.note.<?php echo esc_attr( $meta_key ); ?>').val()
                         makeRequest('POST', window.detailsSettings.post_type + '/email_magic', { root: '<?php echo esc_attr( $app['root'] ); ?>', type: '<?php echo esc_attr( $app['type'] ); ?>', note: note, post_ids: [ window.detailsSettings.post_id ] } )
                         .done( data => {
-                            jQuery('.<?php echo esc_attr( $meta_key ); ?>.loading-spinner').removeClass('active')
-                            jQuery('#modal-small').foundation('close')
+                            $('.<?php echo esc_attr( $meta_key ); ?>.loading-spinner').removeClass('active')
+                            $('#modal-small').foundation('close')
                         })
                     })
                 })
-                jQuery('.section-app-links.<?php echo esc_attr( $meta_key ); ?> .qr').on('click', function(e){
-                    jQuery('#modal-small-title').empty().html(`<h3 class="section-header"><?php echo esc_html( $app['label'] )  ?></h3><span class="small-text"><?php echo esc_html__( 'QR codes are useful for passing the coaching links to mobile devices.', 'disciple_tools' ) ?></span><hr>`)
-                    jQuery('#modal-small-content').empty().html(`<div class="grid-x"><div class="cell center"><img src="https://api.qrserver.com/v1/create-qr-code/?size=400x400&data=${window.app_url['<?php echo esc_attr( $meta_key ) ?>']}${window.app_key['<?php echo esc_attr( $meta_key ) ?>']}" style="width: 100%;max-width:400px;" /></div></div>`)
-                    jQuery('#modal-small').foundation('open')
+                $('.section-app-links.<?php echo esc_attr( $meta_key ); ?> .qr').on('click', function(e){
+                    $('#modal-small-title').empty().html(`<h3 class="section-header"><?php echo esc_html( $app['label'] )  ?></h3><span class="small-text"><?php echo esc_html__( 'QR codes are useful for passing the coaching links to mobile devices.', 'disciple_tools' ) ?></span><hr>`)
+                    $('#modal-small-content').empty().html(`<div class="grid-x"><div class="cell center"><img src="https://api.qrserver.com/v1/create-qr-code/?size=400x400&data=${window.app_url['<?php echo esc_attr( $meta_key ) ?>']}${window.app_key['<?php echo esc_attr( $meta_key ) ?>']}" style="width: 100%;max-width:400px;" /></div></div>`)
+                    $('#modal-small').foundation('open')
                 })
-                jQuery('.section-app-links.<?php echo esc_attr( $meta_key ); ?> .reset').on('click', function(e){
-                    jQuery('#modal-small-title').empty().html(`<h3 class="section-header"><?php echo esc_html( $app['label'] )  ?></h3><span class="small-text"><?php echo esc_html__( 'Reset the security code. No data is removed. Only access. The previous link will be disabled and another one created.', 'disciple_tools' ) ?></span><hr>`)
-                    jQuery('#modal-small-content').empty().html(`<button type="button" class="button <?php echo esc_attr( $meta_key ); ?> delete-and-reset"><?php echo esc_html__( 'Delete and replace the app link', 'disciple_tools' ) ?>  <span class="<?php echo esc_attr( $meta_key ); ?> loading-spinner"></span></button>`)
-                    jQuery('#modal-small').foundation('open')
-                    jQuery('.button.<?php echo esc_attr( $meta_key ); ?>.delete-and-reset').on('click', function(e){
-                        jQuery('.button.<?php echo esc_attr( $meta_key ); ?>.delete-and-reset').prop('disable', true)
-                        jQuery('.<?php echo esc_attr( $meta_key ); ?>.loading-spinner').addClass('active')
+                $('.section-app-links.<?php echo esc_attr( $meta_key ); ?> .reset').on('click', function(e){
+                    $('#modal-small-title').empty().html(`<h3 class="section-header"><?php echo esc_html( $app['label'] )  ?></h3><span class="small-text"><?php echo esc_html__( 'Reset the security code. No data is removed. Only access. The previous link will be disabled and another one created.', 'disciple_tools' ) ?></span><hr>`)
+                    $('#modal-small-content').empty().html(`<button type="button" class="button <?php echo esc_attr( $meta_key ); ?> delete-and-reset"><?php echo esc_html__( 'Delete and replace the app link', 'disciple_tools' ) ?>  <span class="<?php echo esc_attr( $meta_key ); ?> loading-spinner"></span></button>`)
+                    $('#modal-small').foundation('open')
+                    $('.button.<?php echo esc_attr( $meta_key ); ?>.delete-and-reset').on('click', function(e){
+                        $('.button.<?php echo esc_attr( $meta_key ); ?>.delete-and-reset').prop('disable', true)
+                        $('.<?php echo esc_attr( $meta_key ); ?>.loading-spinner').addClass('active')
                         window.API.update_post('<?php echo esc_attr( $post_type ); ?>', <?php echo esc_attr( get_the_ID() ); ?>, { ['<?php echo esc_attr( $meta_key ); ?>']: window.sha256( Date.now() ) })
                         .done( newPost => {
-                            jQuery('#modal-small').foundation('close')
+                            $('#modal-small').foundation('close')
                             window.app_key['<?php echo esc_attr( $meta_key ) ?>'] = newPost['<?php echo esc_attr( $meta_key ) ?>']
-                            jQuery('.section-app-links.<?php echo esc_attr( $meta_key ); ?> .select-button.button.copy_to_clipboard').data('value', `${window.app_url['<?php echo esc_attr( $meta_key ) ?>']}${window.app_key['<?php echo esc_attr( $meta_key ) ?>']}`)
+                            $('.section-app-links.<?php echo esc_attr( $meta_key ); ?> .select-button.button.copy_to_clipboard').data('value', `${window.app_url['<?php echo esc_attr( $meta_key ) ?>']}${window.app_key['<?php echo esc_attr( $meta_key ) ?>']}`)
                         })
                     })
                 })
