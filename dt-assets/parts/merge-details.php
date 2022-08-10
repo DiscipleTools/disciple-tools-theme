1ee82a0f3dfb05ffffcdded2679cb3534f32218c
Prykon <dariomanoukian@gmail.com>
2022-08-10 15:54:06 -0300

    Refactored inline styles into CSS classes

--- dt-assets/parts/merge-details.php
+++ dt-assets/parts/merge-details.php
@@ -3,31 +3,27 @@
 ( function () {
     $post_type = dt_get_post_type();
     $post_settings = DT_Posts::get_post_settings( $post_type );
-
     ?>
-    <div class="reveal" id="merge-dupe-edit-modal" style="border-radius:10px; padding:0px; padding-bottom:20px; border: 1px solid #3f729b;;" data-reveal>
-        <div class="merge-modal-header" style="background-color:#3f729b; color:white; text-align:center;">
-            <h1 style="font-size:1.5rem; padding:10px 0px;"><?php esc_html_e( "Duplicate Contacts", 'disciple_tools' ) ?></h1>
+    <div class="reveal merge-modal" id="merge-dupe-edit-modal" data-reveal>
+        <div class="merge-modal-header">
+            <h1 class="merge-modal-heading"><?php esc_html_e( 'Duplicate Contacts', 'disciple_tools' ) ?></h1>
         </div>
-        <h4 style='text-align: center; font-size: 1.25rem; font-weight: bold; padding:10px 0px 0px; margin-bottom: 0px;'><?php esc_html_e( "Original Contact", 'disciple_tools' ) ?></h4>
+        <h4 class="merge-modal-subheading"><?php esc_html_e( 'Original Contact', 'disciple_tools' ) ?></h4>
         <div id="original-contact" class="display-fields"></div>
-
-        <div style="text-align: center">
-            <h4 style='text-align: center; font-size: 1.25rem; font-weight: bold; padding:20px 0 0; margin-bottom: 0; display: inline-block'><?php esc_html_e( "Possible Duplicates", 'disciple_tools' ) ?></h4>
-            <div style="display: inline-block; " id="duplicates-spinner" class="loading-spinner active"></div>
-            <a id='dismiss_all_duplicates' style="position: absolute; padding-top:23px"><?php esc_html_e( 'Dismiss All', 'disciple_tools' ); ?></a>
+        <div class="center">
+            <h4 class="merge-modal-subheading"><?php esc_html_e( 'Possible Duplicates', 'disciple_tools' ) ?></h4>
+            <a id="dismiss_all_duplicates"><?php esc_html_e( 'Dismiss All', 'disciple_tools' ); ?></a>
+            <div id="duplicates-spinner" class="loading-spinner active merge-modal-spinner"></div>
         </div>
-        <div class="display-fields" style="padding:10px;">
-            <p id="no_dups_message" style="display: none; text-align: center; margin-top: 20px"><?php esc_html_e( 'No duplicates found.', 'disciple_tools' ); ?></p>
-            <div id='duplicates_list'></div>
+        <div>
+            <p id="no_dups_message" class="merge-modal-no-dups-message"><?php esc_html_e( 'No duplicates found.', 'disciple_tools' ); ?></p>
+            <div id="duplicates_list"></div>
         </div>
-
-
         <div class="grid-x grid-padding-x">
             <div class="cell">
-                <span style="float:right; bottom: 0;">
-                    <button class="button" data-close aria-label="Close reveal" type="button">
-                        <?php echo esc_html__( 'Close', 'disciple_tools' )?>
+                <span>
+                    <button class="button merge-modal-close" data-close aria-label="Close reveal" type="button">
+                        <?php esc_html_e( 'Close', 'disciple_tools' ); ?>
                     </button>
                 </span>
             </div>
@@ -36,11 +32,9 @@
             <span aria-hidden="true">&times;</span>
         </button>
     </div>
-
     <div class="reveal" id="merge-with-post-modal" data-reveal style="min-height:500px">
-        <h3><?php echo esc_html( sprintf( _x( "Merge %s", "Merge Contacts", 'disciple_tools' ), $post_settings["label_plural"] ?? $post_type ) )?></h3>
-        <p><?php echo esc_html( sprintf( _x( 'Merge this %1$s with another %2$s', "Merge this contact with another contact", 'disciple_tools' ), $post_settings["label_singular"] ?? $post_type, $post_settings["label_singular"] ?? $post_type ) )?></p>
-
+        <h3><?php echo esc_html( sprintf( _x( 'Merge %s', 'Merge Contacts', 'disciple_tools' ), $post_settings['label_plural'] ?? $post_type ) )?></h3>
+        <p><?php echo esc_html( sprintf( _x( 'Merge this %1$s with another %2$s', 'Merge this contact with another contact', 'disciple_tools' ), $post_settings['label_singular'] ?? $post_type, $post_settings['label_singular'] ?? $post_type ) )?></p>
         <div class="merge_with details">
             <var id="merge_with-result-container" class="result-container merge_with-result-container"></var>
             <div id="merge_with_t" name="form-merge_with">
@@ -48,7 +42,7 @@
                     <div class="typeahead__field">
                         <span class="typeahead__query">
                             <input class="js-typeahead-merge_with input-height"
-                                   name="merge_with[query]" placeholder="<?php echo esc_html( sprintf( _x( "Search %s", "Search 'something'", 'disciple_tools' ), $post_settings["label_plural"] ?? $post_type ) ) ?>"
+                                   name="merge_with[query]" placeholder="<?php echo esc_html( sprintf( _x( 'Search %s', "Search 'something'", 'disciple_tools' ), $post_settings['label_plural'] ?? $post_type ) ) ?>"
                                    autocomplete="off">
                         </span>
                         <span class="typeahead__button">
@@ -60,24 +54,22 @@
                 </div>
             </div>
         </div>
-
         <br>
         <div class="confirm-merge-with-post" style="display: none">
-            <p><span  id="name-of-post-to-merge"></span> <?php echo esc_html_x( "selected.", 'added to the end of a sentence', 'disciple_tools' ) ?></p>
-            <p><?php esc_html_e( "Click merge to continue.", 'disciple_tools' ) ?></p>
+            <p><span id="name-of-post-to-merge"></span> <?php echo esc_html_x( 'selected.', 'added to the end of a sentence', 'disciple_tools' ); ?></p>
+            <p><?php esc_html_e( 'Click merge to continue.', 'disciple_tools' ); ?></p>
         </div>
-
         <div class="grid-x">
             <button class="button button-cancel clear" data-close aria-label="Close reveal" type="button">
-                <?php echo esc_html__( 'Cancel', 'disciple_tools' )?>
+                <?php esc_html_e( 'Cancel', 'disciple_tools' ); ?>
             </button>
             <form
-                action='<?php echo esc_url( site_url() ); ?>/<?php echo esc_html( dt_get_post_type() ); ?>/mergedetails'
-                method='get'>
-                <input type='hidden' name='currentid' value='<?php echo esc_html( GET_THE_ID() ); ?>'/>
-                <input id="confirm-merge-with-post-id" type='hidden' name='dupeid' value=''/>
-                <button type='submit' class="button confirm-merge-with-post" style="display: none">
-                    <?php echo esc_html__( 'Merge', 'disciple_tools' ) ?>
+                action="<?php echo esc_url( site_url() ); ?>/<?php echo esc_html( dt_get_post_type() ); ?>/mergedetails"
+                method="get">
+                <input type="hidden" name="currentid" value="<?php echo esc_html( get_the_id() ); ?>"/>
+                <input id="confirm-merge-with-post-id" type="hidden" name="dupeid" value=""/>
+                <button type="submit" class="button confirm-merge-with-post" style="display: none;">
+                    <?php esc_html_e( 'Merge', 'disciple_tools' ); ?>
                 </button>
             </form>
             <button class="close-button" data-close aria-label="Close modal" type="button">
@@ -85,5 +77,4 @@
             </button>
         </div>
     </div>
-
 <?php } )(); ?>
