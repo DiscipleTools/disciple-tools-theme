7d5fce13fcb869b06a87f3380c1a479f12bfc8e3
Prykon <dariomanoukian@gmail.com>
2022-08-16 13:04:46 -0300

    Added icons to Translations and Get Involved tabs

--- dt-core/release-notifications.php
+++ dt-core/release-notifications.php
@@ -48,8 +48,18 @@ function dt_release_modal() {
                     <div class="dt-tab-wrapper">
                         <a href="#" id="release-modal-theme-news-tab" data-content="theme-news-content" class="dt-tab dt-tab-active">Theme News</a>
                         <a href="#" id="release-modal-plugin-new-tab" data-content="plugin-news-content" class="dt-tab">Plugins News</a>
-                        <a href="#" id="release-modal-translations-tab" data-content="translations-content" class="dt-tab">Translations</a>
-                        <a href="#" id="release-modal-get-involved-tab" data-content="get-involved-content" class="dt-tab">Get Involved</a>
+                        <a href="#" id="release-modal-translations-tab" data-content="translations-content" class="dt-tab">
+                            <svg class="dt-tab-icon" viewBox="0 0 24 24">
+                                <path fill="currentColor" d="M12.87,15.07L10.33,12.56L10.36,12.53C12.1,10.59 13.34,8.36 14.07,6H17V4H10V2H8V4H1V6H12.17C11.5,7.92 10.44,9.75 9,11.35C8.07,10.32 7.3,9.19 6.69,8H4.69C5.42,9.63 6.42,11.17 7.67,12.56L2.58,17.58L4,19L9,14L12.11,17.11L12.87,15.07M18.5,10H16.5L12,22H14L15.12,19H19.87L21,22H23L18.5,10M15.88,17L17.5,12.67L19.12,17H15.88Z" />
+                            </svg>
+                            Translations
+                        </a>
+                        <a href="#" id="release-modal-get-involved-tab" data-content="get-involved-content" class="dt-tab">
+                            <svg class="dt-tab-icon" viewBox="0 0 24 24">
+                                <path fill="currentColor" d="M20 17Q20.86 17 21.45 17.6T22.03 19L14 22L7 20V11H8.95L16.22 13.69Q17 14 17 14.81 17 15.28 16.66 15.63T15.8 16H13L11.25 15.33L10.92 16.27L13 17H20M16 3.23Q17.06 2 18.7 2 20.06 2 21 3T22 5.3Q22 6.33 21 7.76T19.03 10.15 16 13Q13.92 11.11 12.94 10.15T10.97 7.76 10 5.3Q10 3.94 10.97 3T13.31 2Q14.91 2 16 3.23M.984 11H5V22H.984V11Z" />
+                            </svg>
+                            Get Involved
+                        </a>
                     </div>
                     <div class="dt-tab-content" id="theme-news-content">
                         <p>
