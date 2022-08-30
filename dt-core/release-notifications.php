fb3cf0f7f5f7e342208e3e15f2dd43aa2f960f03
Prykon <dariomanoukian@gmail.com>
2022-08-30 12:52:39 -0300

    Updated tabs and their content

--- dt-core/release-notifications.php
+++ dt-core/release-notifications.php
@@ -11,9 +11,9 @@ function dt_release_modal() {
     if ( !is_user_logged_in() ){
         return;
     }
-    $url = dt_get_url_path( true );
+    $url = dt_get_url_path();
     //bail if not an a D.T front end page.
-    if ( !is_archive() && !is_single() && !isset( apply_filters( "desktop_navbar_menu_options", [] )[untrailingslashit( $url )] ) ){
+    if ( !is_archive() && !is_single() && !isset( apply_filters( "desktop_navbar_menu_options", [] )[str_replace( '/', '', $url )] ) ){
         return;
     }
     $show_notification_for_theme_version = '1.30.0'; // increment this number with each new release modal
@@ -25,9 +25,9 @@ function dt_release_modal() {
         $last_release_notification = '1.0.0';
     }
 
-    if ( version_compare( $last_release_notification, $show_notification_for_theme_version, '>=' ) ){
-        return;
-    }
+    // if ( version_compare( $last_release_notification, $show_notification_for_theme_version, '>=' ) ){
+    //     return;
+    // }
     require_once( get_template_directory().'/dt-core/libraries/parsedown/Parsedown.php' );
 
     update_user_meta( get_current_user_id(), 'dt_release_notification', $show_notification_for_theme_version );
@@ -40,26 +40,15 @@ function dt_release_modal() {
             <div id='release-modal' class='reveal medium' data-reveal>
                 <div class="release-banner">
                     <div class="release-banner-text">
-                        <h3>Release Announcement!</h3>
-                        <h4>Disciple.Tools Theme Version <?php echo esc_html( $show_notification_for_theme_version ); ?></h4>
+                        <h3><?php esc_html_e( 'Release Announcement!', 'disciple_tools' ); ?></h3>
+                        <h4><?php echo sprintf( esc_html__( 'Disciple.Tools Theme Version %1$s', 'disciple_tools' ), $show_notification_for_theme_version ); ?></h4>
                     </div>
                 </div>
                 <div id="release-modal-content">
                     <div class="dt-tab-wrapper">
-                        <a href="#" id="release-modal-theme-news-tab" data-content="theme-news-content" class="dt-tab dt-tab-active">Theme News</a>
-                        <a href="#" id="release-modal-plugin-new-tab" data-content="plugin-news-content" class="dt-tab">Plugins News</a>
-                        <a href="#" id="release-modal-translations-tab" data-content="translations-content" class="dt-tab">
-                            <svg class="dt-tab-icon" viewBox="0 0 24 24">
-                                <path fill="currentColor" d="M12.87,15.07L10.33,12.56L10.36,12.53C12.1,10.59 13.34,8.36 14.07,6H17V4H10V2H8V4H1V6H12.17C11.5,7.92 10.44,9.75 9,11.35C8.07,10.32 7.3,9.19 6.69,8H4.69C5.42,9.63 6.42,11.17 7.67,12.56L2.58,17.58L4,19L9,14L12.11,17.11L12.87,15.07M18.5,10H16.5L12,22H14L15.12,19H19.87L21,22H23L18.5,10M15.88,17L17.5,12.67L19.12,17H15.88Z" />
-                            </svg>
-                            Translations
-                        </a>
-                        <a href="#" id="release-modal-get-involved-tab" data-content="get-involved-content" class="dt-tab">
-                            <svg class="dt-tab-icon" viewBox="0 0 24 24">
-                                <path fill="currentColor" d="M20 17Q20.86 17 21.45 17.6T22.03 19L14 22L7 20V11H8.95L16.22 13.69Q17 14 17 14.81 17 15.28 16.66 15.63T15.8 16H13L11.25 15.33L10.92 16.27L13 17H20M16 3.23Q17.06 2 18.7 2 20.06 2 21 3T22 5.3Q22 6.33 21 7.76T19.03 10.15 16 13Q13.92 11.11 12.94 10.15T10.97 7.76 10 5.3Q10 3.94 10.97 3T13.31 2Q14.91 2 16 3.23M.984 11H5V22H.984V11Z" />
-                            </svg>
-                            Get Involved
-                        </a>
+                        <a href="#" id="release-modal-theme-news-tab" data-content="theme-news-content" class="dt-tab dt-tab-active"><?php esc_html_e( 'Theme News', 'disciple_tools' ); ?></a>
+                        <a href="#" id="release-modal-plugin-new-tab" data-content="plugin-news-content" class="dt-tab"><?php esc_html_e( 'Other News', 'disciple_tools' ); ?></a>
+                        <a href="#" id="release-modal-get-involved-tab" data-content="get-involved-content" class="dt-tab"><?php esc_html_e( 'Get Involved', 'disciple_tools' ); ?></a>
                     </div>
                     <div class="dt-tab-content" id="theme-news-content">
                         <p>
@@ -67,11 +56,11 @@ function dt_release_modal() {
                         </p>
                     </div>
                     <div class="dt-tab-content dt-hide-content" id="plugin-news-content">
-                        <h3 class="dt-tab-content-heading">Plugin news</h3>
+                        <h3 class="dt-tab-content-heading"><?php esc_html_e( 'Other News', 'disciple_tools' ); ?></h3>
                         <p>
                             <?php $plugin_news_links = dt_get_plugins_news_links(); ?>
                             <?php if ( !$plugin_news_links ) : ?>
-                                <i><?php esc_html_e( 'Plugin news not available.', 'disciple_tools' ); ?></i>
+                                <i><?php esc_html_e( 'Other news not available.', 'disciple_tools' ); ?></i>
                             <?php else : ?>
                                 <?php foreach ( $plugin_news_links as $plugin_news_link ) : ?>
                                     <li><a href="<?php echo esc_attr( $plugin_news_link['url'] ); ?>" target="_blank"><?php echo esc_html( $plugin_news_link['title'] ); ?></a></li>
@@ -82,18 +71,30 @@ function dt_release_modal() {
                             <a href="https://disciple.tools/news-categories/dt-plugin-releases/" target="_blank"><?php echo esc_html( 'Go to plugin news page', 'disciple_tools' ); ?></a>
                         </p>
                     </div>
-                    <div class="dt-tab-content dt-hide-content" id="translations-content">
-                        <h3 class="dt-tab-content-heading">Translations</h3>
-                        <p>See a problem with your language or don't see your language?
-                        <br>
-                        <a href="https://poeditor.com/join/project/KcPvw3oaKD">You can contribute here!</a></p>
-                    </div>
                     <div class="dt-tab-content dt-hide-content" id="get-involved-content">
-                        <h3 class="dt-tab-content-heading">Get Involved</h3>
-                        <p>Want to help with coding or documentation? <a href="https://disciple.tools/join-the-community/">Click here</a></p>
-                        <p>No coding skills but want to help out financially?
+                        <p>
+                            <h5>
+                                <svg style="width:25px;" viewBox="0 0 24 24"><path fill="currentColor" d="M20 17Q20.86 17 21.45 17.6T22.03 19L14 22L7 20V11H8.95L16.22 13.69Q17 14 17 14.81 17 15.28 16.66 15.63T15.8 16H13L11.25 15.33L10.92 16.27L13 17H20M16 3.23Q17.06 2 18.7 2 20.06 2 21 3T22 5.3Q22 6.33 21 7.76T19.03 10.15 16 13Q13.92 11.11 12.94 10.15T10.97 7.76 10 5.3Q10 3.94 10.97 3T13.31 2Q14.91 2 16 3.23M.984 11H5V22H.984V11Z" /></svg>
+                                <b><?php esc_html_e( 'Give', 'disciple_tools' ); ?></b>
+                            </h5>
+                            <p><?php esc_html_e( 'Make Disciple.Tools better by joining us financially.', 'disciple_tools' ); ?> Join us <a href="https://disciple.tools/give/" target="_blank">here</a>.</p>
+                        </p>
                         <br>
-                        <a href="https://disciple.tools/give/">Donate to disciple.tools through gospelambition.org</a></p>
+                        <p>
+                            <h5>
+                                <svg style="width:25px;" viewBox="0 0 24 24"><path fill="currentColor" d="M12.87,15.07L10.33,12.56L10.36,12.53C12.1,10.59 13.34,8.36 14.07,6H17V4H10V2H8V4H1V6H12.17C11.5,7.92 10.44,9.75 9,11.35C8.07,10.32 7.3,9.19 6.69,8H4.69C5.42,9.63 6.42,11.17 7.67,12.56L2.58,17.58L4,19L9,14L12.11,17.11L12.87,15.07M18.5,10H16.5L12,22H14L15.12,19H19.87L21,22H23L18.5,10M15.88,17L17.5,12.67L19.12,17H15.88Z" /></svg>
+                                <b><?php esc_html_e( 'Translate', 'disciple_tools' ); ?></b>
+                            </h5>
+                            <p>Translate Disciple.Tools into your language or help make a translation better <a href="https://poeditor.com/join/project/KcPvw3oaKD" target="_blank">here</a>.</p>
+                        </p>
+                        <br>
+                        <p>
+                            <h5>
+                                <svg style="width:24px;height:24px" viewBox="0 0 24 24"><path fill="currentColor" d="M12.89,3L14.85,3.4L11.11,21L9.15,20.6L12.89,3M19.59,12L16,8.41V5.58L22.42,12L16,18.41V15.58L19.59,12M1.58,12L8,5.58V8.41L4.41,12L8,15.58V18.41L1.58,12Z" /></svg>
+                                <b><?php esc_html_e( 'Build or Document', 'disciple_tools' ); ?></b>
+                            </h5>
+                            <p><?php esc_html_e( 'Make Disciple.Tools better by building a feature or helping us make the documentation better.', 'disciple_tools' ); ?> Find out more <a href="https://disciple.tools/join-the-community/" target="_blank">here</a>.</p>
+                        </p>
                     </div>
                 </div>
                 <br>
@@ -108,7 +109,7 @@ function dt_release_modal() {
             var currentVisibleTabName = $('.dt-tab-active').data('content');
             $('#' + currentVisibleTabName ).addClass('dt-hide-content');
             $('.dt-tab-active').removeClass('dt-tab-active');
-
+            
             var desiredVisibleTabName = $(this).data('content');
             $(this).addClass('dt-tab-active');
             $( '#' + desiredVisibleTabName ).removeClass('dt-hide-content');
