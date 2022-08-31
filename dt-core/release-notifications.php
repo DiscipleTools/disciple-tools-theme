1a7e35b5f7abbf379921272bc3dc24e2f3aa3860
Prykon <dariomanoukian@gmail.com>
2022-08-31 12:32:52 -0300

    Fixed CTA link wording

--- dt-core/release-notifications.php
+++ dt-core/release-notifications.php
@@ -25,9 +25,9 @@ function dt_release_modal() {
         $last_release_notification = '1.0.0';
     }
 
-    if ( version_compare( $last_release_notification, $show_notification_for_theme_version, '>=' ) ){
-        return;
-    }
+    // if ( version_compare( $last_release_notification, $show_notification_for_theme_version, '>=' ) ){
+    //     return;
+    // }
     require_once( get_template_directory().'/dt-core/libraries/parsedown/Parsedown.php' );
 
     update_user_meta( get_current_user_id(), 'dt_release_notification', $show_notification_for_theme_version );
@@ -77,7 +77,7 @@ function dt_release_modal() {
                                 <svg style="width:25px;" viewBox="0 0 24 24"><path fill="currentColor" d="M20 17Q20.86 17 21.45 17.6T22.03 19L14 22L7 20V11H8.95L16.22 13.69Q17 14 17 14.81 17 15.28 16.66 15.63T15.8 16H13L11.25 15.33L10.92 16.27L13 17H20M16 3.23Q17.06 2 18.7 2 20.06 2 21 3T22 5.3Q22 6.33 21 7.76T19.03 10.15 16 13Q13.92 11.11 12.94 10.15T10.97 7.76 10 5.3Q10 3.94 10.97 3T13.31 2Q14.91 2 16 3.23M.984 11H5V22H.984V11Z" /></svg>
                                 <b><?php esc_html_e( 'Give', 'disciple_tools' ); ?></b>
                             </h5>
-                            <p><?php esc_html_e( 'Make Disciple.Tools better by joining us financially.', 'disciple_tools' ); ?> Join us <a href="https://disciple.tools/give/" target="_blank">here</a>.</p>
+                            <p><?php esc_html_e( 'Make Disciple.Tools better by joining us financially.', 'disciple_tools' ); ?><br><a href="https://disciple.tools/give/" target="_blank"><?php esc_html_e( 'Join us here', 'disciple_tools' ); ?></a></p>
                         </p>
                         <br>
                         <p>
@@ -85,7 +85,7 @@ function dt_release_modal() {
                                 <svg style="width:25px;" viewBox="0 0 24 24"><path fill="currentColor" d="M12.87,15.07L10.33,12.56L10.36,12.53C12.1,10.59 13.34,8.36 14.07,6H17V4H10V2H8V4H1V6H12.17C11.5,7.92 10.44,9.75 9,11.35C8.07,10.32 7.3,9.19 6.69,8H4.69C5.42,9.63 6.42,11.17 7.67,12.56L2.58,17.58L4,19L9,14L12.11,17.11L12.87,15.07M18.5,10H16.5L12,22H14L15.12,19H19.87L21,22H23L18.5,10M15.88,17L17.5,12.67L19.12,17H15.88Z" /></svg>
                                 <b><?php esc_html_e( 'Translate', 'disciple_tools' ); ?></b>
                             </h5>
-                            <p>Translate Disciple.Tools into your language or help make a translation better <a href="https://poeditor.com/join/project/KcPvw3oaKD" target="_blank">here</a>.</p>
+                            <p><?php esc_html_e( 'Translate Disciple.Tools into your language or help make a translation better.', 'disciple_tools' ); ?><br><a href="https://poeditor.com/join/project/KcPvw3oaKD" target="_blank"><?php esc_html_e( 'Click here', 'disciple_tools' ); ?></a></p>
                         </p>
                         <br>
                         <p>
@@ -93,7 +93,7 @@ function dt_release_modal() {
                                 <svg style="width:24px;height:24px" viewBox="0 0 24 24"><path fill="currentColor" d="M12.89,3L14.85,3.4L11.11,21L9.15,20.6L12.89,3M19.59,12L16,8.41V5.58L22.42,12L16,18.41V15.58L19.59,12M1.58,12L8,5.58V8.41L4.41,12L8,15.58V18.41L1.58,12Z" /></svg>
                                 <b><?php esc_html_e( 'Build or Document', 'disciple_tools' ); ?></b>
                             </h5>
-                            <p><?php esc_html_e( 'Make Disciple.Tools better by building a feature or helping us make the documentation better.', 'disciple_tools' ); ?> Find out more <a href="https://disciple.tools/join-the-community/" target="_blank">here</a>.</p>
+                            <p><?php esc_html_e( 'Make Disciple.Tools better by building a feature or helping us make the documentation better.', 'disciple_tools' ); ?><br><a href="https://disciple.tools/join-the-community/" target="_blank"><?php esc_html_e( 'Find out more', 'disciple_tools' ); ?></a></p>
                         </p>
                     </div>
                 </div>
