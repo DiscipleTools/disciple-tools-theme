cc8a122f53b6f6f4a0c47420c1715f7f042de098
kodinkat <me@karlkatewu.com>
2022-10-24 10:46:08 +0100

    Addressed sanitize code smells for arrays

--- dt-core/admin/menu/tabs/tab-custom-lists.php
+++ dt-core/admin/menu/tabs/tab-custom-lists.php
@@ -372,7 +372,7 @@ class Disciple_Tools_Tab_Custom_Lists extends Disciple_Tools_Abstract_Menu_Base
                 return;
             }
 
-            $channel_fields        = isset( $_POST['channel_fields'] ) ? array_keys( $_POST['channel_fields'] ) : [];
+            $channel_fields        = isset( $_POST['channel_fields'] ) ? array_keys( dt_recursive_sanitize_array( $_POST['channel_fields'] ) ) : [];
             $langs                 = dt_get_available_languages();
             $custom_field_options  = dt_get_option( 'dt_field_customizations' );
             $custom_contact_fields = $custom_field_options['contacts'];
