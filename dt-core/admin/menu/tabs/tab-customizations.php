b273bf215316eb7d83122c9147c84c650574f3ef
Prykon <dariomanoukian@gmail.com>
2022-10-26 16:49:16 -0300

    Added functionality for displaying fields in post types without tiles

--- dt-core/admin/menu/tabs/tab-customizations.php
+++ dt-core/admin/menu/tabs/tab-customizations.php
@@ -396,16 +396,75 @@ class Disciple_Tools_Customizations_Tab extends Disciple_Tools_Abstract_Menu_Bas
     private function show_tile_settings() {
         $post_type = self::get_parameter( 'post_type' );
         $post_tiles = DT_Posts::get_post_settings( $post_type, false );
+        if ( !isset( $post_tiles['tiles'] ) ) {
+            self::display_field_rundown_without_tiles();
+            return;
+        }
+        self::display_field_rundown_with_tiles();
+    }
+
+    private function display_field_rundown_without_tiles() {
+        $post_type = self::get_parameter( 'post_type' );
+        $post_tiles = DT_Posts::get_post_settings( $post_type, false );
+        ?>
+        <div class="field-settings-table">
+            <!-- START TOGGLED FIELD ITEMS -->
+            <?php foreach ( $post_tiles['fields'] as $field_key => $field_settings ) :
+                if ( !isset( $field_settings['default'] ) || $field_settings['default'] === '' ) : ?>
+                    <div class="field-settings-table-field-name">
+                        <span class="sortable">::</span>
+                        <span class="field-name-content" data-field-name="<?php echo esc_attr( $field_key ); ?>">    
+                            <?php echo esc_html( $field_settings['name'] ); ?>
+                        </span>
+                    </div>
+                <?php else : ?>
+                    <div class="field-settings-table-field-name expandable" data-field-name="<?php echo esc_attr( $field_key ); ?>">
+                        <span class="sortable">::</span>
+                        <span class="expand-icon" style="padding-left: 16px;">+</span>
+                        <?php echo esc_html( $field_settings['name'] ); ?>
+                    </div>
+                    <!-- START TOGGLED ITEMS -->
+                    <div class="field-settings-table-child-toggle">
+                        <?php foreach ( $field_settings as $key => $value ) : ?>
+                            <?php if ( $key === 'default' && !empty( $field_settings['default'] ) ) : ?>
+                                <?php foreach ( $value as $v ) {
+                                    $label = 'NULL';
+                                    if ( isset( $v['label'] ) || !empty( $v['label'] ) ) {
+                                        $label = $v['label'];
+                                    }
+                                }
+                                ?>
+                                <div class="field-settings-table-field-option">
+                                    <span class="sortable">::</span>
+                                    <span class="field-name-content" style="padding-left: 16px;"><?php echo esc_html( $label ); ?></span>
+                                </div>
+                            <?php endif; ?>
+                        <?php endforeach; ?>
+                        <div class="field-settings-table-field-option">
+                            <span class="sortable">::</span>
+                            <span style="margin-left: 32px;"><?php echo esc_html( 'new field option', 'disciple_tools' ); ?></span>
+                        </div>
+                    </div>
+                    <!-- END TOGGLED ITEMS -->
+                <?php endif; ?>
+            <?php endforeach; ?>
+            <!-- END TOGGLED FIELD ITEMS -->
+            <div class="field-settings-table-field-name expandable">
+                <span class="sortable">::</span>
+                <span class="field-name-content" data-field-name="_add_new_field">    
+                    [<?php echo esc_html( 'add new field', 'disciple_tools' ); ?>]
+                </span>
+            </div>
+        </div>
+        <?php
+    }
+
+    private function display_field_rundown_with_tiles() {
+        $post_type = self::get_parameter( 'post_type' );
+        $post_tiles = DT_Posts::get_post_settings( $post_type, false );
         ?>
         <!-- START TABLE -->
         <div class="field-settings-table">
-            <?php if ( !isset( $post_tiles['tiles'] ) ) : ?>
-                <div>This post type doesn't have tiles.<br>
-                    <b>Todo:</b> add tile-less field display functionality.
-                </div>
-                <?php
-                return;
-                endif; ?>
             <?php foreach ( $post_tiles['tiles'] as $tile_key => $tile_value ) : ?>
                 <!-- START TILE -->
                 <div class="field-settings-table-tile-name expandable" data-tile-key="<?php echo esc_attr( $tile_key ); ?>">
@@ -463,11 +522,11 @@ class Disciple_Tools_Customizations_Tab extends Disciple_Tools_Abstract_Menu_Bas
                     <?php endforeach; ?>
                     <!-- END TOGGLED FIELD ITEMS -->
                     <div class="field-settings-table-field-name expandable">
-                            <span class="sortable">::</span>
-                            <span class="field-name-content" data-field-name="_add_new_field">    
-                                [<?php echo esc_html( 'add new field', 'disciple_tools' ); ?>]
-                            </span>
-                        </div>
+                        <span class="sortable">::</span>
+                        <span class="field-name-content" data-field-name="_add_new_field">    
+                            [<?php echo esc_html( 'add new field', 'disciple_tools' ); ?>]
+                        </span>
+                    </div>
                 </div>
             <?php endforeach; ?>
         </div>
