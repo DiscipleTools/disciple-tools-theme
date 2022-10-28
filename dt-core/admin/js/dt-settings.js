7e119c847677391b84a39e23c62170e66cfa7ace
Prykon <dariomanoukian@gmail.com>
2022-10-28 17:40:52 -0300

    Made tile preview update automatically on new field creation

--- dt-core/admin/js/dt-settings.js
+++ dt-core/admin/js/dt-settings.js
@@ -506,45 +506,51 @@ jQuery(document).ready(function($) {
         var new_field_private = $(`#new-field-private-${new_field_tile}`).is(':checked');
 
         API.new_field(post_type, new_field_tile, new_field_name, new_field_type, new_field_private).promise().then(function(field_key) {
+            window['field_settings']['post_type_settings']['fields'][field_key] = {
+                'name':new_field_name,
+                'private':new_field_private,
+                'tile':new_field_tile,
+                'type':new_field_type,
+            };
             var new_field_nonexpandable_html = `
-            <div class="field-settings-table-field-name">
-                <span class="sortable">⋮⋮</span>
-                <span class="field-name-content" data-key="${field_key}" data-parent-tile-key="${new_field_tile}">
-                    ${new_field_name}
-                    <svg style="width:24px;height:24px;margin-left:6px;vertical-align:middle;" viewBox="0 0 24 24">
-                        <path fill="green" d="M20,4C21.11,4 22,4.89 22,6V18C22,19.11 21.11,20 20,20H4C2.89,20 2,19.11 2,18V6C2,4.89 2.89,4 4,4H20M8.5,15V9H7.25V12.5L4.75,9H3.5V15H4.75V11.5L7.3,15H8.5M13.5,10.26V9H9.5V15H13.5V13.75H11V12.64H13.5V11.38H11V10.26H13.5M20.5,14V9H19.25V13.5H18.13V10H16.88V13.5H15.75V9H14.5V14A1,1 0 0,0 15.5,15H19.5A1,1 0 0,0 20.5,14Z" />
-                    </svg>
-                </span>
-            </div>
+                <div class="field-settings-table-field-name">
+                    <span class="sortable">⋮⋮</span>
+                    <span class="field-name-content" data-key="${field_key}" data-parent-tile-key="${new_field_tile}">
+                        ${new_field_name}
+                        <svg style="width:24px;height:24px;margin-left:6px;vertical-align:middle;" viewBox="0 0 24 24">
+                            <path fill="green" d="M20,4C21.11,4 22,4.89 22,6V18C22,19.11 21.11,20 20,20H4C2.89,20 2,19.11 2,18V6C2,4.89 2.89,4 4,4H20M8.5,15V9H7.25V12.5L4.75,9H3.5V15H4.75V11.5L7.3,15H8.5M13.5,10.26V9H9.5V15H13.5V13.75H11V12.64H13.5V11.38H11V10.26H13.5M20.5,14V9H19.25V13.5H18.13V10H16.88V13.5H15.75V9H14.5V14A1,1 0 0,0 15.5,15H19.5A1,1 0 0,0 20.5,14Z" />
+                        </svg>
+                    </span>
+                </div>
             `;
 
             var new_field_expandable_html = `
-            <div class="field-settings-table-field-name expandable" data-modal="edit-field" data-key="${field_key}"  data-parent-tile-key="${new_field_tile}">
-                <span class="sortable">⋮⋮</span>
-                <span class="expand-icon" style="padding-left: 16px;">+</span>
-                <span style="vertical-align: sub;">
-                    ${new_field_name}
-                    <svg style="width:24px;height:24px;margin-left:6px;vertical-align:middle;" viewBox="0 0 24 24">
-                        <path fill="green" d="M20,4C21.11,4 22,4.89 22,6V18C22,19.11 21.11,20 20,20H4C2.89,20 2,19.11 2,18V6C2,4.89 2.89,4 4,4H20M8.5,15V9H7.25V12.5L4.75,9H3.5V15H4.75V11.5L7.3,15H8.5M13.5,10.26V9H9.5V15H13.5V13.75H11V12.64H13.5V11.38H11V10.26H13.5M20.5,14V9H19.25V13.5H18.13V10H16.88V13.5H15.75V9H14.5V14A1,1 0 0,0 15.5,15H19.5A1,1 0 0,0 20.5,14Z" />
-                    </svg>
-                </span>
-                <span class="edit-icon"></span>
-            </div>
-        <!-- START TOGGLED ITEMS -->
-            <div class="field-settings-table-child-toggle">
-                <div class="field-settings-table-field-option">
+                <div class="field-settings-table-field-name expandable" data-modal="edit-field" data-key="${field_key}"  data-parent-tile-key="${new_field_tile}">
                     <span class="sortable">⋮⋮</span>
-                    <span style="margin-left: 32px;">new field option</span>
+                    <span class="expand-icon" style="padding-left: 16px;">+</span>
+                    <span style="vertical-align: sub;">
+                        ${new_field_name}
+                        <svg style="width:24px;height:24px;margin-left:6px;vertical-align:middle;" viewBox="0 0 24 24">
+                            <path fill="green" d="M20,4C21.11,4 22,4.89 22,6V18C22,19.11 21.11,20 20,20H4C2.89,20 2,19.11 2,18V6C2,4.89 2.89,4 4,4H20M8.5,15V9H7.25V12.5L4.75,9H3.5V15H4.75V11.5L7.3,15H8.5M13.5,10.26V9H9.5V15H13.5V13.75H11V12.64H13.5V11.38H11V10.26H13.5M20.5,14V9H19.25V13.5H18.13V10H16.88V13.5H15.75V9H14.5V14A1,1 0 0,0 15.5,15H19.5A1,1 0 0,0 20.5,14Z" />
+                        </svg>
+                    </span>
+                    <span class="edit-icon"></span>
                 </div>
-            </div>
-        <!-- END TOGGLED ITEMS -->
+                <!-- START TOGGLED ITEMS -->
+                <div class="field-settings-table-child-toggle">
+                    <div class="field-settings-table-field-option">
+                        <span class="sortable">⋮⋮</span>
+                        <span style="margin-left: 32px;">new field option</span>
+                    </div>
+                </div>
+                <!-- END TOGGLED ITEMS -->
             `;
             var new_field_html = new_field_nonexpandable_html;
             if(['key_select', 'multi_select'].indexOf(new_field_type) > -1) {
                 new_field_html = new_field_expandable_html;
             }
-            console.log(new_field_html);
             $(`.field-name-content.add-new-field[data-parent-tile-key='${new_field_tile}']`).parent().before(new_field_html);
+            show_preview_tile(new_field_tile);
             closeModal();
         });
     });
