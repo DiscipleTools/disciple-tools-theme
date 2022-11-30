52d1aa639a3a1ecf62358ee600b83588206baad6
corsac <corsacca@gmail.com>
2022-11-30 11:14:21 +0100

    phpcbf and and rebuild styles

--- dt-assets/parts/modals/modal-record-history.php
+++ dt-assets/parts/modals/modal-record-history.php
@@ -3,7 +3,7 @@ global $post;
 ?>
 
 <div class="large reveal" id="record_history_modal" data-reveal data-reset-on-close style="padding-bottom: 50px;">
-    <h3><?php echo esc_html( sprintf( _x( "%s Record History", "Record History", 'disciple_tools' ), $post->post_title ) ); ?></h3>
+    <h3><?php echo esc_html( sprintf( _x( '%s Record History', 'Record History', 'disciple_tools' ), $post->post_title ) ); ?></h3>
     <hr>
 
     <div class="grid-container">
@@ -18,7 +18,7 @@ global $post;
                     <tbody style="border: none;">
                     <tr style="border: none;">
                         <td style="vertical-align: top;">
-                            <span><?php echo esc_html( _x( "Show All", "Show All", 'disciple_tools' ) ) ?></span>
+                            <span><?php echo esc_html( _x( 'Show All', 'Show All', 'disciple_tools' ) ) ?></span>
                         </td>
                         <td>
                             <div class="switch tiny">
@@ -26,7 +26,7 @@ global $post;
                                        name="record_history_all_activities_switch" checked>
                                 <label class="switch-paddle" for="record_history_all_activities_switch">
                         <span
-                            class="show-for-sr"><?php echo esc_html( _x( "Show All", "Show All", 'disciple_tools' ) ) ?></span>
+                            class="show-for-sr"><?php echo esc_html( _x( 'Show All', 'Show All', 'disciple_tools' ) ) ?></span>
                                 </label>
                             </div>
                         </td>
