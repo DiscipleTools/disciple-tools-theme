76b8db8b89bf5d457befb280e9f9b453d4768c40
Prykon <dariomanoukian@gmail.com>
2022-12-06 11:55:52 -0300

    Made non-expandable field options be editable without refreshing the page

--- dt-core/admin/js/dt-settings.js
+++ dt-core/admin/js/dt-settings.js
@@ -690,7 +690,7 @@ jQuery(document).ready(function($) {
             var field_key = result['key'];
             window['field_settings']['post_type_settings']['fields'][field_key] = result;
             var new_field_nonexpandable_html = `
-                <div class="field-settings-table-field-name submenu-highlight" data-parent-tile-key="${new_field_tile}" data-field-name="${field_key}">
+                <div class="field-settings-table-field-name submenu-highlight" data-parent-tile-key="${new_field_tile}" data-key="${field_key}" data-modal="edit-field">
                     <span class="sortable">⋮⋮</span>
                     <span class="field-name-content" style="margin-left: 16px;" data-parent-tile="${new_field_tile}" data-key="${field_key}">
                         ${new_field_name}
@@ -698,6 +698,7 @@ jQuery(document).ready(function($) {
                             <path fill="green" d="M20,4C21.11,4 22,4.89 22,6V18C22,19.11 21.11,20 20,20H4C2.89,20 2,19.11 2,18V6C2,4.89 2.89,4 4,4H20M8.5,15V9H7.25V12.5L4.75,9H3.5V15H4.75V11.5L7.3,15H8.5M13.5,10.26V9H9.5V15H13.5V13.75H11V12.64H13.5V11.38H11V10.26H13.5M20.5,14V9H19.25V13.5H18.13V10H16.88V13.5H15.75V9H14.5V14A1,1 0 0,0 15.5,15H19.5A1,1 0 0,0 20.5,14Z" />
                         </svg>
                     </span>
+                    <span class="edit-icon"></span>
                 </div>
             `;
 
