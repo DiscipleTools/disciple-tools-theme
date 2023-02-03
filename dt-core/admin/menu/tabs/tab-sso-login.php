f148e62f4bd3a42c56240895d08c113e99f00fb2
Nath <nathinabob@gmail.com>
2023-02-03 10:50:41 +0000

    feat: add functions to access fields; add SSO fields

--- dt-core/admin/menu/tabs/tab-sso-login.php
+++ dt-core/admin/menu/tabs/tab-sso-login.php
@@ -70,12 +70,7 @@ class Disciple_Tools_SSO_Login extends Disciple_Tools_Abstract_Menu_Base
         $is_dt = class_exists( 'Disciple_Tools' );
         $tabs = [];
         foreach ( $vars as $val ) {
-            if ( $is_dt === $val['requires_dt'] ) {
-                $tabs[$val['tab']] = ucwords( $val['tab'] );
-            }
-            elseif ( ! $val['requires_dt'] ) {
-                $tabs[$val['tab']] = ucwords( $val['tab'] );
-            }
+            $tabs[$val['tab']] = ucwords( str_replace( '_', ' ', $val['tab'] ) );
         }
         ?>
         <div class="wrap">
@@ -103,12 +98,7 @@ class Disciple_Tools_SSO_Login extends Disciple_Tools_Abstract_Menu_Base
                                     if ( ! empty( $vars ) ) {
                                         foreach ( $vars as $key => $value ) {
                                             if ( $tab === $value['tab'] ) {
-                                                if ( $is_dt === $value['requires_dt'] ) {
-                                                    $this->tab( $value );
-                                                }
-                                                elseif ( ! $value['requires_dt'] ) {
-                                                    $this->tab( $value );
-                                                }
+                                                $this->tab( $value );
                                             }
                                         }
                                     }
@@ -186,7 +176,7 @@ class Disciple_Tools_SSO_Login extends Disciple_Tools_Abstract_Menu_Base
     }
 
     public function process_postback(){
-        $vars = dt_sso_login_fields();
+        $vars = self::dt_sso_login_fields();
 
         // process POST
         if ( isset( $_POST[$this->token.'_nonce'] )
@@ -200,13 +190,217 @@ class Disciple_Tools_SSO_Login extends Disciple_Tools_Abstract_Menu_Base
             }
 
             if ( isset( $params['delete'] ) ) {
-                delete_option( 'dt_sso_login_fields' );
+                delete_site_option( 'dt_sso_login_fields' );
             } else {
-                update_option( 'dt_sso_login_fields', $vars, true );
+                update_site_option( 'dt_sso_login_fields', $vars );
             }
         }
 
         return $vars;
+    }
+
+    public static function dt_sso_login_fields() {
+        $defaults = [
+
+            // general
+            'general_label' => [
+                'tab' => 'general',
+                'key' => 'general_label',
+                'label' => 'GENERAL',
+                'description' => '',
+                'value' => '',
+                'type' => 'label',
+            ],
+            'login_method' => [
+                'tab' => 'general',
+                'key' => 'login_method',
+                'label' => 'Login Method',
+                'description' => 'Login like Wordpress normally does or like a mobile app.',
+                'default' => [
+                    'wordpress' => DT_Login_Methods::WORDPRESS,
+                    'mobile' => DT_Login_Methods::MOBILE,
+                ],
+                'value' => 'wordpress',
+                'type' => 'select',
+            ],
+            'login_redirect_to' => [
+                'tab' => 'general',
+                'key' => 'login_redirect_to',
+                'label' => 'Login Redirect',
+                'description' => 'Url to redirect the user to after successful login',
+                'value' => '/',
+                'type' => 'text',
+
+            ],
+
+            'shortcode_firebase_logon_buttons' => [
+                'tab' => 'shortcodes',
+                'key' => 'shortcode_firebase_logon_buttons',
+                'label' => 'Firebase Logon Buttons',
+                'description' => '[dt_firebase_login_ui]',
+                'description_2' => '',
+                'value' => '',
+                'type' => 'label',
+            ],
+
+            'shortcode_firebase_logout_script' => [
+                'tab' => 'shortcodes',
+                'key' => 'shortcode_firebase_logout_script',
+                'label' => 'shortcode to add on your logout screen to log the user out if using the mobile login',
+                'description' => '[dt_firebase_logout_script]',
+                'description_2' => '',
+                'value' => '',
+                'type' => 'label',
+            ],
+
+
+            // firebase
+            'firebase_config_label' => [
+                'tab' => 'firebase',
+                'key' => 'firebase_config_label',
+                'label' => 'Where to find the config details',
+                'description' => 'Go to your firebase console and in the project settings get the config details from your webapp https://console.firebase.google.com/',
+                'description_2' => '',
+                'value' => '',
+                'type' => 'label',
+            ],
+            'firebase_api_key' => [
+                'tab' => 'firebase',
+                'key' => 'firebase_api_key',
+                'label' => 'Firebase API Key',
+                'description' => '',
+                'value' => '',
+                'type' => 'text',
+            ],
+            'firebase_project_id' => [
+                'tab' => 'firebase',
+                'key' => 'firebase_project_id',
+                'label' => 'Firebase Project ID',
+                'description' => '',
+                'value' => '',
+                'type' => 'text',
+            ],
+            'firebase_app_id' => [
+                'tab' => 'firebase',
+                'key' => 'firebase_app_id',
+                'label' => 'Firebase App ID',
+                'description' => '',
+                'value' => '',
+                'type' => 'text',
+            ],
+
+            'identity_providers' => [
+                'tab' => 'identity_providers',
+                'key' => 'identity_providers_label',
+                'label' => 'SSO Identity Providers',
+                'description' => 'Choose which identity providers you are using. These also need to be set up in the Firebase project.',
+                'value' => '',
+                'type' => 'label',
+            ],
+            'identity_providers_email' => [
+                'tab' => 'identity_providers',
+                'key' => 'identity_providers_email',
+                'label' => 'Email and Password',
+                'description' => '',
+                'value' => 'on',
+                'type' => 'select',
+                'default' => [
+                    'on' => 'on',
+                    'off' => 'off',
+                ],
+            ],
+            'identity_providers_google' => [
+                'tab' => 'identity_providers',
+                'key' => 'identity_providers_google',
+                'label' => 'Google',
+                'description' => '',
+                'value' => 'off',
+                'type' => 'select',
+                'default' => [
+                    'on' => 'on',
+                    'off' => 'off',
+                ],
+            ],
+            'identity_providers_facebook' => [
+                'tab' => 'identity_providers',
+                'key' => 'identity_providers_facebook',
+                'label' => 'Facebook',
+                'description' => '',
+                'value' => 'off',
+                'type' => 'select',
+                'default' => [
+                    'on' => 'on',
+                    'off' => 'off',
+                ],
+            ],
+            'identity_providers_microsoft' => [
+                'tab' => 'identity_providers',
+                'key' => 'identity_providers_microsoft',
+                'label' => 'Microsoft',
+                'description' => '',
+                'value' => 'off',
+                'type' => 'select',
+                'default' => [
+                    'on' => 'on',
+                    'off' => 'off',
+                ],
+            ],
+            'identity_providers_apple' => [
+                'tab' => 'identity_providers',
+                'key' => 'identity_providers_apple',
+                'label' => 'Apple',
+                'description' => '',
+                'value' => 'off',
+                'type' => 'select',
+                'default' => [
+                    'on' => 'on',
+                    'off' => 'off',
+                ],
+            ],
+            'identity_providers_twitter' => [
+                'tab' => 'identity_providers',
+                'key' => 'identity_providers_twitter',
+                'label' => 'Twitter',
+                'description' => '',
+                'value' => 'off',
+                'type' => 'select',
+                'default' => [
+                    'on' => 'on',
+                    'off' => 'off',
+                ],
+            ],
+
+        ];
+
+        $defaults_count = count( $defaults );
+
+        $saved_fields = get_option( 'dt_sso_login_fields', [] );
+        $saved_count = count( $saved_fields );
+
+        $fields = wp_parse_args( $saved_fields, $defaults );
+
+        if ( $defaults_count !== $saved_count ) {
+            update_option( 'dt_sso_login_fields', $fields, true );
+        }
+
+        return $fields;
+    }
+
+    /**
+     * Get the value from the fields array
+     * @param string $field_name
+     * @return mixed
+     */
+    public static function dt_sso_login_field( string $field_name ) {
+        $fields = self::dt_sso_login_fields();
+
+        if ( !isset( $fields[$field_name] ) ) {
+            return false;
+        }
+
+        $value = $fields[$field_name]['value'];
+
+        return $value;
     }
 
 }
