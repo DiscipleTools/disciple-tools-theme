317b7336ff093f544edafc6d613c758b2807d77f
Prykon <dariomanoukian@gmail.com>
2023-03-02 15:53:53 -0300

    Removed commented code

--- dt-core/admin/menu/tabs/tab-featured-extensions.php
+++ dt-core/admin/menu/tabs/tab-featured-extensions.php
@@ -163,36 +163,6 @@ class Disciple_Tools_Tab_Featured_Extensions extends Disciple_Tools_Abstract_Men
 
     //main page
     public function box_message( $tab ) {
-        // //check for actions
-        // if ( isset( $_POST['activate'] ) && is_admin() && isset( $_POST['_ajax_nonce'] ) && check_ajax_referer( 'portal-nonce', sanitize_key( $_POST['_ajax_nonce'] ) ) && current_user_can( 'manage_dt' ) ) {
-        //     //activate the plugin
-        //     activate_plugin( sanitize_text_field( wp_unslash( $_POST['activate'] ) ) );
-        //     exit;
-        // }
-        // elseif ( isset( $_POST['install'] ) && is_admin() && isset( $_POST['_ajax_nonce'] )
-        //     && check_ajax_referer( 'portal-nonce', sanitize_key( $_POST['_ajax_nonce'] ) )
-        //     && ( ( is_multisite() && is_super_admin() ) || ( ! is_multisite() && current_user_can( 'manage_dt' ) ) ) ) {
-        //     //check for admin or multisite super admin
-        //     //install plugin
-        //     $this->install_plugin( sanitize_text_field( wp_unslash( $_POST['install'] ) ) );
-        //     exit;
-        // }
-
-        // elseif ( isset( $_POST['uninstall'] ) && is_admin() && isset( $_POST['_ajax_nonce'] )
-        //     && check_ajax_referer( 'portal-nonce', sanitize_key( $_POST['_ajax_nonce'] ) )
-        //     && ( ( is_multisite() && is_super_admin() ) || ( ! is_multisite() && current_user_can( 'manage_dt' ) ) ) ) {
-        //     //check for admin or multisite super admin
-        //     //uninstall plugin
-        //     delete_plugins( [ sanitize_text_field( wp_unslash( $_POST['uninstall'] ) ) ] );
-        //     exit;
-        // }
-
-        // elseif ( isset( $_POST['deactivate'] ) && is_admin() && isset( $_POST['_ajax_nonce'] ) && check_ajax_referer( 'portal-nonce', sanitize_key( $_POST['_ajax_nonce'] ) ) && current_user_can( 'manage_dt' ) ) {
-        //     //deactivate the plugin
-        //     deactivate_plugins( sanitize_text_field( wp_unslash( $_POST['deactivate'] ) ), true );
-        //     exit;
-        // }
-
         $network_active_plugins = get_site_option( 'active_sitewide_plugins', [] );
         $active_plugins = get_option( 'active_plugins', [] );
         foreach ( $network_active_plugins as $plugin => $time ){
