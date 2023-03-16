8ddd935f8e11a2e4b8b89034ce4877124e1b832b
corsac <corsacca@gmail.com>
2023-03-16 09:05:02 +0100

    use empty string to clear number

--- dt-posts/dt-posts.php
+++ dt-posts/dt-posts.php
@@ -246,7 +246,7 @@ class DT_Posts extends Disciple_Tools_Posts {
                 $post_user_meta[$field_key] = $field_value;
                 unset( $fields[ $field_key ] );
             }
-            if ( $field_type === 'number' && (
+            if ( $field_type === 'number' && $field_value !== '' && (
                 isset( $post_settings['fields'][$field_key]['min_option'] ) && ( !empty( $post_settings['fields'][$field_key]['min_option'] ) || $post_settings['fields'][$field_key]['min_option'] === 0 ) &&
                 $field_value < $post_settings['fields'][$field_key]['min_option'] ||
                 isset( $post_settings['fields'][$field_key]['max_option'] ) && ( !empty( $post_settings['fields'][$field_key]['max_option'] ) || $post_settings['fields'][$field_key]['max_option'] === 0 ) &&
@@ -508,7 +508,7 @@ class DT_Posts extends Disciple_Tools_Posts {
                     $field_value = strtotime( $field_value );
                 }
 
-                if ( $field_type === 'number' && !empty( $field_value ) && (
+                if ( $field_type === 'number' && $field_value !== '' && (
                     isset( $post_settings['fields'][$field_key]['min_option'] ) && ( !empty( $post_settings['fields'][$field_key]['min_option'] ) || $post_settings['fields'][$field_key]['min_option'] === 0 ) &&
                     $field_value < $post_settings['fields'][$field_key]['min_option'] ||
                     isset( $post_settings['fields'][$field_key]['max_option'] ) && ( !empty( $post_settings['fields'][$field_key]['max_option'] ) || $post_settings['fields'][$field_key]['max_option'] === 0 ) &&
