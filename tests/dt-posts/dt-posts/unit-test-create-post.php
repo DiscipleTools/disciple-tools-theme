23c9d19c4044f9d48ab11e3bd5230c64f55cd20e
kodinkat <me@karlkatewu.com>
2023-03-16 08:40:40 +0000

    Support Deleting Of Number Fields With Min/Max Constraints (#1979)
    * Ensure number fields with min/max constraints can be deleted
    
    * Adjusted unit tests
    
    * use empty string to clear number
    
    * update tests
    
    ---------
    
    Co-authored-by: corsac <corsacca@gmail.com>

--- tests/dt-posts/dt-posts/unit-test-create-post.php
+++ tests/dt-posts/dt-posts/unit-test-create-post.php
@@ -154,12 +154,20 @@ class DT_Posts_DT_Posts_Create_Post extends WP_UnitTestCase {
 
     public function test_custom_number_field_min_max_error() {
         // test that lower than the minimum creates an error
-        $contact1 = DT_Posts::create_post( 'contacts', [ 'name' => 'one', 'number_test' => 0 ], true, false );
+        $contact1 = DT_Posts::create_post( 'contacts', [ 'name' => 'one', 'number_test' => -1 ], true, false );
         $this->assertWPError( $contact1 );
 
-        // test that higher than the maximum creates an error
-        $contact2 = DT_Posts::create_post( 'contacts', [ 'name' => 'one', 'number_test_private' => 300 ], true, false );
+        // test that lower than the minimum creates an error
+        $contact2 = DT_Posts::create_post( 'contacts', [ 'name' => 'one', 'number_test' => 0 ], true, false );
         $this->assertWPError( $contact2 );
+
+        // test that higher than the maximum creates an error
+        $contact3 = DT_Posts::create_post( 'contacts', [ 'name' => 'one', 'number_test_private' => 300 ], true, false );
+        $this->assertWPError( $contact3 );
+
+        // test that clearing the field is not an error
+        $contact4 = DT_Posts::create_post( 'contacts', [ 'name' => 'one', 'number_test' => '' ], true, false );
+        $this->assertNotWPError( $contact4 );
 
     }
 
