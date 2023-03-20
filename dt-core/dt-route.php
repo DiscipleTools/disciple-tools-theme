75ff8da47d43dcd0e8ab979d0267244cbbc8b56b
Nath <nathinabob@gmail.com>
2023-03-20 14:41:14 +0000

    feat: add options array to adjust permission_callback and any other options in dt-route

--- dt-core/dt-route.php
+++ dt-core/dt-route.php
@@ -5,25 +5,32 @@
  */
 class DT_Route {
 
-    public static function get( string $namespace, string $route, array $callback ) {
-        self::endpoint( WP_REST_Server::READABLE, $namespace, $route, $callback );
+    public static function get( string $namespace, string $route, array $callback, array $options = [] ) {
+        self::endpoint( WP_REST_Server::READABLE, $namespace, $route, $callback, $options );
     }
 
-    public static function post( string $namespace, string $route, array $callback ) {
-        self::endpoint( WP_REST_Server::CREATABLE, $namespace, $route, $callback );
+    public static function post( string $namespace, string $route, array $callback, array $options = [] ) {
+        self::endpoint( WP_REST_Server::CREATABLE, $namespace, $route, $callback, $options );
     }
 
-    private static function endpoint( string $methods, string $namespace, string $route, array $callback ) {
+    private static function endpoint( string $methods, string $namespace, string $route, array $callback, array $options ) {
+        $default_options = [
+            'permission_callback' => '__return_true'
+        ];
+
+        $rest_options = [
+            'methods'  => $methods,
+            'callback' => $callback,
+        ];
+
+        $merged_options = array_merge( $rest_options, $default_options, $options );
+
         register_rest_route(
             $namespace,
             $route,
             [
-                [
-                    'methods'  => $methods,
-                    'callback' => $callback,
-                    'permission_callback' => '__return_true'
-                ],
-            ]
+                $merged_options
+            ],
         );
     }
 }
