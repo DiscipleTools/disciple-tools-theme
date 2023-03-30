2500da4b9031e8c25af63f151a86e01d121a1850
Nath <nathinabob@gmail.com>
2023-03-30 15:57:25 +0100

    fix: big oopsy copy over from PG

--- tests/test_phpcs.sh
+++ tests/test_phpcs.sh
@@ -13,7 +13,7 @@ fi
 cd "$(dirname "${BASH_SOURCE[0]}")/../"
 
 if [ "$(php -r 'echo version_compare( phpversion(), "7.0", ">=" ) ? 1 : 0;')" != 1 ] ; then
-    vendor/bin/phpcs prayer-global-porch.php
+    vendor/bin/phpcs functions.php
     exit
 fi
 
