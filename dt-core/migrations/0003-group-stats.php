461bcdf47f2d8c2c2cd8d7f7329f5e85b7f1504c
Chris Chasm <chris@chasm.solutions>
2023-03-30 09:53:55 -0600

    Migration: Bug in array sitation

--- dt-core/migrations/0003-group-stats.php
+++ dt-core/migrations/0003-group-stats.php
@@ -8,11 +8,9 @@ class Disciple_Tools_Migration_0003 extends Disciple_Tools_Migration {
             return;
         }
 
-
-
         $groups = DT_Posts::search_viewable_post( 'groups', [] );
-        if ( !is_wp_error( $groups ) && count( $groups['groups'] ) > 0 ) {
-            foreach ( $groups['groups'] as $group ){
+        if ( !is_wp_error( $groups ) && count( $groups['posts'] ) > 0 ) {
+            foreach ( $groups['posts'] as $group ){
                 $meta_fields = get_post_custom( $group->ID );
 
                 if ( count( $meta_fields ) > 0 ) {
