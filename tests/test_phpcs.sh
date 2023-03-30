87bf3fc35b7de17b5cccf30960a5555100831f5a
Nath <nathinabob@gmail.com>
2023-03-30 16:00:41 +0100

    fix: make test_phpcs stop conditional instead of changing script

--- tests/test_phpcs.sh
+++ tests/test_phpcs.sh
@@ -2,24 +2,11 @@
 
 set -e
 
-if [[ $# = 0 ]]; then
-    echo "No arguments given: skipping test"
-    echo "If you meant to run phpcs on the entire codebase then run"
-    echo
-    echo "$0 --test-all"
-    exit 0
-fi
-
 cd "$(dirname "${BASH_SOURCE[0]}")/../"
 
 if [ "$(php -r 'echo version_compare( phpversion(), "7.0", ">=" ) ? 1 : 0;')" != 1 ] ; then
     vendor/bin/phpcs functions.php
     exit
-fi
-
-args=$@
-if [[ "$1" = "--test-all" ]]; then
-    args=""
 fi
 
 eval vendor/bin/phpcs $args
