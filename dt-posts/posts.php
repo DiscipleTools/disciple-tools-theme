ebe134ced9f484c38b7af015c66e77002c9ade9b
corsac <corsacca@gmail.com>
2023-05-15 14:55:42 +0200

    People Groups Lists page: fix queries.

--- dt-posts/posts.php
+++ dt-posts/posts.php
@@ -1198,7 +1198,7 @@ class Disciple_Tools_Posts
 
                 $locale = get_user_locale();
 
-                $post_query = " OR p.ID IN ( SELECT post_id
+                $post_query .= " OR p.ID IN ( SELECT post_id
                                   FROM $wpdb->postmeta
                                   WHERE meta_key LIKE '" . esc_sql( $locale ) . "'
                                   AND meta_value LIKE '%" . esc_sql( $search ) . "%' )";
