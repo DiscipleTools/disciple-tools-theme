3ec4560618f5a05f7329e2b72887a57865e5bbed
corsac <corsacca@gmail.com>
2023-06-06 17:08:09 +0200

    Re-enable needed migration for demo content

--- dt-core/migrations/0014-milestones-field.php
+++ dt-core/migrations/0014-milestones-field.php
@@ -5,9 +5,9 @@ if ( !defined( 'ABSPATH' ) ) { exit; } // Exit if accessed directly
 class Disciple_Tools_Migration_0014 extends Disciple_Tools_Migration {
     public function up() {
         //skip this migration on a new install
-        if ( dt_get_initial_install_meta( 'migration_number' ) > 14 ){
-            return;
-        }
+//        if ( dt_get_initial_install_meta( 'migration_number' ) > 14 ){
+//            return;
+//        }
 
 //        get all miltestones grouped by contact
         global $wpdb;
