c170ff398a60b7edf70af6f7239efa0a4c3d5eb0
kodinkat <me@karlkatewu.com>
2023-06-07 17:27:37 +0100

    Magic link published customised favicon

--- template-blank.php
+++ template-blank.php
@@ -27,19 +27,21 @@ if ( ! apply_filters( 'dt_blank_access', false ) ){
      * These default settings are sufficient, unless you are building a progressing web app
      * then you can override them.
      */
-    $dt_override_header_meta = apply_filters( 'dt_override_header_meta', false ); if ( ! $dt_override_header_meta ) : ?>
-    <meta name="apple-mobile-web-app-title" content="Disciple.Tools">
-    <link rel="apple-touch-icon" sizes="180x180" href="<?php echo esc_url( get_template_directory_uri() ); ?>/dt-assets/favicons/apple-touch-icon.png">
-    <link rel="icon" type="image/png" sizes="32x32" href="<?php echo esc_url( get_template_directory_uri() ); ?>/dt-assets/favicons/favicon-32x32.png">
-    <link rel="icon" type="image/png" sizes="16x16" href="<?php echo esc_url( get_template_directory_uri() ); ?>/dt-assets/favicons/favicon-16x16.png">
-    <link rel="mask-icon" href="<?php echo esc_url( get_template_directory_uri() ); ?>/dt-assets/favicons/safari-pinned-tab.svg" color="#3f729b">
-    <link rel="shortcut icon" href="<?php echo esc_url( get_template_directory_uri() ); ?>/dt-assets/favicons/favicon.ico">
-    <meta name="msapplication-TileColor" content="#3f729b">
-    <meta name="msapplication-TileImage" content="<?php echo esc_url( get_template_directory_uri() ); ?>/dt-assets/favicons/mstile-144x144.png">
-    <meta name="msapplication-config" content="<?php echo esc_url( get_template_directory_uri() ); ?>/dt-assets/favicons/browserconfig.xml">
-    <meta name="theme-color" content="#3f729b">
+    $dt_override_header_meta = apply_filters( 'dt_override_header_meta', false );
+    if ( !$dt_override_header_meta ) : ?>
+        <?php if ( ! function_exists( 'has_site_icon' ) || ! has_site_icon() ) { ?>
+            <link rel="apple-touch-icon" sizes="180x180" href="<?php echo esc_url( get_template_directory_uri() ); ?>/dt-assets/favicons/apple-touch-icon.png">
+            <link rel="icon" type="image/png" sizes="32x32" href="<?php echo esc_url( get_template_directory_uri() ); ?>/dt-assets/favicons/favicon-32x32.png">
+            <link rel="icon" type="image/png" sizes="16x16" href="<?php echo esc_url( get_template_directory_uri() ); ?>/dt-assets/favicons/favicon-16x16.png">
+            <link rel="manifest" href="<?php echo esc_url( get_template_directory_uri() ); ?>/dt-assets/favicons/site.webmanifest">
+            <link rel="mask-icon" href="<?php echo esc_url( get_template_directory_uri() ); ?>/dt-assets/favicons/safari-pinned-tab.svg" color="#3f729b">
+            <link rel="shortcut icon" href="<?php echo esc_url( get_template_directory_uri() ); ?>/dt-assets/favicons/favicon.ico">
+            <meta name="msapplication-TileColor" content="#3f729b">
+            <meta name="msapplication-TileImage" content="<?php echo esc_url( get_template_directory_uri() ); ?>/dt-assets/favicons/mstile-144x144.png">
+            <meta name="msapplication-config" content="<?php echo esc_url( get_template_directory_uri() ); ?>/dt-assets/favicons/browserconfig.xml">
+            <meta name="theme-color" content="#3f729b">
+        <?php }  ?>
     <?php endif; ?>
-
 
     <title><?php echo esc_html( apply_filters( 'dt_blank_title', __( 'Form', 'disciple_tools' ) ) ) ?></title>
 
