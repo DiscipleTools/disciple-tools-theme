9c2dd24f6fa17564382c7dad7251183681c0ce06
Micah Mills <micahmills@gmail.com>
2023-06-11 13:41:07 +0100

    cast post ID to INT
    This is the same as https://github.com/DiscipleTools/disciple-tools-theme/pull/2066 but on the next branch.

--- dt-posts/dt-posts.php
+++ dt-posts/dt-posts.php
@@ -1108,7 +1108,7 @@ class DT_Posts extends Disciple_Tools_Posts {
             $post = get_post( $search_string );
             if ( $post && self::can_view( $post_type, $post->ID ) ){
                 $compact[] = [
-                    'ID' => (string) $post->ID,
+                    'ID' => (int) $post->ID,
                     'name' => $post->post_title,
                     'user' => false,
                     'status' => null
@@ -1221,7 +1221,7 @@ class DT_Posts extends Disciple_Tools_Posts {
                 continue;
             }
             $compact[] = [
-                'ID' => (string) $post->ID,
+                'ID' => (int) $post->ID,
                 'name' => wp_specialchars_decode( $post->post_title )
             ];
         }
@@ -1244,7 +1244,7 @@ class DT_Posts extends Disciple_Tools_Posts {
                     if ( !in_array( $post_id, $post_ids, true ) ) {
                         $post_ids[] = $post_id;
                         $compact[] = [
-                            'ID' => (string) $post_id,
+                            'ID' => (int) $post_id,
                             'name' => $user['name'],
                             'user' => true
                         ];
@@ -1308,7 +1308,7 @@ class DT_Posts extends Disciple_Tools_Posts {
                 foreach ( $compact as $index => &$p ){
                     if ( $compact[ $index ]['ID'] === $post->ID ) {
                         $compact[ $index ] = [
-                            'ID'    => (string) $post->ID,
+                            'ID'    => (int) $post->ID,
                             'name'  => $post->post_title,
                             'label' => $label
                             ];
