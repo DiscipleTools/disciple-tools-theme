75b1540502ffe18117801bd54562f42a16471809
kodinkat <me@karlkatewu.com>
2023-06-13 12:43:01 +0100

    Allow custom favicons on magic links. Function to set D.T header meta (#2088)
    * Magic link published customised favicon
    
    * One function do display favicon
    
    ---------
    
    Co-authored-by: corsac <corsacca@gmail.com>

--- dt-reports/magic-url-landing-page.php
+++ dt-reports/magic-url-landing-page.php
@@ -8,34 +8,9 @@ if ( !defined( 'ABSPATH' ) ){
 
 <html class="no-js" <?php language_attributes(); ?>>
     <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport"
-              content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0, shrink-to-fit=no">
-        <meta class="foundation-mq">
 
-        <?php
-        $dt_override_header_meta = apply_filters( 'dt_override_header_meta', false );
-        if ( !$dt_override_header_meta ) : ?>
-            <meta name="apple-mobile-web-app-title" content="Disciple.Tools">
-            <link rel="apple-touch-icon" sizes="180x180"
-                  href="<?php echo esc_url( get_template_directory_uri() ); ?>/dt-assets/favicons/apple-touch-icon.png">
-            <link rel="icon" type="image/png" sizes="32x32"
-                  href="<?php echo esc_url( get_template_directory_uri() ); ?>/dt-assets/favicons/favicon-32x32.png">
-            <link rel="icon" type="image/png" sizes="16x16"
-                  href="<?php echo esc_url( get_template_directory_uri() ); ?>/dt-assets/favicons/favicon-16x16.png">
-            <link rel="mask-icon"
-                  href="<?php echo esc_url( get_template_directory_uri() ); ?>/dt-assets/favicons/safari-pinned-tab.svg"
-                  color="#3f729b">
-            <link rel="shortcut icon"
-                  href="<?php echo esc_url( get_template_directory_uri() ); ?>/dt-assets/favicons/favicon.ico">
-            <meta name="msapplication-TileColor" content="#3f729b">
-            <meta name="msapplication-TileImage"
-                  content="<?php echo esc_url( get_template_directory_uri() ); ?>/dt-assets/favicons/mstile-144x144.png">
-            <meta name="msapplication-config"
-                  content="<?php echo esc_url( get_template_directory_uri() ); ?>/dt-assets/favicons/browserconfig.xml">
-            <meta name="theme-color" content="#3f729b">
-        <?php endif; ?>
+        <?php dt_header_icon_and_meta(); ?>
+
 
         <style>
             body {
