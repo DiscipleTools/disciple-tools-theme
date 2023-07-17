5e948e9256ffa87595942b1cd1a48904371b0f7e
corsacca <corsacca@gmail.com>
2023-07-17 15:22:52 +0100

    Add activity_log table index for object_subtype (#2119)

--- dt-core/configuration/class-migration-engine.php
+++ dt-core/configuration/class-migration-engine.php
@@ -12,7 +12,7 @@ if ( !defined( 'ABSPATH' ) ) {
 class Disciple_Tools_Migration_Engine
 {
 
-    public static $migration_number = 48;
+    public static $migration_number = 49;
 
     protected static $migrations = null;
 
