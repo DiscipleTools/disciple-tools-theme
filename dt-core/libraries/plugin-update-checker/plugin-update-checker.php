6ee6c6f86992fc1a250f8f03c590f571363fa66d
corsacca <corsacca@gmail.com>
2023-07-25 09:55:42 +0100

    Puc library backwards compatibility (#2124)

--- dt-core/libraries/plugin-update-checker/plugin-update-checker.php
+++ dt-core/libraries/plugin-update-checker/plugin-update-checker.php
@@ -8,3 +8,13 @@
  */
 
 require dirname(__FILE__) . '/load-v5p1.php';
+
+
+/**
+ * A bunch of plugin Rely on Puc_v4. For our use it is the same as v5.
+ */
+if ( !class_exists('Puc_v4_Factory' ) ){
+    class Puc_v4_Factory extends \YahnisElsts\PluginUpdateChecker\v5\PucFactory{
+
+    }
+}
