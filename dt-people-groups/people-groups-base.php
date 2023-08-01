73e691d14c838ca69efc13edbc5b2ad656add115
corsacca <corsacca@gmail.com>
2023-08-01 15:03:15 +0100

    Add delete any record to Contacts, Groups and People Groups (#2133)

--- dt-people-groups/people-groups-base.php
+++ dt-people-groups/people-groups-base.php
@@ -135,6 +135,7 @@ class Disciple_Tools_People_Groups_Base {
             $expected_roles['administrator']['permissions'][ 'create_' . $this->post_type ]     = true;
             $expected_roles['administrator']['permissions'][ 'view_any_' . $this->post_type ]   = true;
             $expected_roles['administrator']['permissions'][ 'update_any_' . $this->post_type ] = true;
+            $expected_roles['administrator']['permissions'][ 'delete_any_' . $this->post_type ] = true;
             $expected_roles['administrator']['permissions']['edit_peoplegroups']                = true;
         }
         if ( isset( $expected_roles['dt_admin'] ) ) {
@@ -142,6 +143,7 @@ class Disciple_Tools_People_Groups_Base {
             $expected_roles['dt_admin']['permissions'][ 'create_' . $this->post_type ]     = true;
             $expected_roles['dt_admin']['permissions'][ 'view_any_' . $this->post_type ]   = true;
             $expected_roles['dt_admin']['permissions'][ 'update_any_' . $this->post_type ] = true;
+            $expected_roles['dt_admin']['permissions'][ 'delete_any_' . $this->post_type ] = true;
             $expected_roles['dt_admin']['permissions']['edit_peoplegroups']                = true;
         }
 
