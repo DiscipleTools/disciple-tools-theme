4a8d50afcb658801d6b153b2a1e8f01135194c92
corsac <corsacca@gmail.com>
2023-08-17 16:22:07 +0100

    Check if role editable before saving.

--- dt-core/admin/menu/tabs/tab-custom-roles.php
+++ dt-core/admin/menu/tabs/tab-custom-roles.php
@@ -210,12 +210,12 @@ class Disciple_Tools_Tab_Custom_Roles extends Disciple_Tools_Abstract_Menu_Base 
      * Process a role save
      */
     public function save() {
-        global $wpdb;
-
         if ( !isset( $_POST['role_edit_nonce'] ) || !wp_verify_nonce( sanitize_key( $_POST['role_edit_nonce'] ), 'role_edit' ) ) {
             $error = new WP_Error( 401, __( 'Unauthorized', 'disciple_tools' ) );
             $this->show_error( $error );
         }
+
+        $roles = Disciple_Tools_Roles::get_dt_roles_and_permissions( false );
 
         $label = isset( $_POST['label'] ) ? sanitize_text_field( wp_unslash( $_POST['label'] ) ) : null;
         $slug = isset( $_POST['slug'] ) ? sanitize_text_field( wp_unslash( $_POST['slug'] ) ) : null;
@@ -235,6 +235,13 @@ class Disciple_Tools_Tab_Custom_Roles extends Disciple_Tools_Abstract_Menu_Base 
 
         if ( !$description ) {
             return $this->show_error( new WP_Error( 400, 'The description field is required.' ) );
+        }
+
+        if ( !isset( $roles[ $slug ] ) ) {
+            return $this->show_error( new WP_Error( 400, 'The role does not exist.' ) );
+        }
+        if ( isset( $roles[ $slug ]['is_editable'] ) && empty( $roles[ $slug ]['is_editable'] ) ) {
+            return $this->show_error( new WP_Error( 400, 'The role is not editable' ) );
         }
 
         $option = get_option( self::OPTION_NAME, [] );
@@ -433,7 +440,7 @@ class Disciple_Tools_Tab_Custom_Roles extends Disciple_Tools_Abstract_Menu_Base 
                                                            class="button">Duplicate</a>
                                                     </li>
                                                 <?php endif; ?>
-                                                <?php if ( current_user_can( 'delete_roles' ) && $editable ): ?>
+                                                <?php if ( current_user_can( 'delete_roles' ) && $editable && !empty( $role['custom'] ) ): ?>
                                                     <li>
                                                         <a href="<?php echo esc_url( $this->url_base . '&' . http_build_query( [ 'action' => 'delete', 'role' => $key ] ) ) ?>"
                                                            class="button button-primary">Delete</a>
