4fb2e5f5455a246ca44591aa598cd85e34cd272c
kodinkat <me@karlkatewu.com>
2023-08-30 15:52:52 +0100

    A more optimized shape

--- dt-core/migrations/0053-update-old-activity-log-connection-formats.php
+++ dt-core/migrations/0053-update-old-activity-log-connection-formats.php
@@ -14,20 +14,16 @@ class Disciple_Tools_Migration_0053 extends Disciple_Tools_Migration {
         $old_connection_logs = $wpdb->get_results(
             // phpcs:disable
             $wpdb->prepare( "
-                SELECT
-                    log.histid AS id, log.object_type AS post_type, log.meta_key AS p2p_key, log.field_type AS direction
+                SELECT DISTINCT log.object_type AS post_type, log.meta_key AS p2p_key, log.field_type AS direction
                 FROM $wpdb->dt_activity_log AS log
-                WHERE log.object_subtype = 'p2p'
-                    AND log.field_type LIKE 'connection%'
+                WHERE log.object_subtype = 'p2p' AND log.field_type LIKE 'connection%'
             " ), ARRAY_A
             // phpcs:enable
         );
 
         // Iterate over identified connections.....
-        $sql_queries = [];
         foreach ( $old_connection_logs ?? [] as $log ){
-            if ( isset( $log['id'], $log['post_type'], $log['p2p_key'], $log['direction'] ) ){
-                $log_id = $log['id'];
+            if ( isset( $log['post_type'], $log['p2p_key'], $log['direction'] ) ){
                 $post_type = $log['post_type'];
                 $p2p_key = $log['p2p_key'];
                 $log_direction = $log['direction'];
@@ -45,47 +41,25 @@ class Disciple_Tools_Migration_0053 extends Disciple_Tools_Migration {
                 $post_field_settings = DT_Posts::get_post_field_settings( $post_type, false );
                 $connection_field = DT_Posts::get_post_field_settings_by_p2p( $post_field_settings, $p2p_key, $directions );
                 if ( !empty( $connection_field ) && isset( $connection_field['key'] ) ){
-                    $sql_queries[] = [
-                        'log_id' => $log_id,
-                        'field_key' => $connection_field['key'],
-                        'log_direction' => $log_direction
-                    ];
+                    $field_key = $connection_field['key'];
+
+                    // Migrate common log records to new format.
+                    $wpdb->query(
+                        // phpcs:disable
+                        $wpdb->prepare( "
+                             UPDATE $wpdb->dt_activity_log
+                             SET
+                                 object_subtype = %s,
+                                 object_note = %s,
+                                 field_type = %s
+                             WHERE object_type = %s
+                                AND object_subtype = 'p2p'
+                                AND field_type = %s
+                                AND meta_key = %s
+                         ", $field_key, $log_direction, 'connection', $post_type, $log_direction, $p2p_key )
+                        // phpcs:enable
+                    );
                 }
-            }
-        }
-
-        // Batch and dispatch update queries.
-        $batch_limit = 1000;
-        $batch_size = ( count( $sql_queries ) < $batch_limit ) ? count( $sql_queries ) : $batch_limit;
-        $batch_counter = 0;
-        $obj_subtype_sql = [];
-        $obj_note_sql = [];
-        $hist_ids = [];
-        foreach ( $sql_queries as $log ) {
-            $hist_ids[] = $log['log_id'];
-            $obj_subtype_sql[] = 'WHEN ' . $log['log_id'] . " THEN '" . $log['field_key'] . "'";
-            $obj_note_sql[] = 'WHEN ' . $log['log_id'] . " THEN '" . $log['log_direction'] . "'";
-
-            if ( ++$batch_counter >= $batch_size ) {
-                // phpcs:disable
-                $sql = "
-                    UPDATE $wpdb->dt_activity_log
-                    SET
-                        object_subtype = ( CASE histid ". implode(' ', $obj_subtype_sql ) ." END ),
-                        object_note = ( CASE histid ". implode(' ', $obj_note_sql ) ." END ),
-                        field_type = 'connection'
-                    WHERE histid IN ( ". implode( ',', $hist_ids ) ." );
-                ";
-
-                // Dispatch batch....
-                $wpdb->query( $sql );
-
-                // Reset counters and containers.
-                $batch_counter = 0;
-                $obj_subtype_sql = [];
-                $obj_note_sql = [];
-                $hist_ids = [];
-                // phpcs:enable
             }
         }
     }
