d669938c707fcbff82df6324d3b4add3354fcf51
corsac <corsacca@gmail.com>
2023-08-31 10:56:26 +0100

    skip this migration on a new install

--- dt-core/migrations/0053-update-old-activity-log-connection-formats.php
+++ dt-core/migrations/0053-update-old-activity-log-connection-formats.php
@@ -8,6 +8,11 @@ if ( !defined( 'ABSPATH' ) ) { exit; } // Exit if accessed directly
  */
 class Disciple_Tools_Migration_0053 extends Disciple_Tools_Migration {
     public function up() {
+        //skip this migration on a new install
+        if ( dt_get_initial_install_meta( 'migration_number' ) > 52 ){
+            return;
+        }
+
         global $wpdb;
 
         // Identify old connection log entry formats.
