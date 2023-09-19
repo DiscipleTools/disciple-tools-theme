e923a715b505e6d12aff3090d252d219bb90cfbf
kodinkat <me@karlkatewu.com>
2023-09-19 15:34:40 +0100

    Metrics: Generation Tree > Child Addition & Node focus Support (#2190)
    * Support node child addition and node focus
    
    * Added show all button + Persist focus following child node addition

--- dt-assets/parts/modals/modal-template-metrics.php
+++ dt-assets/parts/modals/modal-template-metrics.php
@@ -8,6 +8,8 @@
         <?php echo esc_html__( 'Close', 'disciple_tools' ) ?>
     </button>
 
+    <span id="template_metrics_modal_buttons"></span>
+
     <button class="close-button" data-close aria-label="<?php esc_html_e( 'Close', 'disciple_tools' ); ?>"
             type="button">
         <span aria-hidden="true">&times;</span>
