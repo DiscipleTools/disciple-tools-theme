0cdc11916304d477e958f3bb9d5b71a16171a91d
Nath <nathinabob@gmail.com>
2023-09-28 16:55:21 +0100

    feat: add setCookie function to shared-functions.js

--- dt-assets/js/shared-functions.js
+++ dt-assets/js/shared-functions.js
@@ -575,6 +575,12 @@ window.SHAREDFUNCTIONS = {
     }
     return "";
   },
+  setCookie(cname, cvalue, exdays = 30 ) {
+    var d = new Date();
+    d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+    var expires = "expires="+d.toUTCString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+  },
   get_json_cookie(cname, default_val = []) {
     let cookie = this.getCookie(cname);
     try {
