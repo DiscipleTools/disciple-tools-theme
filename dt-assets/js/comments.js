e6026ac10776ddb6b2c8e466f834992a20fedaff
corsacca <corsacca@gmail.com>
2024-04-25 14:47:28 +0100

    Ability to set custom icons for different comment types. Also fix icon alignment (#2447)
    * Correctly center comment avatar
    
    * Comments: Only create a gravatar if there is a user
    
    * rebuild styles

--- dt-assets/js/comments.js
+++ dt-assets/js/comments.js
@@ -118,15 +118,15 @@ jQuery(document).ready(function ($) {
    * input with dir=auto set, especially when using a right-to-left language
    * with multiple paragraphs. */
   let commentTemplate = window.lodash.template(`
-  <div class="activity-block">
-    <div>
-        <span class="gravatar">
+  <div class="activity-block" >
+    <div class="comment-header" style="">
+        <div class="gravatar">
         <% if( $.trim( gravatar ) ) { %>
             <img src="<%- gravatar  %>"/>
         <% } else { %>
             <i class="mdi mdi-robot-outline"></i>
         <% } %>
-        </span>
+        </div>
         <span><strong><%- name %></strong></span>
         <span class="comment-date"> <%- date %> </span>
       </div>
