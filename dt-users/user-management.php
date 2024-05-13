679b23124f19b6426953c95780fa69f0e283b64a
corsacca <corsacca@gmail.com>
2024-05-13 17:04:31 +0100

    Users list: Fix for empty arrays

--- dt-users/user-management.php
+++ dt-users/user-management.php
@@ -754,13 +754,13 @@ class DT_User_Management
         foreach ( $users_query as &$user ){
             foreach ( $fields_by_type['array'] as $field_key ){
                 if ( isset( $user[ $field_key ] ) ){
-                    $user[ $field_key ] = unserialize( $user[ $field_key ] );
+                    $user[ $field_key ] = maybe_unserialize( $user[ $field_key ] );
                 }
             }
             foreach ( $fields_by_type['array_keys'] as $field_key ){
                 if ( isset( $user[ $field_key ] ) ){
-                    $user[ $field_key ] = unserialize( $user[ $field_key ] );
-                    $user[ $field_key ] = array_keys( $user[ $field_key ] );
+                    $user[ $field_key ] = maybe_unserialize( $user[ $field_key ] );
+                    $user[ $field_key ] = is_array( $user[ $field_key ] ) ? array_keys( $user[ $field_key ] ) : [];
                 }
             }
             foreach ( $fields_by_type['location_grid'] as $field_key ){
