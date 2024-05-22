e997bd390f6afb140b5730b2fc72e4cea4088f17
Micah Mills <micahmills@gmail.com>
2024-05-22 12:39:16 +0300

    removes ashima css class

--- template-new-post.php
+++ template-new-post.php
@@ -151,7 +151,7 @@ if ( isset( $post_settings['fields']['type'] ) && sizeof( $post_settings['fields
 
                         <div style="text-align: center">
                             <a href="<?php echo esc_html( get_site_url() . '/' . $dt_post_type )?>" class="button small clear"><?php echo esc_html__( 'Cancel', 'disciple_tools' )?></a>
-                            <button class="button loader js-create-post-button dt-green ashima" type="submit" disabled><?php esc_html_e( 'Save and continue editing', 'disciple_tools' ); ?></button>
+                            <button class="button loader js-create-post-button dt-green" type="submit" disabled><?php esc_html_e( 'Save and continue editing', 'disciple_tools' ); ?></button>
                         </div>
                         <div class="error-text"></div>
                     </div>
