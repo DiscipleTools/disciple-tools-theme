ee30e68c6f51e6ce9f0d01a7316a4dbc9aa1cfb4
corsacca <corsacca@gmail.com>
2024-07-17 15:23:55 +0100

    SOO Login: setting to persist login

--- dt-login/login-user-manager.php
+++ dt-login/login-user-manager.php
@@ -142,9 +142,21 @@ class DT_Login_User_Manager {
             wp_logout();
         }
 
+        $login_length = DT_Login_Fields::get( 'login_length' );
+        if ( empty( $login_length ) || !is_numeric( $login_length ) ) {
+            $login_length = 14;
+        }
+
+        add_filter( 'auth_cookie_expiration', function () use ( $login_length ) {
+            return $login_length * DAY_IN_SECONDS;
+        } );
+
         add_filter( 'authenticate', [ $this, 'allow_programmatic_login' ], 10, 3 );    // hook in earlier than other callbacks to short-circuit them
 
-        $user = wp_signon( array( 'user_login' => $this->email ) );
+        $user = wp_signon( [
+            'user_login' => $this->email,
+            'remember' => true,
+        ] );
 
         remove_filter( 'authenticate', [ $this, 'allow_programmatic_login' ], 10 );
 
