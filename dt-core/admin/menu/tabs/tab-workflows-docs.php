722c91151e656ce8d7b2e0e2f3b4627dabc5e9bf
brady-lamansky-gtt <125215973+brady-lamansky-gtt@users.noreply.github.com>
2024-09-17 05:51:30 -0500

    Workflows: Help Text for Comments (#2553)
    * Comments {subassigned} functionality
    
    * removed hardcoded {connections}
    
    * PR cleanup/optimizations
    
    * Copied Jon's Fixes
    
    * comment help text for applicable fields
    
    * prettier, removed unneeded wording
    
    * fixed comments
    
    * Addressing comments, made help_text list scrollable
    
    * made cursor a pointer on help_text button
    
    * help text button now goes away, formatting fixes, etc
    
    * fixed help button scrolling to top of page

---
+++ dt-core/admin/menu/tabs/tab-workflows-docs.php
@@ -0,0 +1,12 @@
+<!-- Here you can create different Help Texts for use in different places -->
+
+<!-- Comments Field -->
+<div id="dt_right_docs_comments_title" style="display: none;">
+    Comments Field
+</div>
+<div id="dt_right_docs_comments_content" style="display: none;">
+    When creating comments, you can insert field values from the current record. This can be helpful for showing what has changed or for tagging users who are selected in a given field. The following fields are supported for replacement using the given replacement tokens.
+    <ul id="dt_right_docs_comments_list">
+    </ul>
+</div>
+<!-- Help Text -->
