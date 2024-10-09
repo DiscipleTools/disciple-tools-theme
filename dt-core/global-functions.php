b1aed3519b53d75b95fc31c60fc83f29f2ee3e8d
Vijenderrr <142395840+Vijenderrr@users.noreply.github.com>
2024-10-09 20:30:30 +0530

    dt-modal integration in DT-Multiselect-Button-Group (#2589)
    * dt-modal integration
    
    * phpcs fixes
    
    * phpcs fixes
    
    ---------
    
    Co-authored-by: Micah Mills <micahmills@gmail.com>

--- dt-core/global-functions.php
+++ dt-core/global-functions.php
@@ -708,7 +708,10 @@ if ( !defined( 'DT_FUNCTIONS_READY' ) ) {
                     <?php $faith_milestone = array( $fields[$field_key]['default'] );
                     $faith_milestone_json = json_encode( $faith_milestone );
                     ?>
+                            <?php $is_modal_array = [ 'Baptized' ];
+                            $is_modal_json = json_encode( $is_modal_array );?>
                         <dt-multiselect-buttons-group context=<?php echo wp_kses_post( $shared_attributes ) ?>
+                            isModal='<?php echo esc_attr( $is_modal_json ); ?>'
                             buttons='<?php echo esc_attr( $faith_milestone_json ); ?>'
                             value="<?php echo esc_attr( isset( $post[$field_key] ) ? json_encode( $post[$field_key] ) : '' ) ?>">
                         <?php dt_render_icon_slot( $fields[$field_key] ) ?></dt-multiselect-buttons-group>
