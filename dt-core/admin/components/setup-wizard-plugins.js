9829327bfb78cda2bb739be58220183f4c75310e
corsac <corsacca@gmail.com>
2024-12-17 11:15:44 +0100

    table view for plugins

--- dt-core/admin/components/setup-wizard-plugins.js
+++ dt-core/admin/components/setup-wizard-plugins.js
@@ -22,62 +22,94 @@ export class SetupWizardPlugins extends OpenLitElement {
   constructor() {
     super();
     this.plugins = window.setupWizardShare.data.plugins;
+    //@todo get list of recommended plugins
   }
 
   back() {
     this.dispatchEvent(new CustomEvent('back'));
   }
-  next() {
+  async next() {
+    const plugins_to_install = this.plugins.filter((plugin) => plugin.selected);
+
+    for (let plugin of plugins_to_install) {
+      if (!plugin.installed) {
+        plugin.installing = true;
+        this.requestUpdate();
+        await window.dt_admin_shared.plugin_install(plugin.download_url);
+        await window.dt_admin_shared.plugin_activate(plugin.slug);
+        plugin.installing = false;
+        plugin.installed = true;
+        // plugin.active = true;
+      }
+      if (plugin.selected && !plugin.active) {
+        plugin.installing = true;
+        this.requestUpdate();
+        await window.dt_admin_shared.plugin_activate(plugin.slug);
+        plugin.installing = false;
+        plugin.active = true;
+      }
+    }
+    this.requestUpdate();
+
     this.dispatchEvent(new CustomEvent('next'));
+  }
+
+  select_all() {
+    let already_all_selected =
+      this.plugins.filter((plugin) => plugin.selected).length ===
+      this.plugins.length;
+    this.plugins.forEach((plugin) => {
+      plugin.selected = !already_all_selected;
+    });
+    this.requestUpdate();
   }
 
   render() {
     return html`
       <div class="cover">
         <div class="content">
-          <div
-            class="plugin grid"
-            style="grid-template-columns: 1fr 1fr 1fr 1fr"
-          >
-            ${this.plugins.map((plugin) => {
-              return html`
-                <div
-                  class="btn-card flow ${plugin.active || plugin.selected
-                    ? 'selected'
-                    : ''}"
-                  @click=${() => {
-                    plugin.selected = !plugin.selected;
-                    this.requestUpdate();
-                  }}
-                >
-                  <div style="display: inline">${plugin.name}</div>
-                  <div>
-                    ${plugin.installed && !plugin.active
-                      ? html`<div
-                          class="tag"
-                          style="background-color:var(--default-dark)"
-                        >
-                          Installed
-                        </div>`
-                      : ''}
-                    ${plugin.active
-                      ? html`<div
-                          class="tag"
-                          style="background-color: var(--secondary-color);"
-                        >
-                          Active
-                        </div>`
-                      : ''}
-                  </div>
-                  <div title="${plugin.description}">
-                    ${plugin.description.length > 100
-                      ? plugin.description.substring(0, 100) + '...'
-                      : plugin.description}
-                  </div>
-                </div>
-              `;
-            })}
-          </div>
+          <table>
+            <thead>
+              <tr>
+                <th>Plugin Name</th>
+                <th>
+                  Install/Activate <br />
+                  <spin
+                    style="color: blue;cursor: pointer"
+                    @click=${() => this.select_all()}
+                    >select all</spin
+                  >
+                </th>
+                <th style="width: 60%">Description</th>
+                <th>Info</th>
+              </tr>
+            </thead>
+            <tbody>
+              ${this.plugins.map((plugin) => {
+                let action = 'Active';
+                if (plugin.installing) {
+                  action = html`<span class="spinner"></span>`;
+                } else if (!plugin.active) {
+                  action = html`<input
+                    type="checkbox"
+                    .checked=${plugin.selected}
+                  />`;
+                }
+
+                return html`
+          <tr @click=${() => {
+            plugin.selected = !plugin.selected;
+            this.requestUpdate();
+          }}>
+            <td>${plugin.name}</td>
+            <td>${action}</td>
+            <td style="max-width: 50%">${plugin.description}</td>
+            <td><a href=${plugin.permalink} target="_blank">Plugin Link</a></
+          </tr>
+          `;
+              })}
+            </tbody>
+          </table>
         </div>
         <setup-wizard-controls
           ?hideBack=${this.firstStep}
