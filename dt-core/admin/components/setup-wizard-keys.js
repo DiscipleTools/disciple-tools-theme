650fc51325baef89b085328acdd22d2bb0712aa1
corsac <corsacca@gmail.com>
2024-12-19 10:00:08 +0100

    add geocoding explanation

--- dt-core/admin/components/setup-wizard-keys.js
+++ dt-core/admin/components/setup-wizard-keys.js
@@ -54,6 +54,26 @@ export class SetupWizardKeys extends OpenLitElement {
       <div class="cover">
         <div class="content flow">
           <h2>Part 3: Mapping and Geocoding</h2>
+          <p>
+            Disciple.Tools comes with high level mapping which is only as
+            precise as a country county or department. Extra tools are needed to
+            be able to geolocate to more precise locations like a street address
+            or a city. See
+            <a
+              href="https://disciple.tools/user-docs/getting-started-info/admin/geolocation/"
+              >Geolocation Docs</a
+            >
+            for more details.
+          </p>
+          <p>
+            We highly recommend setting up MapBox to enable better mapping and
+            location pins for your records. This however is not required and
+            Disciple.Tools can be used without it.
+          </p>
+          <p>
+            Mapbox is needed for better maps. You will want to also add a Google
+            key for more precise worldwide geocoding.
+          </p>
           <table style="width: 100%">
             <thead>
               <tr>
