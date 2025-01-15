ad1abbfcf6a1c4ede25cbb3df680fc307f6adb93
corsac <corsacca@gmail.com>
2025-01-15 11:57:18 +0100

    Update Finished step and change finish to go to the front end

--- dt-core/admin/components/setup-wizard-celebration.js
+++ dt-core/admin/components/setup-wizard-celebration.js
@@ -20,7 +20,7 @@ export class SetupWizardCelebration extends OpenLitElement {
     await window.dt_admin_shared.update_dt_options({
       dt_setup_wizard_completed: true,
     });
-    window.location.href = window.setupWizardShare.admin_url;
+    window.location.href = window.dt_admin_scripts.site_url;
   }
 
   render() {
@@ -37,67 +37,92 @@ export class SetupWizardCelebration extends OpenLitElement {
               />
             </div>
             <p>
-              After closing this setup wizard, you'll find yourself in the
-              WordPress admin dashboard. From there, you can explore these
-              settings and continue customizing your Disciple.Tools site.
+              You can now start using Disciple.Tools! Read more about using and
+              customizing Disciple.Tools in the documentation.
             </p>
-            <p>
-              Disciple.Tools has a huge ability to be customized to serve your
-              needs. If you have any questions or need any further assistance in
-              getting Disciple.Tools. to work for you, please reach out on our
-              community forum or our discord channel.
-            </p>
-            <div
-              style="display: grid; grid-template-columns: 1fr 1fr; gap: 1rem"
-            >
-              <div style="text-align: center">
+            <div style="text-align: center">
+              <img
+                class="blue-svg"
+                style="width: 50px; height: 50px;"
+                src="${window.setupWizardShare.admin_image_url + 'documentation.svg'}"
+              />
+              <br />
+              <a
+                href="https://disciple.tools/docs"
+                target="_blank"
+                class="button btn-primary btn-with-icon"
+              >
+                Documentation
                 <img
-                  class="blue-svg"
-                  style="width: 50px; height: 50px;"
-                  src="${window.setupWizardShare.admin_image_url +
-                  'community.svg'}"
-                /><br />
-                <a
-                  href="https://community.disciple.tools/"
-                  target="_blank"
-                  class="button btn-primary btn-with-icon"
-                >
-                  Community Forum
-                  <img
-                    class="white-svg"
-                    src="${window.setupWizardShare.image_url + 'open-link.svg'}"
-                  />
-                </a>
+                  class="white-svg"
+                  src="${window.setupWizardShare.image_url + 'open-link.svg'}"
+                />
+              </a>
               </div>
-              <div style="text-align: center">
-                <img
-                  class="blue-svg"
-                  style="width: 50px; height: 50px;"
-                  src="${window.setupWizardShare.admin_image_url +
-                  'discord.svg'}"
-                /><br />
-                <a
-                  href="https://discord.gg/kp5pYmrhSd"
-                  target="_blank"
-                  class="button btn-primary btn-with-icon"
-                >
-                  Discord Invitation
+
+              <p>
+                For any questions or further assistance in getting
+                Disciple.Tools to work for you, please reach out on our
+                community forum or our discord channel.
+              </p>
+              <div
+                style="display: grid; grid-template-columns: 1fr 1fr; gap: 1rem"
+              >
+                <div style="text-align: center">
                   <img
-                    class="white-svg"
-                    src="${window.setupWizardShare.image_url + 'open-link.svg'}"
-                  />
-                </a>
+                    class="blue-svg"
+                    style="width: 50px; height: 50px;"
+                    src="${
+                      window.setupWizardShare.admin_image_url + 'community.svg'
+                    }"
+                  /><br />
+                  <a
+                    href="https://community.disciple.tools/"
+                    target="_blank"
+                    class="button btn-primary btn-with-icon"
+                  >
+                    Community Forum
+                    <img
+                      class="white-svg"
+                      src="${
+                        window.setupWizardShare.image_url + 'open-link.svg'
+                      }"
+                    />
+                  </a>
+                </div>
+                <div style="text-align: center">
+                  <img
+                    class="blue-svg"
+                    style="width: 50px; height: 50px;"
+                    src="${
+                      window.setupWizardShare.admin_image_url + 'discord.svg'
+                    }"
+                  /><br />
+                  <a
+                    href="https://discord.gg/kp5pYmrhSd"
+                    target="_blank"
+                    class="button btn-primary btn-with-icon"
+                  >
+                    Discord Invitation
+                    <img
+                      class="white-svg"
+                      src="${
+                        window.setupWizardShare.image_url + 'open-link.svg'
+                      }"
+                    />
+                  </a>
+                </div>
               </div>
             </div>
           </div>
+          <setup-wizard-controls
+            hideSkip
+            nextLabel=${this.translations.finish}
+            @next=${this.next}
+            @back=${this.back}
+            nextLabel="Close Wizard"
+          ></setup-wizard-controls>
         </div>
-        <setup-wizard-controls
-          hideSkip
-          nextLabel=${this.translations.finish}
-          @next=${this.next}
-          @back=${this.back}
-          nextLabel="Close Wizard"
-        ></setup-wizard-controls>
       </div>
     `;
   }
