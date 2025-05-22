88cbc90650a43d7156b0a4ac5c8843592fe58d9d
kodinkat <19330800+kodinkat@users.noreply.github.com>
2025-05-22 13:16:45 +0100

    #2559 - Customizations: Cannot Delete Default Blank Option In Key Select (#2675)
    * #2559 - Customizations: Cannot delete default blank option in key select
    
    * #2410 - Custom Create Comm-Channels On Custom Post Types
    
    * Rollback #2410 changes
    
    * Support manual removal of default blank option instead of auto-removal
    
    * Addressed spacing code smell
    
    * Addressed array spacing code smell

--- dt-core/admin/js/dt-settings.js
+++ dt-core/admin/js/dt-settings.js
@@ -2211,7 +2211,7 @@ jQuery(document).ready(function ($) {
                 <!-- START TOGGLED ITEMS -->
                 <div class="field-settings-table-child-toggle">
                     <div class="field-settings-table-field-option">
-                       <span class="sortable ui-icon ui-icon-arrow-4"></span>
+                        <span class="sortable ui-icon ui-icon-arrow-4"></span>
                         <span class="field-name-content">default blank</span>
                     </div>
                     <div class="field-settings-table-field-option new-field-option" data-parent-tile-key="${tile_key}" data-field-key="${field_key}">
