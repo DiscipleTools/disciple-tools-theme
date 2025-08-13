9ad5db4414e4323f7f32ceab87eb99807cfdfc42
kodinkat <me@karlkatewu.com>
2025-08-13 12:02:49 +0100

    Enhance update triggers management in admin panel
                    - Updated JavaScript to improve the handling of update triggers, allowing users to add, delete, and save configurations dynamically.
                    - Refactored the HTML structure of the update triggers table for better clarity and maintainability.
                    - Introduced a new save function to capture and submit update trigger settings, including status, field, option, days, comment, and translations.
                    - Updated PHP logic to process the new structure of update trigger options and ensure proper data handling.
                    - Improved user interface elements for better interaction and usability in managing update triggers.

--- dt-core/admin/menu/tabs/tab-general.php
+++ dt-core/admin/menu/tabs/tab-general.php
@@ -420,25 +420,11 @@ class Disciple_Tools_General_Tab extends Disciple_Tools_Abstract_Menu_Base
             }
             $site_options['update_required']['enabled'] = isset( $_POST['triggers_enabled'] );
 
-            // TODO: REWORK THIS LOGIC TO WORK AROUND OPTION INDICES; WHICH COULD CHANGE WHEN ROWS DELETED/ADDED!
-            foreach ( $site_options['update_required']['options'] as $option_index => $option ){
-                if ( isset( $_POST[$option_index . '_days'] ) ){
-                    $site_options['update_required']['options'][$option_index]['days'] = sanitize_text_field( wp_unslash( $_POST[$option_index . '_days'] ) );
-                }
-                if ( isset( $_POST[$option_index . '_comment'] ) ){
-                    $site_options['update_required']['options'][$option_index]['comment'] = wp_unslash( sanitize_text_field( wp_unslash( $_POST[$option_index . '_comment'] ) ) );
-                }
-                if ( isset( $_POST[$option_index . '_translations'] ) ){
-                    $translations = [];
-                    $uploaded_translations = dt_recursive_sanitize_array( $_POST[$option_index . '_translations'] );
-                    foreach ( $uploaded_translations ?? [] as $lang_key => $translation ) {
-                        if ( !empty( $translation ) ) {
-                            $translations[$lang_key] = sanitize_text_field( wp_unslash( $translation ) );
-                        }
-                    }
-                    $site_options['update_required']['options'][$option_index]['translations'] = $translations;
-                }
+            // Capture updated trigger options.
+            if ( isset( $_POST['update_needed_triggers_options'] ) ) {
+                $site_options['update_required']['options'] = json_decode( sanitize_text_field( wp_unslash( $_POST['update_needed_triggers_options'] ) ), true );
             }
+
             update_option( 'dt_site_options', $site_options, true );
         }
 
@@ -474,11 +460,11 @@ class Disciple_Tools_General_Tab extends Disciple_Tools_Abstract_Menu_Base
     }
 
     public function update_required_options(){
-        $site_options = dt_seeker_path_triggers_capture_pre_existing_options( dt_get_option( 'dt_site_options' ) );
+        $site_options = dt_get_option( 'dt_site_options' );
         $available_languages = dt_get_available_languages();
         ?>
         <h3><?php esc_html_e( 'Contacts', 'disciple_tools' ) ?></h3>
-        <form method="post" name="update_required-form">
+        <form id="update_needed_triggers_form" method="post" name="update_required-form">
             <button type="submit" class="button-like-link" name="run_update_required" value="1"><?php esc_html_e( 'Run checker now', 'disciple_tools' ) ?></button>
             <p><?php esc_html_e( 'Change how long to wait before a contact needs an update', 'disciple_tools' ) ?></p>
 
@@ -506,6 +492,7 @@ class Disciple_Tools_General_Tab extends Disciple_Tools_Abstract_Menu_Base
             <table id="update_needed_triggers_table" class="widefat">
                 <thead>
                     <tr>
+                        <th><?php esc_html_e( 'Status', 'disciple_tools' ) ?></th>
                         <th><?php esc_html_e( 'Field', 'disciple_tools' ) ?></th>
                         <th><?php esc_html_e( 'Option', 'disciple_tools' ) ?></th>
                         <th><?php esc_html_e( 'Days to wait', 'disciple_tools' ) ?></th>
@@ -514,12 +501,16 @@ class Disciple_Tools_General_Tab extends Disciple_Tools_Abstract_Menu_Base
                         <th></th>
                     </tr>
                 </thead>
-                <tbody></tbody>
+                <tbody id="update_needed_triggers_table_tbody"></tbody>
             </table>
+
+            <input id="update_needed_triggers_latest_site_options" name="update_needed_triggers_latest_site_options" type="hidden" value="<?php echo esc_attr( wp_json_encode( $site_options, JSON_PRETTY_PRINT ) ) ?>" />
+            <input id="update_needed_triggers_options" name="update_needed_triggers_options" type="hidden" />
+
             <br>
             <span style="float:right;">
                 <button class="button float-right add-update-trigger"><?php esc_html_e( 'New Trigger', 'disciple_tools' ) ?></button>
-                <button type="submit" class="button float-right"><?php esc_html_e( 'Save', 'disciple_tools' ) ?></button>
+                <button class="button float-right save-update-triggers"><?php esc_html_e( 'Save', 'disciple_tools' ) ?></button>
             </span>
         </form>
 
