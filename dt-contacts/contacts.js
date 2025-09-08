03d1c362c1bb422916cd2814461c7a4f59ebfa44
kodinkat <me@karlkatewu.com>
2025-09-08 17:47:14 +0100

    Refactor API call formatting in contacts.js for improved readability
                    - Updated the formatting of the API call to get duplicates, enhancing code clarity by aligning parameters and removing unnecessary line breaks.

--- dt-contacts/contacts.js
+++ dt-contacts/contacts.js
@@ -141,13 +141,13 @@ jQuery(document).ready(function ($) {
       original_contact_html += `</div>`;
       $('#original-contact').append(original_contact_html);
 
-      window.API.get_duplicates_on_post('contacts', post_id, { 'ignore_archived': '1' }).done(
-        (dups_with_data) => {
-          possible_duplicates = dups_with_data;
-          $('#duplicates-spinner').removeClass('active');
-          loadDuplicates();
-        },
-      );
+      window.API.get_duplicates_on_post('contacts', post_id, {
+        ignore_archived: '1',
+      }).done((dups_with_data) => {
+        possible_duplicates = dups_with_data;
+        $('#duplicates-spinner').removeClass('active');
+        loadDuplicates();
+      });
 
       openedOnce = true;
     }
