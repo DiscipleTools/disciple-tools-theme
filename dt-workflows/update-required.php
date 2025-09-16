363c33e2ec16a51d3add47126a5aad84618f40e1
corsacca <corsacca@gmail.com>
2025-09-16 16:06:10 +0100

    In update needed reminders, check for comments (#2754)
    * In update needed reminders, check for comments
    
    * remove test code

--- dt-workflows/update-required.php
+++ dt-workflows/update-required.php
@@ -61,6 +61,7 @@ class Disciple_Tools_Update_Needed_Async extends Disciple_Tools_Async_Task {
                     AND overall_status_field.meta_value = %s
                     AND setting_field.meta_value = %s
                     AND %d >= ( SELECT MAX( hist_time ) FROM $wpdb->dt_activity_log WHERE object_id = $wpdb->posts.ID and user_id != 0 && action = 'field_update' )
+                    AND NOT EXISTS ( SELECT 1 FROM $wpdb->comments WHERE comment_post_ID = $wpdb->posts.ID AND user_id != 0 )
                     AND $wpdb->posts.post_type = 'contacts' AND $wpdb->posts.post_status = 'publish'
                     GROUP BY $wpdb->posts.ID ORDER BY $wpdb->posts.post_date DESC LIMIT 0, 50",
                         esc_sql( $setting_field ),
@@ -112,6 +113,7 @@ class Disciple_Tools_Update_Needed_Async extends Disciple_Tools_Async_Task {
                     WHERE ( requires_update_field.meta_value = '' OR requires_update_field.meta_value = '0' OR requires_update_field.meta_value IS NULL )
                     AND group_status_field.meta_value = %s
                     AND %d >= ( SELECT MAX( hist_time ) FROM $wpdb->dt_activity_log WHERE object_id = $wpdb->posts.ID and user_id != 0 && action = 'field_update' )
+                    AND NOT EXISTS ( SELECT 1 FROM $wpdb->comments WHERE comment_post_ID = $wpdb->posts.ID AND user_id != 0 )
                     AND $wpdb->posts.post_type = 'groups' AND $wpdb->posts.post_status = 'publish'
                     GROUP BY $wpdb->posts.ID ORDER BY $wpdb->posts.post_date DESC LIMIT 0, 50",
                     esc_sql( $setting['status'] ),
